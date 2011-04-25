1d71e6144a258671cedf2c6b8d084d10bfc80f02
Karim Malhas <karim@malhas.de>
2011-04-25 17:01:25 +0200

    added WMSBundle

---
+++ Controller/WMSController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MB\WMSBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class WMSController extends Controller {
+
+    /**
+     * Shows the startpage of the WMS Bundle
+    */
+    public function indexAction(){
+        return $this->render("MBWMSBundle:WMS:index.html.twig");
+    }
+
+    /**
+     * Shows the details of a WMS
+    */
+    public function detailsAction($wmsId){
+        $wmsTitle = "Title for $wmsId";
+        return $this->render("MBWMSBundle:WMS:details.html.twig",array('wmsTitle'=>$wmsTitle));
+    }
+    
+    /**
+     * shows the dialog that allows adding a WMS
+    */
+    public function showaddAction(){
+        return $this->render("MBWMSBundle:WMS:showadd.html.twig");
+    }
+
+    /**
+     * shows preview of WMS
+    */
+    public function previewAction(){
+        $getcapa_url = $_POST['getcapa_url'];
+        return $this->render("MBWMSBundle:WMS:preview.html.twig",
+            array( "getcapa_url"=>$getcapa_url)
+        );
+    }
+    
+    /**
+     * shows preview of WMS
+    */
+    public function addAction(){
+
+        // add and then redirect
+        $newWMSId = 4;
+        $uri = $this->generateUrl("wms_details",array("wmsId"=>$newWMSId));
+        return  new RedirectResponse($uri);
+    }
+
+}
---
+++ MBWMSBundle.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace MB\WMSBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MBWMSBundle extends Bundle {
+}
