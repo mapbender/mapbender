41aea5fe8bad32ca98f632a1ecc77f025db8850c
Christian Wygoda <arsgeografica@gmail.com>
2011-05-07 12:23:44 +0200

    Added basic user entity

---
+++ src/Mapbender/CoreBundle/Entity/User.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Mapbender\CoreBundle\Entity;
+
+/**
+ * User entity.
+ *
+ * @author Christian Wygoda <arsgeografica@gmail.com>
+ * @orm:Entity
+ */
+class User {
+	/** 
+	 * @orm:Column(type="integer") 
+	 * @orm:Id
+	 */
+	protected $id;
+
+	/** @orm:Column(length=256) */
+	protected $first_name;
+
+	/** @orm:Column(length=256) */
+	protected $last_name;
+
+	/** @orm:Column(length=256) */
+	protected $email;
+
+	/**
+	 * Set id
+	 *
+	 * @param integer $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * Get id
+	 *
+	 * @return integer $id
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set first_name
+	 *
+	 * @param string $firstName
+	 */
+	public function setFirstName($firstName)
+	{
+		$this->first_name = $firstName;
+	}
+
+	/**
+	 * Get first_name
+	 *
+	 * @return string $firstName
+	 */
+	public function getFirstName()
+	{
+		return $this->first_name;
+	}
+
+	/**
+	 * Set last_name
+	 *
+	 * @param string $lastName
+	 */
+	public function setLastName($lastName)
+	{
+		$this->last_name = $lastName;
+	}
+
+	/**
+	 * Get last_name
+	 *
+	 * @return string $lastName
+	 */
+	public function getLastName()
+	{
+		return $this->last_name;
+	}
+
+	/**
+	 * Set email
+	 *
+	 * @param string $email
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+	/**
+	 * Get email
+	 *
+	 * @return string $email
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Set roles
+	 *
+	 * @param array $roles
+	 */
+	public function setRoles($roles) {
+		//TODO: Store roles
+	}
+
+	/**
+	 * Get roles
+	 *
+	 * @return array $roles
+	 */
+	public function getRoles() {
+		//TODO: Retrieve roles
+		return array('ROLE_USER');
+	}
+}
+
