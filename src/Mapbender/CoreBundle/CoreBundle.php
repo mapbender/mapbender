110a4f345d1dda2f21cf1283bdfc605e76347fde
Christian Wygoda <arsgeografica@gmail.com>
2011-05-07 11:08:55 +0200

    Inital empty CoreBundle

---
+++ src/Mapbender/CoreBundle/CoreBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Mapbender 3 project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mapbender\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * CoreBundle.
+ *
+ * @author Christian Wygoda <arsgeografica@gmail.com>
+ */
+class CoreBundle extends Bundle {
+}
+
