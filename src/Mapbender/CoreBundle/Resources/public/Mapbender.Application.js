20cf67ebedb75e232c8265a2817b800310aafff0
Christian Wygoda <christian.wygoda@wheregroup.com>
2011-05-17 16:41:16 +0200

    Changes for namespaced element widgets

--- src/Mapbender/CoreBundle/Resources/public/Mapbender.Application.js
+++ src/Mapbender/CoreBundle/Resources/public/Mapbender.Application.js
@@ -2,7 +2,8 @@ $(function() {
 
 // Initialize all elements by calling their init function with their options
 $.each(Mapbender.configuration.elements, function(id, data) {
-	if(typeof($.mapbender[data.init]) == 'function') {
+	var ns = data.ns || 'mapbender';
+	if(typeof($[ns][data.init]) == 'function') {
 		$('#' + id)[data.init](data.options);
 	}
 });
