220b1efc7ac5a21e995d3411657255cd43f3881d
Christian Wygoda <arsgeografica@gmail.com>
2011-06-23 16:24:57 +0200

    Added MapQuery as a submodule

---
+++ src/Mapbender/CoreBundle/Resources/public/mapquery/src/jquery.mapquery.mqMousePosition.js
@@ -0,0 +1,53 @@
+(function($) {
+$.template('mqMousePosition',
+    '<div class="mq-mouseposition ui-widget ui-helper-clearfix ">'+
+    '<span class="ui-widget-content ui-helper-clearfix ui-corner-all ui-corner-all">'+
+    '<div id="mq-mouseposition-x" class="mq-mouseposition-coordinate"></div><div id="mq-mouseposition-y" class="mq-mouseposition-coordinate"></div>'+
+    '</div></span>');
+        
+$.widget("mapQuery.mqMousePosition", {
+    _create: function() {
+        var map;       
+
+        var self = this;
+        var element = this.element;
+        var mousepos;
+        //get the mapquery object
+        if (this.options.jquery === $().jquery) {
+            map = this.options.data('mapQuery');
+            this.options = {};
+        }
+        else {
+            map = this.options.map.data('mapQuery');
+        }
+        
+        map.bind("mousemove",
+            {widget:self,map:map},
+            self._onMouseMove);
+
+        
+        $.tmpl('mqMousePosition',{
+            mouseposition:mousepos
+        }).appendTo(element);
+       
+    },
+    _onMouseMove: function(evt, data) {
+        var self = evt.data.widget;
+        var element = self.element;
+        
+        var x = data.layerX;
+        var y = data.layerY;
+        var map = evt.data.map;
+        var mapProjection = map.options.projection;
+        var displayProjection = map.options.projection;
+        //if the coordinates should be displayed in something else, set them via the map displayProjection option
+        if(map.options.displayProjection) {
+            displayProjection = map.options.displayProjection; 
+        }
+        var pos = map.olMap.getLonLatFromLayerPx(new OpenLayers.Pixel(x,y));
+        pos=pos.transform(new OpenLayers.Projection(mapProjection),new OpenLayers.Projection(displayProjection));        
+        $("#mq-mouseposition-x", element).html('x: '+pos.lon);
+        $("#mq-mouseposition-y", element).html('y: '+pos.lat);
+    }
+});
+})(jQuery);
---
+++ src/Mapbender/CoreBundle/Resources/public/mapquery/src/jquery.mapquery.mqOverviewMap.js
@@ -0,0 +1,50 @@
+(function($) {
+$.template('mqOverviewMap',
+    '<div id="${id}" class="mq-overviewmap ui-widget ">'+
+    '</div>');
+        
+$.widget("mapQuery.mqOverviewMap", {
+    _create: function() {
+        var map;
+        var self = this;
+        var element = this.element;
+        var id = 'mqOverviewMap-dialog'; //TODO smo20110620 make this configurable
+        
+        //get the mapquery object
+        if (this.options.jquery === $().jquery) {
+            map = this.options.data('mapQuery');
+            this.options = {};
+        }
+        else {
+            map = this.options.map.data('mapQuery');
+        }
+        
+        $.tmpl('mqOverviewMap',{
+            id: id}).appendTo(element);
+                    
+        var dialogElement = $(".mq-overviewmap").dialog({
+            dialogClass: 'mq-overviewmap-dialog',
+            autoOpen: true,
+            title: 'Overview', //TODO smo20110620 make this configurable and/or i18n
+            position: ['right','bottom'], //TODO smo20110620 make this configurable
+            
+            resizeStop: function (event, ui) { 
+                $('.olMap', this).width($(this).width()); $('.olMap', this).height($(this).height());     
+            }
+            
+        });
+        
+        var overviewmapsize = { w: $(dialogElement).width(), h: $(dialogElement).height() };        
+        var mapOptions = map.olMapOptions;
+        //remove the controls, otherwise you end up with recursing events
+        delete mapOptions.controls;                           
+        // use the lowest layer of the map as overviewmap
+        // TODO: make the layer configurable
+        var overview = new OpenLayers.Control.OverviewMap( {div: document.getElementById(id),size:overviewmapsize,mapOptions:mapOptions,layers:[map.layers().reverse()[0].olLayer.clone()]});
+        map.olMap.addControl(overview);
+        
+        // remove OpenLayers blue border around overviewmap
+        $('.olControlOverviewMapElement', dialogElement).removeClass ('olControlOverviewMapElement');
+    }
+});
+})(jQuery);
