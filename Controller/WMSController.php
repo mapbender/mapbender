7ece5dba3a702999bff3173ea35ba4c1d932ed6d
Karim Malhas <karim@malhas.de>
2011-08-15 09:23:25 +0200

    commencing cleanup

--- Controller/WMSController.php
+++ Controller/WMSController.php
@@ -5,7 +5,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use MB\WMSBundle\Entity\WMSService;
 use MB\WMSBundle\Entity\WMSLayer;
 use MB\WMSBundle\Entity\GroupLayer;
@@ -19,28 +21,12 @@ use MB\WMSBundle\Form\WMSType;
 class WMSController extends Controller {
     
     /**
-     * Use this function in all controllers return statements to  augment them with common data.
-     * @param Array The specific data from the controller
-     * @return Array The specific data from the controller augmented with the data common to all controllers,
-    */
-    public function commonData(array $extendedData){
-
-        $em = $this->get("doctrine.orm.entity_manager");
-        $q = $em->createQuery("select wms from MB\WMSBundle\Entity\WMSService wms ");
-        $wmsArr = $q->getResult();
-        $common = array(
-            "menu_wmsArr" => $wmsArr
-        );
-        return array_merge($common,$extendedData);
-    }
-
-    /**
      * Shows the startpage of the WMS Bundle
-     * @Route("/", name="mb_wms_index", requirements = { "_method" = "GET" })
+     * @Route("/")
+     * @Method({"GET"})
      * @Template()
     */
     public function indexAction(){
-
 
         $request = $this->get('request');
         $first = $request->get('first') ? $request->get('first') : 0;
@@ -56,112 +42,29 @@ class WMSController extends Controller {
 
         $nextFirst = count($wmsArr) < $max ? $first : $first + $max;
         $prevFirst = ($first - $max)  > 0 ? $first - $max : 0;
-        return $this->commonData(array(
+        return array(
             "wmsArr" => $wmsArr,
             "nextFirst" =>  $nextFirst,
             "prevFirst" => $prevFirst,
             "max" => $max
-        ));
+        );
     }
 
 
-    /**
-     * Shows the details of a WMS
-     * @Route("/{id}", name="mb_wms_details", requirements = { "id" = "\d+","_method" = "GET" })
-     * @Template()
-    */
-    public function detailsAction($id){
-        $em = $this->get("doctrine.orm.entity_manager");
-        $wms = $em->find('MBWMSBundle:WMSService',$id);
-
-        if(!$wms){
-            return array();
-        }
-        $bounds = $wms->getRootLayer()->getLatLonBounds();
-        $b = explode(" ",$bounds);
-        return $this->commonData(array(
-            "wms" => $wms,
-            "minx" => isset($b[0])?$b[0]:"",
-            "miny" => isset($b[1])?$b[1]:"",
-            "maxx" => isset($b[2])?$b[2]:"",
-            "maxy" => isset($b[3])?$b[3]:""
-        ));
-    }
-    
     /**
      * shows the dialog that allows adding a WMS
-     * @Route("/add", name="mb_wms_showadd", requirements = { "_method" = "GET" })
+     * @Route("/add")
+     * @Method({"GET"})
      * @Template()
     */
-    public function showaddAction(){
-        return $this->commonData(array());
+    public function registerAction(){
+        return array();
     }
-    /**
-     * adds a WMS
-     * @Route("/", name="mb_wms_add", requirements = { "_method" = "POST" })
-     * @Template()
-    */
-    public function addAction(){
-
-        $request = $this->get('request');
-        /* build up nested wmslayer structure */
-        $requestWMS = $request->get('WMSService');
-        $wms = new WMSService();
-        $wms = $this->buildWMSFormStructure($requestWMS,$wms);
-        $form = $this->get('form.factory')->create(new WMSType(),$wms); 
-        $form->bindRequest($request);
     
-        if($form->isValid()){
-            $em = $this->get("doctrine.orm.entity_manager");
-            $this->persistRecursive($wms,$em);
-            $em->persist($wms);
-            $em->flush();
-            return $this->redirect($this->generateUrl("mb_wms_details",array("id" => $wms->getId()),true));
-        }else{
-            // FIXME: getcapa_url is missing, xml is missing
-            return $this->render("MBWMSBundle:WMS:preview.html.twig",array(
-                    "getcapa_url"=> "",
-                    "wms" => $wms,
-                    "form" => $form->createView(),
-                    "xml" =>""
-                ));
-        }
-    
-        
-    }
-
-    /**
-     * shows the dialog for wms Deletion confirmation
-     * @Route("/{id}/delete", name="mb_wms_showdelete", requirements = { "id" = "\d+","_method" = "GET" })
-     * @Template()
-    */
-    public function showdeleteAction($id){
-        return $this->commonData(array(
-                'wmsTitle'=>"",
-                'wmsId' => $id
-        ));
-    }
-    /**
-     * deletes a WMS
-     * @Route("/{id}/delete", name="mb_wms_delete", requirements = { "id" = "\d+","_method" = "POST" })
-     * @Template()
-    */
-    public function deleteAction($id){
-
-        $em = $this->get("doctrine.orm.entity_manager");
-        $wms = $em->find('MBWMSBundle:WMSService',$id);
-        $em->remove($wms);
-        $em->flush();
-
-        return $this->commonData(array(
-            "wmsId"=>$wms->getId(),
-            "wmsTitle"=>$wms->getTitle()
-        ));
-    }
-
     /**
      * shows preview of WMS
-     * @Route("/preview", name="mb_wms_preview", requirements = { "_method" = "POST" })
+     * @Route("/preview")
+     * @Method({"POST"})
      * @Template()
     */
     public function previewAction(){
@@ -214,15 +117,128 @@ class WMSController extends Controller {
             "requestPutStylesFormats" => $wms->getRequestPutStylesFormats(),
         ));
 
-        return $this->commonData(array(
+        return array(
                 "getcapa_url"=>$getcapa_url,
                 "wms" => $wms,
                 "form" => $form->createView(),
                 "xml" => $data
-            ));
+            );
     }
 
+    /**
+     * adds a WMS
+     * @Route("/")
+     * @Method({"POST"})
+    */
+    public function addAction(){
 
+        $request = $this->get('request');
+        /* build up nested wmslayer structure */
+        $requestWMS = $request->get('WMSService');
+        $wms = new WMSService();
+        $wms = $this->buildWMSFormStructure($wms,$requestWMS);
+        $form = $this->get('form.factory')->create(new WMSType(),$wms); 
+        $form->bindRequest($request);
+    
+        if($form->isValid()){
+            $em = $this->get("doctrine.orm.entity_manager");
+            $this->persistRecursive($wms,$em);
+            $em->persist($wms);
+            $em->flush();
+            return $this->redirect($this->generateUrl("mb_wms_wms_edit",array("id" => $wms->getId()),true));
+        }else{
+            // FIXME: getcapa_url is missing, xml is missing
+            return $this->render("MBWMSBundle:WMS:preview.html.twig",array(
+                    "getcapa_url"=> "",
+                    "wms" => $wms,
+                    "form" => $form->createView(),
+                    "xml" =>""
+                ));
+        }
+    
+        
+    }
+
+    /**
+     * Shows the WMS in an Editor
+     * @Route("/{id}"))
+     * @Method({"GET"})
+     * @Template()
+    */
+    public function editAction(WMSService $wms){
+        $form = $this->get('form.factory')->create(new WMSType(),$wms); 
+        return array(
+            "wms" => $wms,
+            "form"  => $form->createView(),
+        );
+    }
+    
+    /**
+     * Shows the WMS in an Editor
+     * @Route("/{id}"))
+     * @Method({"POST"})
+     * @Template()
+    */
+    public function saveAction(WMSService $wms){
+        $request = $this->get('request');
+        /* build up nested wmslayer structure */
+        $requestWMS = $request->get('WMSService');
+        $form = $this->get('form.factory')->create(new WMSType(),$wms); 
+        $form->bindRequest($request);
+        $em = $this->get("doctrine.orm.entity_manager");
+        $this->persistRecursive($wms,$em);
+        return $this->redirect($this->generateUrl("mb_wms_wms_edit", array("id"=>$wms->getId())));
+    }
+
+    /**
+     * shows the dialog for wms Deletion confirmation
+     * @Route("/{id}/delete")
+     * @Method({"GET"})
+     * @Template()
+    */
+    public function confirmdeleteAction(WMSService $wms){
+        return array(
+               'wms' => $wms 
+        );
+    }
+
+    /**
+     * deletes a WMS
+     * @Route("/{id}/delete")
+     * @Method({"POST"})
+    */
+    public function deleteAction(WMSService $wms){
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($wms);
+        $em->flush();
+        return $this->redirect($this->generateUrl("mb_wms_wms_index"));
+    }
+
+    /**
+     * FIXME: this can be removed
+     * Shows the details of a WMS
+     * @Route("/{id}/details")
+     * @Method({"GET"})
+     * @Template()
+    */
+    public function detailsAction(WMSService $wms){
+        $bounds = $wms->getRootLayer()->getLatLonBounds();
+        $b = explode(" ",$bounds);
+        return array(
+            "wms" => $wms,
+            "minx" => isset($b[0])?$b[0]:"",
+            "miny" => isset($b[1])?$b[1]:"",
+            "maxx" => isset($b[2])?$b[2]:"",
+            "maxy" => isset($b[3])?$b[3]:""
+        );
+    }
+    
+
+    /**
+     * Recursively persists a nested Layerstructure
+     * param GroupLayer
+     * param EntityManager
+    */
     public function persistRecursive($grouplayer,$em){
         $em->persist($grouplayer);
         if(count($grouplayer->getLayer()) > 0 ){
@@ -234,14 +250,18 @@ class WMSController extends Controller {
         $em->flush();
     }
     /**
-     * Aids in Form Construction
+     *  
+     * Takes an Arraystructure from a POSTrequest and recurses into the nested layers to build a matching WMSLayer structure
+     * So that a Form can be bound to the layer
+     * param GroupLayer the rootlayer of the Layer hierarchy
+     * param array POST request from a WMS structure
     */
-    public function buildWMSFormStructure(array $grouplayerArr, $grouplayer){
+    public function buildWMSFormStructure($grouplayer,array $grouplayerArr){
         if(isset($grouplayerArr['layer']) && is_array($grouplayerArr['layer'])){
                 foreach($grouplayerArr['layer'] as $layerArr){
                         $layer = new WMSLayer();
                         if(isset($layerArr['layer']) && is_array($layerArr['layer']) && count($layerArr['layer'])){
-                                $layer = $this->buildWMSFormStructure($layerArr,$layer);
+                                $layer = $this->buildWMSFormStructure($layer, $layerArr);
 
                         }   
                         $grouplayer->addLayer($layer);
@@ -249,7 +269,5 @@ class WMSController extends Controller {
         }   
         return $grouplayer;
     } 
-    
-
 
 }
--- Controller/WMSLayerController.php
+++ Controller/WMSLayerController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use MB\WMSBundle\Entity\WMSService;
 use MB\WMSBundle\Entity\WMSLayer;
@@ -18,25 +19,13 @@ use MB\WMSBundle\Form\WMSType;
 */
 class WMSLayerController extends Controller {
 
-    public function getWMS($wmsId){
-        $wms = $this->getDoctrine()
-            ->getRepository('MBWMSBundle:WMSService')
-            ->find($wmsId);
-            
-        if(!$wms){
-            throw new NotFoundHttpException('WMS does not exist');
-        }   
-        return $wms;
-
-    } 
-
     /**
-     * Shows the details of a WMS
-     * @Route("/{wmsId}/layer/{id}", name="mb_wmslayer_details", requirements = { "id" = "\d+","_method" = "GET" })
+     * Shows the WMSlayer in an Editor
+     * @Route("/{wmsId}/layer/{id}")
+     * @Method({"GET"})
      * @Template()
     */
-    public function detailsAction($wmsId = null, $id){
-        $wms = $this->getWMS($wmsId);
+    public function editAction($wmsId = null, $id){
 
         $wmsLayer = $this->getDoctrine()
             ->getRepository('MBWMSBundle:WMSLayer')
@@ -45,6 +34,39 @@ class WMSLayerController extends Controller {
         if(!$wmsLayer){
             throw new NotFoundHttpException('WMSLayer does not exist');
         }
+        
+        $wms = $wmsLayer->getWMS(); $this->getWMS($wmsId);
+        if($wmsId != $wms->getId()){
+            throw new NotFoundHttpException('WMSLayer does not exist');
+        } 
+
+        $form = $this->get('form.factory')->create(new WMSLayerType(),$wmsLayer); 
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * FIXME: can be removed
+     * Shows the details of a WMS
+     * @Route("/{wmsId}/layer/{id}")
+     * @Method({"GET"})
+     * @Template()
+    */
+    public function detailsAction($wmsId = null, $id){
+
+        $wmsLayer = $this->getDoctrine()
+            ->getRepository('MBWMSBundle:WMSLayer')
+            ->find($id);
+
+        if(!$wmsLayer){
+            throw new NotFoundHttpException('WMSLayer does not exist');
+        }
+        
+        $wms = $wmsLayer->getWMS(); $this->getWMS($wmsId);
+        if($wmsId != $wms->getId()){
+            throw new NotFoundHttpException('WMSLayer does not exist');
+        } 
 
         return array(
             "wms" => $wms,
