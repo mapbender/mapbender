904c4986dc6817582c7caf696cf6b8d2b8e95070
Christian Wygoda <arsgeografica@gmail.com>
2011-08-23 10:45:30 +0200

    Use ElementGenator for InitElementCommand

---
+++ src/Mapbender/CoreBundle/Command/ElementGenerator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Mapbender\CoreBundle\Command;
+
+use Sensio\Bundle\GeneratorBundle\Generator\Generator;
+
+class ElementGenerator extends Generator {
+    public function create($container, $bundle, $bundleDir, $bundleNamespace, $className, $type) {
+        $files = Array();
+
+        $classNameLower = strtolower($className);
+
+        // Copy skeleton files
+        $filesystem = $container->get('filesystem');
+
+        $classFile = sprintf('%s/Element/%s.php', $bundleDir, $className);
+        $widgetFile = sprintf('%s/Resources/public/mapbender.element.%s.js', $bundleDir, $classNameLower);
+        $twigFile = sprintf('%s/Resources/views/Element/%s.html.twig', $bundleDir, $classNameLower);
+
+        if(file_exists($classFile) || file_exists($widgetFile) || file_exists($twigFile)) {
+            $msg = array();
+            $msg[] = "One of the the following files already exists and would be overwritten. Aborting.";
+            $msg[] = $classFile;
+            $msg[] = $twigFile;
+            $msg[] = $widgetFile;
+            $msg = implode("\n", $msg);
+            throw new \RuntimeException($msg);
+        }
+
+        $this->renderFile(__DIR__ . '/../Resources/skeleton/element',
+            $type . '.php.twig', $classFile, array(
+            'bundleNamespace' => $bundleNamespace,
+            'className' => $className,
+            'classNameLower' => $classNameLower,
+            'bundle' => $bundle));
+        $files['PHP class'] = $classFile;
+
+        $this->renderFile(__DIR__ . '/../Resources/skeleton/element',
+            $type . '.js.twig', $widgetFile, array(
+                'widgetName' => $className));
+        $files['jQuery widget'] = $widgetFile;
+
+        if($type === 'general') {
+            $this->renderFile(__DIR__ . '/../Resources/skeleton/element',
+                $type . 'html.twig', $twigFile, array(
+                    'classNameLower' => $classNameLower));
+            $files['Twig template'] = $twigFile;
+        }
+
+        return $files;
+    }
+}
--- src/Mapbender/CoreBundle/Command/InitElementCommand.php
+++ src/Mapbender/CoreBundle/Command/InitElementCommand.php
@@ -3,7 +3,7 @@
 namespace Mapbender\CoreBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Bundle\FrameworkBundle\Generator\Generator;
+use Sensio\Bundle\GeneratorBundle\Generator\Generator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,6 +11,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
 
 class InitElementCommand extends ContainerAwareCommand {
+    private $generator;
+
+    protected function getGenerator() {
+        if($this->generator === null) {
+            $this->generator = new ElementGenerator();
+        }
+        return $this->generator;
+    }
     protected function configure() {
         $this->setDefinition(array(
                 new InputArgument('bundle', InputArgument::REQUIRED, 'The bundle namespace of the Element to create'),
@@ -32,7 +40,6 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output) {
         $bundleNamespace = $input->getArgument('bundle');
         $className = $input->getArgument('classname');
-        $classNameLower = strtolower($className);
         $type = $input->getOption('type');
 
         if(!in_array($type, array(
@@ -57,7 +64,7 @@ EOT
         // validate that the namespace is at least one level deep
         if (false === strpos($bundleNamespace, '\\')) {
             $msg = array();
-            $msg[] = sprintf('The namespace must contain a vendor namespace (e.g. "VendorName\%s" instead of simply "%s").', $bundle, $bundle);
+            $msg[] = sprintf('The namespace must contain a vendor namespace (e.g. "VendorName\%s" instead of simply "%s").', $bundleNamespace, $bundleNamespace);
             $msg[] = 'If you\'ve specified a vendor namespace, did you forget to surround it with quotes (mapbender:init:element "Acme\BlogBundle")?';
 
             throw new \InvalidArgumentException(implode("\n\n", $msg));
@@ -74,57 +81,18 @@ EOT
             throw new \RuntimeException(sprintf('Bundle directory "%s" does not exist.', $bundleDir));
         }
 
-        // Copy skeleton files
-        $filesystem = $this->container->get('filesystem');
-
-        $classFile = sprintf('%s/Element/%s.php', $bundleDir, $className);
-        $widgetFile = sprintf('%s/Resources/public/mapbender.element.%s.js', $bundleDir, $classNameLower);
-        $twigFile = sprintf('%s/Resources/views/Element/%s.html.twig', $bundleDir, $classNameLower);
-        if(file_exists($classFile) || file_exists($widgetFile) || file_exists($twigFile)) {
-            $msg = array();
-            $msg[] = "One of the the following files already exists and would be overwritten. Aborting.";
-            $msg[] = $classFile;
-            $msg[] = $twigFile;
-            $msg[] = $widgetFile;
-            $msg = implode("\n", $msg);
-            throw new \RuntimeException($msg);
-        }
-
-        $filesystem->copy(__DIR__ . '/../Resources/skeleton/element/' . $type . '.php',
-            $classFile);
-        Generator::renderFile($classFile, array(
-            'bundleNamespace' => $bundleNamespace,
-            'className' => $className,
-            'classNameLower' => $classNameLower,
-            'bundle' => $bundle,
-        ));
-
-        $filesystem->copy(__DIR__ . '/../Resources/skeleton/element/widget-' . $type . '.js',
-            $widgetFile);
-        Generator::renderFile($widgetFile, array(
-            'widgetName' => $className
-        ));
-
-        if($type === 'general') {
-            $filesystem->copy(__DIR__ . '/../Resources/skeleton/element/widget-' . $type . '.html.twig',
-                $twigFile);
-            Generator::renderFile($twigFile, array(
-                'classNameLower' => $classNameLower
-            ));
-        }
+        $files = $this->getGenerator()->create($this->getContainer(),
+            $bundle, $bundleDir, $bundleNamespace, $className, $type);
 
         $output->writeln('<comment>Summary of actions</comment>');
         $output->writeln(sprintf('- Your element %s\Element\%s has been created.', $bundle, $className));
-        $output->writeln('- A PHP class, Twig template and jQuery widget have been created:');
-        $output->writeln('  - ' . $classFile);
-        if($type === 'general') {
-            $output->writeln('  - ' . $twigFile);
+        $output->writeln('- The following files have been created:');
+        foreach($files as $k => $v) {
+            $output->writeLn(sprintf('  - %s (%s)', $k, $v));
         }
-        $output->writeln('  - ' . $widgetFile);
         $output->writeln('');
         $output->writeln('<comment>Follow up actions</comment>');
         $output->writeln('Read about adapting your bare-bone element at <info>http://mapbender.org/3/cookbook/element-from-skeleton</info>');
-        //TODO: SUmmary
     }
 }
 
