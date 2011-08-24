d916a8cb7f0eb166b633a8e8a7806f41b66e7fda
Christian Wygoda <arsgeografica@gmail.com>
2011-08-24 11:42:14 +0200

    Basic WMTS support

---
+++ src/Mapbender/WmtsBundle/MapbenderWmtsBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Mapbender 3 project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mapbender\WmtsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * WMTSBundle.
+ *
+ * @author Christian Wygoda <arsgeografica@gmail.com>
+ */
+class MapbenderWmtsBundle extends Bundle {
+}
+
---
+++ src/Mapbender/WmtsBundle/Resources/public/mapbender.layer.wmts.js
@@ -0,0 +1,29 @@
+Mapbender.layer = $.extend(Mapbender.layer, {
+    'wmts': {
+        create: function(layerDef) {
+            var origin = null;
+            if(layerDef.configuration.origin) {
+                origin = new OpenLayers.LonLat(
+                    layerDef.configuration.origin[0],
+                    layerDef.configuration.origin[1]);
+            }
+            mqLayerDef = {
+                type:        'wmts',
+                label:       layerDef.configuration.title,
+                url:         layerDef.configuration.url,
+
+                layer:       layerDef.configuration.layer,
+                style:       layerDef.configuration.style,
+                matrixSet:   layerDef.configuration.matrixSet,
+                format:      layerDef.configuration.format,
+                tileOrigin:  origin,
+
+                isBaseLayer: layerDef.configuration.baselayer,
+                opacity:     layerDef.configuration.opacity,
+                visible:     layerDef.configuration.visible
+            };
+            return mqLayerDef;
+        }
+    }
+});
+
