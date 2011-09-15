e1d98978c2696f0ce9ab9dd406fee09d3075b812
Christian Wygoda <arsgeografica@gmail.com>
2011-09-15 08:58:21 +0200

    Starting the login process...

--- src/Mapbender/CoreBundle/Controller/UserController.php
+++ src/Mapbender/CoreBundle/Controller/UserController.php
@@ -18,64 +18,64 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
  * @Route("/user")
  */
 class UserController extends Controller {
-	protected $em;
+    protected $em;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function setContainer(ContainerInterface $container = NULL) {
-		parent::setContainer($container);
-		if($this->container === NULL) {
-			throw \Exception('Mapbender\CoreBundle\Controller\UserController requires the container to be set.');
-		}
+    /**
+     * @inheritdoc
+     */
+    public function setContainer(ContainerInterface $container = NULL) {
+        parent::setContainer($container);
+        if($this->container === NULL) {
+            throw \Exception('Mapbender\CoreBundle\Controller\UserController requires the container to be set.');
+        }
 
-		$this->em = $this->get('doctrine.orm.default_entity_manager');
-	}
+        $this->em = $this->get('doctrine.orm.default_entity_manager');
+    }
 
-	/**
-	 * User login
-	 *
-	 * @Route("/login", name="mapbender_user_login")
-	 * @Template()
-	 */
-	public function loginAction() {
-		$request = $this->get('request');
-		if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-		} else {
-			$error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
-		}
+    /**
+     * User login
+     *
+     * @Route("/login")
+     * @Template()
+     */
+    public function loginAction() {
+        $request = $this->get('request');
+        if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
 
-		return array(
-			'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
-			'error' => $error,
-		);
-	}
+        return array(
+            'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error' => $error,
+        );
+    }
 
-	/**
-	 * @Route("/login_check", name="_security_check")
-	 */
-	public function loginCheckAction() {
-		//Don't worry, this is actually intercepted by the security layer.
-	}
+    /**
+     * @Route("/login/check")
+     */
+    public function loginCheckAction() {
+        //Don't worry, this is actually intercepted by the security layer.
+    }
 
-	/**
-	 * @Route("/logout", name="mapbender_user_logout")
-	 */
-	public function logoutAction() {
-		//Don't worry, this is actually intercepted by the security layer.
-	}
+    /**
+     * @Route("/logout")
+     */
+    public function logoutAction() {
+        //Don't worry, this is actually intercepted by the security layer.
+    }
 
-	/**
-	 * @Route("/", name="mapbender_user_profile")
-	 * @Secure("ROLE_USER")
-	 * @Template()
-	 */
-	public function profileAction() {
-		$user = $this->get('security.context')->getToken()->getUser();
-		return array(
-			'user' => $user,
-		);
-	}
+    /**
+     * @Route("/")
+     * @Secure("ROLE_USER")
+     * @Template()
+     */
+    public function profileAction() {
+        $user = $this->get('security.context')->getToken()->getUser();
+        return array(
+            'user' => $user,
+        );
+    }
 }
 
