64807ebbf894d7fa8a998c075adfbe3c6161d899
Christian Wygoda <arsgeografica@gmail.com>
2011-09-15 14:04:14 +0200

    Bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+# Store current directory away
 start=`pwd`
 
+# Clone starter and Mapbender
 git clone git://github.com/mapbender/mapbender-starter.git mapbender-starter
 cd mapbender-starter
 git submodule update --init --recursive
+
+# Get extra deps needed by MapQuery
 cd application/mapbender/src/Mapbender/CoreBundle/Resources/public/mapquery/lib
 ./getdepsh.sh
 
-cd $start
+cd "$start/mapbender-starter/application"
+
+# Install assets
+app/console assets:install --symlink web
+
+# Create SQLite database
+app/console doctrine:database:create
+app/console doctrine:schema:create
 
