62c8d1e2113d06ac0b6c7bb410134bffa49d4c0f
A.R.Pour <a.rashid.pour@wheregroup.com>
2011-10-19 12:21:20 +0200

    Formular angepasst

--- src/Mapbender/MonitoringBundle/Form/MonitoringDefinitionType.php
+++ src/Mapbender/MonitoringBundle/Form/MonitoringDefinitionType.php
@@ -14,7 +14,23 @@ class MonitoringDefinitionType extends AbstractType {
 	}
 	
 	public function buildForm(FormBuilder $builder,array $options) {
+//		$builder->add("type","",array(
+//			"required" => false
+//		));
+//		
+//		$builder->add("typeId","",array(
+//			"required" => false
+//		));
+		
+		$builder->add("name","text",array(
+			"required" => false
+		));
+		
 		$builder->add("title","text",array(
+			"required" => false
+		));
+		
+		$builder->add("alias","text",array(
 			"required" => false
 		));
 		
@@ -22,6 +38,46 @@ class MonitoringDefinitionType extends AbstractType {
 		));
 		
 		$builder->add("requestUrl","textarea",array(
+		));
+		
+//		$builder->add("response","",array(
+//			"required" => false
+//		));
+//		
+//		$builder->add("lastResponse","",array(
+//			"required" => false
+//		));
+		
+//		$builder->add("contactEmail","",array(
+//			"required" => false
+//		));
+		
+//		$builder->add("contact","text",array(
+//			"required" => false
+//		));
+		
+//		$builder->add("lastNotificationTime","",array(
+//			"required" => false
+//		));
+		
+		$builder->add("ruleStart","text",array(
+			"required" => false
+		));
+		
+		$builder->add("ruleEnd","text",array(
+			"required" => false
+		));
+		
+		$builder->add("ruleMonitor","choice",array(
+			'choices'   => array('0' => 'allow', '1' => 'disallow'),
+			'preferred_choices' => array('allow'),
+			"required" => false
+		));
+		
+		$builder->add("enabled","choice",array(
+			'choices'   => array('0' => 'true', '1' => 'false'),
+			'preferred_choices' => array('true'),
+			"required" => false
 		));
 	}
 }
