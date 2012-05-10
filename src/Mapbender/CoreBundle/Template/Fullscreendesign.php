2b8bf3c58ec962fa7c73775a8effee35826f1631
schaef <jschaefermeyer@wheregroup.com>
2012-05-10 13:35:03 +0200

    Versuch Änderungen für icons

---
+++ src/Mapbender/CoreBundle/Template/Fullscreendesign.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mapbender\CoreBundle\Template;
+
+use Mapbender\CoreBundle\Component\TemplateInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+class Fullscreen implements TemplateInterface {
+	private $templating;
+
+	public function __construct(EngineInterface $templating) {
+		$this->templating = $templating;
+	}
+
+	public function getTemplate($_format) {
+		return 'MapbenderCoreBundle:Template:fullscreendesign.html.twig';
+	}
+
+	public function getMetadata() {
+		return array(
+			'type' => 'application',
+			'regions' => array('top', 'content'),
+			'css' => array('mapbender.template.fullscreen.css'),
+			'js' => array(),
+		);
+	}
+
+	public function render($data, $parts, $_format = 'html') {
+		return $this->templating->render($this->getTemplate($_format), $data);
+	}
+}
+
