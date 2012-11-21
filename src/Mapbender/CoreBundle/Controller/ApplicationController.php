d683c76143c896d5f295a11320e1df1b127e1ec9
Christian Wygoda <arsgeografica@gmail.com>
2012-11-21 09:33:07 +0100

    CSS Url rewrite target path

--- src/Mapbender/CoreBundle/Controller/ApplicationController.php
+++ src/Mapbender/CoreBundle/Controller/ApplicationController.php
@@ -64,8 +64,8 @@ class ApplicationController extends Controller {
             'css' => array($this->container->get('assetic.filter.cssrewrite')));
 
         // Set target path for CSS rewrite to work
-        $target = $this->get('request')->server->get('DOCUMENT_ROOT')
-            . $this->get('request')->server->get('REQUEST_URI');
+        $target = $this->get('request')->server->get('SCRIPT_FILENAME')
+            . $this->get('request')->server->get('PATH_INFO');
 
         $mimetypes = array(
             'css' => 'text/css',
