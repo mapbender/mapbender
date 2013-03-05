9dfd435c9975cadf2ad971b6e83207f52ee82a64
Vadim Hermann <vhermann@wheregroup.com>
2013-03-05 14:33:43 +0100

    service repository designed;

--- src/Mapbender/WmsBundle/Resources/public/repository/repository.js
+++ src/Mapbender/WmsBundle/Resources/public/repository/repository.js
@@ -1,12 +1,11 @@
 $(function() {
-//    window.console && console.log("WMS");
+    $(".subTitle").bind("click", function(){
+        var parent = $(this).parent();
 
-    $('section#wms-layers .layer legend').bind("click", function(e){
-//        window.console && console.log($(this).parent().find('dl:first'));
-        if($(this).parent().find('dl:first').hasClass("hidde")){
-            $(this).parent().find('dl:first').removeClass("hidde");
-        } else {
-            $(this).parent().find('dl:first').addClass("hidde");
+        if(parent.hasClass("closed")){
+            parent.removeClass("closed");
+        }else{
+            parent.addClass("closed");
         }
     });
 });
