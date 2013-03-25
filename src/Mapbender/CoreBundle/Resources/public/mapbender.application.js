521ce40b0b2f086421fd289d54b1ef68c6b336a7
Christian Wygoda <arsgeografica@gmail.com>
2013-03-25 10:16:44 +0100

    Mapbender element widgets registry for better initalization handling.
    The new Mapbender.elementRegistry is the better way to handle widget
    initialization. Register for your target widget to be ready and have
    your callback fired whenever that happens or immediately when the
    target is already in a ready state.

--- src/Mapbender/CoreBundle/Resources/public/mapbender.application.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.application.js
@@ -1,9 +1,59 @@
 var Mapbender = Mapbender || {};
+
+Mapbender.ElementRegistry = function() {
+    this.readyElements = {};
+    this.readyCallbacks = {};
+
+    this.onElementReady = function(targetId, callback) {
+        if(true === callback) {
+            // Register as ready
+            this.readyElements[targetId] = true;
+
+            // Execute all callbacks registered so far
+            if('undefined' !== this.readyCallbacks[targetId]) {
+                for(var idx in this.readyCallbacks[targetId]) {
+                    this.readyCallbacks[targetId][idx]();
+                }
+
+                // Finally, remove readyCallback list, so they may be garbage
+                // collected if no one else is keeping them
+                delete this.readyCallbacks[targetId];
+            }
+        } else if('function' === typeof callback) {
+            if(true === this.readyElements[targetId]) {
+                // If target is ready already, execute callback right away
+                callback();
+            } else {
+                // Register callback for targetId for later execution
+                this.readyCallbacks[targetId] = this.readyCallbacks[targetId] || [];
+                this.readyCallbacks[targetId].push(callback);
+            }
+        } else {
+            throw 'ElementRegistry.onElementReady callback must be function or undefined!';
+        }
+    }
+};
+Mapbender.elementRegistry = new Mapbender.ElementRegistry();
+
 Mapbender.setup = function() {
     // Initialize all elements by calling their init function with their options
     $.each(Mapbender.configuration.elements, function(id, data) {
         // Split for namespace and widget name
         var widget = data.init.split('.');
+
+        // Register for ready event to operate ElementRegistry
+        var readyEvent = widget[1].toLowerCase() + 'ready';
+        $('#' + id).one(readyEvent, function(event) {
+            for(var i in Mapbender.configuration.elements) {
+                var conf = Mapbender.configuration.elements[i],
+                    widget = data.init.split('.'),
+                    readyEvent = widget[1].toLowerCase() + 'ready';
+                if(readyEvent === event.type) {
+                    Mapbender.elementRegistry.onElementReady(i, true);
+                }
+            }
+        });
+
         // This way we call by namespace and widget name
         // The namespace is kinda useless, as $.widget creates a function with
         // the widget name directly in the jQuery object, too. Still, let's be
