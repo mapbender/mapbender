8f6249602d6c0c223562e30838ba7ee7b06102ad
arp-x201 <info@rpour.de>
2013-05-14 10:31:49 +0200

    Confirm delete dialog

---
+++ src/Mapbender/ManagerBundle/Resources/public/js/service-list.js
@@ -0,0 +1,27 @@
+$(function() {
+    // Delete element
+    $('.iconRemove').bind("click", function(){
+        var me  = $(this);
+        var title = me.attr('title');
+
+        if(!$('body').data('mbPopup')) {
+            $("body").mbPopup();
+            $("body").mbPopup('showModal',
+                {
+                    title:"Confirm delete",
+                    content:"Confirm delete - " + title + "?"
+                },
+                function(){
+                    $.ajax({
+                        url: me.attr('data-url'),
+                        data : {'id': me.attr('data-id')},
+                        type: 'POST',
+                        success: function(data) {
+                            window.location.reload();
+                        }
+                    });
+                });
+        }
+        return false;
+    });
+});
