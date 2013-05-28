b6978454443452e4c9fd3204dc2ffe02dead5b88
Stefan Winkelmann <stefan.winkelmann@wheregroup.com>
2013-05-28 17:34:07 +0200

    WMSBundle: Unique layer ids for YAML layers

--- src/Mapbender/WmsBundle/Entity/WmsInstance.php
+++ src/Mapbender/WmsBundle/Entity/WmsInstance.php
@@ -203,7 +203,7 @@ class WmsInstance extends SourceInstance
                     $layersource->addStyle($style);
                 }
                 $layer->setTitle($layerDef["title"])
-                        ->setId($num)
+                        ->setId($this->getId() . '-' . $num)
                         ->setSelected(!isset($layerDef["visible"]) ? false : $layerDef["visible"])
                         ->setInfo(!isset($layerDef["queryable"]) ? false : $layerDef["queryable"])
                         ->setParent($rootlayer)
