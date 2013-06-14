80ed5ec107c105d80f25eef7415ade47dfb3aba6
Stefan Winkelmann <stefan.winkelmann@wheregroup.com>
2013-06-14 14:58:41 +0200

    scaledisplay added

---
+++ src/Mapbender/CoreBundle/Element/ScaleDisplay.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Mapbender\CoreBundle\Element;
+
+use Mapbender\CoreBundle\Component\Element;
+
+/**
+ * Map's overview element
+ *
+ * @author Paul Schmidt
+ */
+class ScaleDisplay extends Element
+{
+
+    /**
+     * @inheritdoc
+     */
+    static public function getClassTitle()
+    {
+        return "ScaleDisplay";
+    }
+
+    /**
+     * @inheritdoc
+     */
+    static public function getClassDescription()
+    {
+        return "ScaleDisplay";
+    }
+
+    /**
+     * @inheritdoc
+     */
+    static public function getClassTags()
+    {
+        return array('ScaleDisplay', "Map's scale display");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDefaultConfiguration()
+    {
+        return array(
+            'title' => 'Scale Display',
+            'target' => null,
+            'anchor' => 'right-bottom');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWidgetName()
+    {
+        return 'mapbender.mbScaledisplay';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getType()
+    {
+        return 'Mapbender\CoreBundle\Element\Type\ScaleDisplayAdminType';
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public static function getFormTemplate()
+    {
+        return 'MapbenderManagerBundle:Element:scaledisplay.html.twig';
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function getAssets()
+    {
+        return array(
+            'js' => array('mapbender.element.scaledisplay.js'),
+            'css' => array());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function render()
+    {
+        return $this->container->get('templating')
+                        ->render('MapbenderCoreBundle:Element:scaledisplay.html.twig',
+                                 array(
+                            'id' => $this->getId(),
+                            "title" => $this->getTitle(),
+                            'configuration' => $this->getConfiguration()));
+    }
+
+}
+
---
+++ src/Mapbender/CoreBundle/Element/Type/ScaleDisplayAdminType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Mapbender\CoreBundle\Element\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Mapbender\CoreBundle\Form\Type\PositionType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * 
+ */
+class ScaleDisplayAdminType extends AbstractType
+{
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'scaledisplay';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'application' => null
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $app = $options['application'];
+        $layersets = array();
+        foreach($app->getLayersets() as $layerset)
+        {
+            $layersets[$layerset->getId()] = $layerset->getTitle();
+        }
+        
+        $builder->add('target', 'target_element',
+                      array(
+                    'element_class' => 'Mapbender\\CoreBundle\\Element\\Map',
+                    'application' => $options['application'],
+                    'property_path' => '[target]',
+                    'required' => false))
+                ->add('anchor', "choice",
+                      array(
+                    'required' => true,
+                    "choices" => array(
+                        'left-top' => 'left-top',
+                        'left-bottom' => 'left-bottom',
+                        'right-top' => 'right-top',
+                        'right-bottom' => 'right-bottom')));
+    }
+
+}
