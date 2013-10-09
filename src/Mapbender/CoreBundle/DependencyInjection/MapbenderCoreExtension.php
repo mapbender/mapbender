22afec832d6ae1a384f997c36be1ba2c134a69c9
Christian Wygoda <christian.wygoda@wheregroup.com>
2013-10-09 12:08:42 +0200

    Multiple Mapbender applications.
    Instead of having one massive mapbender.yml you can now add multiple
    YAML files in the app/config/applications directory. Each YAML in
    there must have a top-level "parameters" key and therefore can
    list any parameter desired, but especially the "applications"
    parameter. The MapbenderCoreExtension takes care of loading, merging
    and setting the final parameters in the container.
    
    Right now these files are not monitored by the cache invalidation
    in the dev mode, so clear the cache manually or touch the config.yml

--- src/Mapbender/CoreBundle/DependencyInjection/MapbenderCoreExtension.php
+++ src/Mapbender/CoreBundle/DependencyInjection/MapbenderCoreExtension.php
@@ -6,6 +6,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class MapbenderCoreExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
@@ -26,6 +29,44 @@ class MapbenderCoreExtension extends Extension {
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $this->loadYamlApplications($container);
+    }
+
+    private function loadYamlApplications(ContainerBuilder $container)
+    {
+        $configDir = $container->getParameter('kernel.root_dir') . '/config';
+        $finder = new Finder();
+        $finder2 = new Finder();
+
+        $finder
+            ->in($configDir)
+            ->files()->name('mapbender.yml');
+
+        if(is_dir($configDir . '/applications')) {
+            $finder2
+                ->in($configDir . '/applications')
+                ->files()->name('*.yml');
+
+            $finder->append($finder2);
+        }
+
+        foreach($finder as $file) {
+            $yml = YAML::parse($file->getRealPath());
+            if(array_key_exists('parameters', $yml) && is_array($yml['parameters'])) {
+                foreach($yml['parameters'] as $key => $data) {
+                    if($container->hasParameter($key)) {
+                        $ante = $container->getParameter($key);
+                        $container->setParameter(
+                            $key,
+                            array_merge_recursive($ante, $data));
+                    } else {
+                        $container->setParameter($key, $data);
+                    }
+                }
+            }
+        }
+        $container->getParameterBag()->resolve();
     }
 
     public function getAlias() {
