578b223fb27caf716f9f04e561d4ad8bc1bedec0
Paul Schmidt <paul.schmidt@wheregroup.com>
2013-12-11 18:42:02 +0100

    wmsbundle translation changed

--- src/Mapbender/WmsBundle/Resources/public/mapbender.source.wms.js
+++ src/Mapbender/WmsBundle/Resources/public/mapbender.source.wms.js
@@ -306,13 +306,13 @@ $.extend(true, Mapbender, {
                     url: Mapbender.configuration.application.urls.proxy + "?url=" + encodeURIComponent(self._removeProxy(imgEl.attr('src'))),
                     success: function(message, text, response){
                         if(typeof(response.responseText) === "string"){
-                            var details = "The map cannot be displayed.";
+                            var details = Mapbender.trans("mb.wms.source.image_error.datails");
                             var layerTree;
                             try{
                                 layerTree = new OpenLayers.Format.WMSCapabilities().read(response.responseText);
                             }catch(e){
                                 layerTree = null;
-                                details += ".\n" + "Exception" + ": " + e.toString();
+                                details += ".\n" + Mapbender.trans("mb.wms.source.image_error.exception",{'exception': e.toString()});
                             }
                             if(layerTree && layerTree.error){
                                 if(layerTree.error.exceptionReport && layerTree.error.exceptionReport.exceptions){
@@ -331,14 +331,14 @@ $.extend(true, Mapbender, {
                         loadError.details = details;
                     },
                     error: function(err){
-                        var details = "The map cannot be displayed.";
+                        var details = Mapbender.trans("mb.wms.source.image_error.datails");
                         if(err.status == 200){
                             var capabilities;
                             try{
                                 capabilities = new OpenLayers.Format.WMSCapabilities().read(err.responseText);
                             }catch(e){
                                 capabilities = null;
-                                details += ".\n" + "Exception" + ": " + e.toString();
+                                details += ".\n" + Mapbender.trans("mb.wms.source.image_error.exception",{'exception': e.toString()});
                             }
                             if(capabilities && capabilities.error){
                                 if(capabilities.error.exceptionReport && capabilities.error.exceptionReport.exceptions){
@@ -359,7 +359,7 @@ $.extend(true, Mapbender, {
                                 }
                             }
                         }else{
-                            details += ".\n" + "HTTP status code" + ": " + err.status + " - " + err.statusText;
+                            details += ".\n" + Mapbender.trans("mb.wms.source.image_error.statuscode") + ": " + err.status + " - " + err.statusText;
                         }
                         loadError.details = details;
                     }
