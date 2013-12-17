8954b20ef624ef568b0dd5b6c9a753c4a9037651
Paul Schmidt <paul.schmidt@wheregroup.com>
2013-12-17 10:01:09 +0100

    tamplates: Classic,Fullscreen region.html.twig added

--- src/Mapbender/CoreBundle/Template/Classic.php
+++ src/Mapbender/CoreBundle/Template/Classic.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Mapbender\CoreBundle\Template;
 
 use Mapbender\CoreBundle\Component\Template;
@@ -27,7 +26,7 @@ class Classic extends Template
         $assets = array(
             'css' => array('@FOMCoreBundle/Resources/public/css/frontend/classic.css'),
             'js' => array('@FOMCoreBundle/Resources/public/js/widgets/popup.js',
-                          '@FOMCoreBundle/Resources/public/js/frontend/sidepane.js'),
+                '@FOMCoreBundle/Resources/public/js/frontend/sidepane.js'),
             'trans' => array()
         );
 
@@ -46,16 +45,21 @@ class Classic extends Template
      * @inheritdoc
      */
     public function render($format = 'html', $html = true, $css = true,
-            $js = true)
+        $js = true)
     {
+        $region_props = $this->application->getEntity()->getNamedRegionProperties();
+        $default_region_props = $this->getRegionsProperties();
+
         $templating = $this->container->get('templating');
         return $templating
-                        ->render('MapbenderCoreBundle:Template:classic.html.twig',
-                                 array(
-                            'html' => $html,
-                            'css' => $css,
-                            'js' => $js,
-                            'application' => $this->application));
+                ->render('MapbenderCoreBundle:Template:classic.html.twig',
+                    array(
+                    'html' => $html,
+                    'css' => $css,
+                    'js' => $js,
+                    'application' => $this->application,
+                    'region_props' => $region_props,
+                    'default_region_props' => $default_region_props));
     }
 
 }
