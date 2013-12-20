b6ebca69589b24738040a66d42de3e50773eb1b9
Paul Schmidt <paul.schmidt@wheregroup.com>
2013-12-20 10:59:33 +0100

    sort of element list changed

--- src/Mapbender/ManagerBundle/Controller/ElementController.php
+++ src/Mapbender/ManagerBundle/Controller/ElementController.php
@@ -47,6 +47,7 @@ class ElementController extends Controller
                 'description' => $trans->trans($elementClassName::getClassDescription()),
                 'tags' => $tags);
         }
+        ksort($elements);
         return array(
             'elements' => $elements,
             'region' => $this->get('request')->get('region'));
