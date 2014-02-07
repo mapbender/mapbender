0a786a4f23fab7be7a275e048e459c1db9b33f5f
Vadim Hermann <vhermann@wheregroup.com>
2014-02-07 10:22:12 +0100

    layertree context menu slider

--- src/Mapbender/CoreBundle/Element/Layertree.php
+++ src/Mapbender/CoreBundle/Element/Layertree.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * 
+ *
  */
 class Layertree extends Element
 {
@@ -63,12 +63,14 @@ class Layertree extends Element
         $assets = array(
             'js' => array(
                 '@FOMCoreBundle/Resources/public/js/widgets/popup.js',
+                '@FOMCoreBundle/Resources/public/js/dragdealer.min.js',
+                '@FOMCoreBundle/Resources/public/js/layertree.js',
                 '@FOMCoreBundle/Resources/public/js/widgets/checkbox.js'),
             'css' => array(),
             'trans' => array('MapbenderCoreBundle:Element:layertree.json.twig')
         );
         $config = parent::getConfiguration();
-        if (true) //@TODO 
+        if (true) //@TODO
                 $assets["js"][] = 'mapbender.element.layertree.tree.js';
         else if (isset($config["displaytype"]) && $config["displaytype"] === "list")
                 $assets["js"][] = 'mapbender.element.layertree.list.js';
