ccac0712efd1f7f00246c74e7826e4bd2cab8705
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2014-06-03 18:07:07 +0200

    make log actually visible

--- src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
@@ -9,7 +9,7 @@ class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
     public function setUp() {
         $this->setHost('localhost');
         $this->setPort(9876);
-        $this->setBrowserUrl('http://localhost:8000');
+        $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_test.php/');
     }
 
     public function prepareSession() {
@@ -19,7 +19,7 @@ class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
     }
 
     public function testIndex() {
-        $this->url('http://localhost:8000');
+        $this->url('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_test.php/');
         $this->assertEquals('Applications', $this->title());
     }
 }
