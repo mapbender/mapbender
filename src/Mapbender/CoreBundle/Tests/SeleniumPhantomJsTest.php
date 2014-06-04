a6ac6d5a6abeb889650034ad7cd0793574aa6c76
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2014-06-04 10:35:55 +0200

    try to skip test in setup for PHP 5.3

--- src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
@@ -5,6 +5,10 @@ namespace Mapbender\CoreBundle\Tests\Controller;
 class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
 
     public function setUp() {
+        if(PHP_MINOR_VERSION == 3) {
+            $this->markTestIncomplete('This test does not run on PHP 5.3.');
+            return;
+        }
         try {
             $this->setHost('localhost');
             $this->setPort(9876);
