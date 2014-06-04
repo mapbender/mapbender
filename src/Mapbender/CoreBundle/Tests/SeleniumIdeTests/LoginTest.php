ca4a8170db987bfe82bba2c273d9ce44721e6411
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2014-06-04 11:50:28 +0200

    added a new test case using selenium, trying to connect to sauce labs

---
+++ src/Mapbender/CoreBundle/Tests/SeleniumIdeTests/LoginTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Mapbender\CoreBundle\Tests\SeleniumIdeTests;
+
+class LoginTest extends \PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+      $this->setHost('localhost');
+      $this->setPort(4445);
+      $this->setBrowser("*chrome");
+      $this->getDriver('*chrome')->setWebDriverCapabilities(array('tunnel-identifier' => getenv('TRAVIS_JOB_NUMBER')));
+      $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
+  }
+
+  public function testMyTestCase()
+  {
+    $this->open("/");
+    $this->click("//a[contains(@href, '/user/login')]");
+    $this->waitForPageToLoad("3000");
+    $this->type("id=username", "root");
+    $this->type("id=password", "foobar123");
+    $this->click("css=input.right.button");
+    $this->waitForPageToLoad("3000");
+  }
+}
+?>
--- src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
@@ -9,43 +9,19 @@ class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
             $this->markTestIncomplete('This test does not run on PHP 5.3.');
             return;
         }
-        try {
-            $this->setHost('localhost');
-            $this->setPort(9876);
-            $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
-        }catch(\Exception $e) {
-            // skip test on PHP 5.3
-            if(PHP_MINOR_VERSION == 3) {
-                return;
-            }
-            throw $e;
-        }
+        $this->setHost('localhost');
+        $this->setPort(9876);
+        $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
     }
 
     public function prepareSession() {
-        try{
-            $res = parent::prepareSession();
-            $this->url('/');
-            return $res;
-        }catch(\Exception $e) {
-            // skip test on PHP 5.3
-            if(PHP_MINOR_VERSION == 3) {
-                return;
-            }
-            throw $e;
-        }
+        $res = parent::prepareSession();
+        $this->url('/');
+        return $res;
     }
 
     public function testIndex() {
-        try {
-            $this->url('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
-            $this->assertEquals('Applications', $this->title());
-        }catch(\PHPUnit_Extensions_Selenium2TestCase_WebDriverException $e) {
-            // skip test on PHP 5.3
-            if(PHP_MINOR_VERSION == 3) {
-                return;
-            }
-            throw $e;
-        }
+        $this->url('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
+        $this->assertEquals('Applications', $this->title());
     }
 }
