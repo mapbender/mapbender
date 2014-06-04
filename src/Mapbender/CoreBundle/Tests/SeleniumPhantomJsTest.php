15f794c32f79a639703300686b3652934b805dd7
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2014-06-04 10:00:06 +0200

    fix PHP 5.3 workaround

--- src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
@@ -5,15 +5,31 @@ namespace Mapbender\CoreBundle\Tests\Controller;
 class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
 
     public function setUp() {
-        $this->setHost('localhost');
-        $this->setPort(9876);
-        $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
+        try {
+            $this->setHost('localhost');
+            $this->setPort(9876);
+            $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
+        }catch(\Exception $e) {
+            // skip test on PHP 5.3
+            if(PHP_MINOR_VERSION == 3) {
+                return;
+            }
+            throw $e;
+        }
     }
 
     public function prepareSession() {
-        $res = parent::prepareSession();
-        $this->url('/');
-        return $res;
+        try{
+            $res = parent::prepareSession();
+            $this->url('/');
+            return $res;
+        }catch(\Exception $e) {
+            // skip test on PHP 5.3
+            if(PHP_MINOR_VERSION == 3) {
+                return;
+            }
+            throw $e;
+        }
     }
 
     public function testIndex() {
