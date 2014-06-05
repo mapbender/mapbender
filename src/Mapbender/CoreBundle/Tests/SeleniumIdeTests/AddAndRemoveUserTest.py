2f9b5e5b4c296cd53904065d43eeee3d0b9b7c55
uli.rothstein <uli.rothstein@wheregroup.com>
2014-06-05 14:03:19 +0200

    switch to python

---
+++ src/Mapbender/CoreBundle/Tests/SeleniumIdeTests/AddAndRemoveUserTest.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.phantomjs.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+success = True
+wd = WebDriver()
+wd.implicitly_wait(60)
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+try:
+    wd.get("http://localhost/data/mapbender-starter/application/web/app_dev.php/")
+    wd.find_element_by_link_text("Login").click()
+    wd.find_element_by_id("username").click()
+    wd.find_element_by_id("username").clear()
+    wd.find_element_by_id("username").send_keys("root")
+    wd.find_element_by_id("password").click()
+    wd.find_element_by_id("password").clear()
+    wd.find_element_by_id("password").send_keys("root")
+    wd.find_element_by_css_selector("input.right.button").click()
+    wd.find_element_by_css_selector("h1.contentTitle").click()
+    wd.find_element_by_link_text("New user").click()
+    wd.find_element_by_id("user_username").click()
+    wd.find_element_by_id("user_username").clear()
+    wd.find_element_by_id("user_username").send_keys("test")
+    wd.find_element_by_id("user_email").click()
+    wd.find_element_by_id("user_email").clear()
+    wd.find_element_by_id("user_email").send_keys("testing@example.com")
+    wd.find_element_by_id("user_password_first").click()
+    wd.find_element_by_id("user_password_first").clear()
+    wd.find_element_by_id("user_password_first").send_keys("test1234")
+    wd.find_element_by_id("user_password_second").click()
+    wd.find_element_by_id("user_password_second").clear()
+    wd.find_element_by_id("user_password_second").send_keys("test1234")
+    wd.find_element_by_css_selector("input.button").click()
+    wd.find_element_by_css_selector("span.iconRemove.iconSmall").click()
+    wd.find_element_by_link_text("Delete").click()
+    wd.save_screenshot("./test.png")
+finally:
+    wd.save_screenshot("./error.png")
+    wd.quit()
+    if not success:
+        raise Exception("Test failed.")
