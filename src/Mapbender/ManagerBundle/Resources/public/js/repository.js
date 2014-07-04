9dff1fc8e0cf47e8bd33ddbb1d3874621d96bc1a
Paul Schmidt <paul.schmidt@wheregroup.com>
2014-07-04 15:34:03 +0200

    instance layers sort fixed

--- src/Mapbender/ManagerBundle/Resources/public/js/repository.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/repository.js
@@ -1,18 +1,18 @@
-$(function(){
-    var showInfoBox = function(){
+$(function() {
+    var showInfoBox = function() {
         var me = $(this);
         var infoBox = me.find(".infoMsgBox");
 
-        if(infoBox.hasClass("hide")){
+        if (infoBox.hasClass("hide")) {
             $(".infoMsgBox").addClass("hide");
             infoBox.removeClass("hide");
-        }else{
+        } else {
             infoBox.addClass("hide");
         }
         return false;
     }
 
-    function setRootState(className){
+    function setRootState(className) {
         var root = $("#" + className);
         var column = $("#instanceTableCheckBody").find("[data-check-identifier=" + className + "]")
         var rowCount = column.find(".checkWrapper:not(.checkboxDisabled)").length;
@@ -20,16 +20,16 @@ $(function(){
 
         root.removeClass("iconCheckboxActive").removeClass("iconCheckboxHalf");
 
-        if(rowCount == checkedCount){
+        if (rowCount == checkedCount) {
             root.addClass("iconCheckboxActive");
-        }else if(checkedCount == 0){
+        } else if (checkedCount == 0) {
             // do nothing!
-        }else{
+        } else {
             root.addClass("iconCheckboxHalf");
         }
     }
     // toggle all permissions
-    var toggleAllStates = function(){
+    var toggleAllStates = function() {
         var self = $(this);
         var className = self.attr("id");
         var checkElements = $(".checkboxColumn[data-check-identifier=" + className + "]");
@@ -37,13 +37,13 @@ $(function(){
         var me;
 
         // change all tagboxes with the same permission type
-        checkElements.find(".checkbox:not(:disabled)").each(function(i, e){
+        checkElements.find(".checkbox:not(:disabled)").each(function(i, e) {
             me = $(e);
             me.get(0).checked = state;
 
-            if(state){
+            if (state) {
                 me.parent().addClass("iconCheckboxActive");
-            }else{
+            } else {
                 me.parent().removeClass("iconCheckboxActive");
             }
         });
@@ -52,8 +52,8 @@ $(function(){
         setRootState(className);
     }
     // init permission root state
-    var initRootState = function(){
-        $(this).find(".iconCheckbox").each(function(){
+    var initRootState = function() {
+        $(this).find(".iconCheckbox").each(function() {
             setRootState($(this).attr("id"));
             $(this).bind("click", toggleAllStates);
         });
@@ -61,92 +61,72 @@ $(function(){
     $("#instanceTableCheckHead").one("load", initRootState).load();
 
     // toggle permission Event
-    var toggleState = function(){
+    var toggleState = function() {
         setRootState($(this).parent().attr("data-check-identifier"));
     };
-    $('tr[data-type="root"], tr[data-type="node"]', $('.instanceTable tbody')).each(function(){
-        var that = $(this),
-                id = $(this).attr("data-id"),
-                children = [],
-                start_pos = null,
-                stop_pos = null;
-        var changePosition = function(options, idx, reverse){
-            $.ajax({
-                'url': options[idx]['url'],
-                'type': "POST",
-                'data': {
-                    'number': options[idx]['idx'],
-                    'id': options[idx]['id']
-                },
-                success: function(data, textStatus, jqXHR){
-                    if(data.error && data.error !== ''){
-                        document.location.href = document.location.href;
-                    }else{
-                        var elm = $('.instanceTable tbody tr[data-id="' + options[idx]['id'] + '"]');
-                        if(reverse){
-                            if(options.length - 1 !== idx){
-                                elm.insertAfter($('.instanceTable tbody tr[data-id="' + options[options.length - 1]['id'] + '"]'));
-                            }
-                        }else{
-                            if(0 !== idx){
-                                elm.insertAfter($('.instanceTable tbody tr[data-id="' + options[idx - 1]['id'] + '"]'));
-                            }
-                        }
-                        if(idx < options.length - 1)
-                            changePosition(options, idx + 1, reverse);
-                    }
-                },
-                error: function(jqXHR, textStatus, errorThrown){
-                    document.location.href = document.location.href;
-                }
-            });
-        };
-
+    function resetLayerPriority() {
+        $('tr:not(.dummy) .layer-priority input[type="hidden"]', $('.instanceTable tbody')).each(function(idx, item) {
+            $(item).val(idx).parents('tr:first').attr("data-priority", idx);
+        });
+    }
+    resetLayerPriority();
+    $('tr[data-type="root"], tr[data-type="node"]', $('.instanceTable tbody')).each(function() {
+        var id = $(this).attr("data-id");
+        var children = [];
+        var start_pos = null;
+        var stop_pos = null;
         $('.instanceTable tbody').sortable({
             cursor: 'move',
             axis: 'y',
             items: 'tr:not(.root,[data-parent="' + id + '"])',
             distance: 6,
             containment: 'parent',
-            start: function(event, ui){
-                if($(ui.item).hasClass('root') || $(ui.item).hasClass('dummy'))
+            start: function(event, ui) {
+                if ($(ui.item).hasClass('root') || $(ui.item).hasClass('dummy'))
                     return false;
                 var subs = $('.instanceTable tbody tr[data-parent="' + $(ui.item).attr('data-id') + '"]');
                 children = [];
                 start_pos = $(ui.item).index() - $(ui.item).prevAll('.header').length;
                 stop_pos = null;
-                if(subs.length > 0){
+                if (subs.length > 0) {
                     var nextAll = $(ui.item).nextAll('[data-id]');
-                    for(var i = 0; i < nextAll.length; i++){
+                    for (var i = 0; i < nextAll.length; i++) {
                         var tmp = $(nextAll.get(i));
-                        if(tmp.attr('data-parent') === $(ui.item).attr('data-parent')){
+                        if (tmp.attr('data-parent') === $(ui.item).attr('data-parent')) {
                             break;
                         }
-                        children.push({'id': tmp.attr('data-id'), 'url': tmp.attr('data-href')});
+                        children.push(tmp.attr('id'));
                     }
                 }
             },
-            stop: function(event, ui){
-                var item = $(ui.item),
-                        stop_pos = item.index() - item.prevAll('.header').length,
-                        reverse = stop_pos > start_pos;
-                var main_pos = reverse ? stop_pos - children.length : stop_pos;
-                var toChange = [{'url': item.attr("data-href"), 'idx': main_pos, 'id': item.attr("data-id")}];//, 'children': children};
-                $.each(children, function(i, item){
-                    toChange.push({'url': item.url, 'idx': (main_pos + 1 + i), 'id': item.id});
-                });
-                if(reverse)
-                    toChange.reverse();
-                if(item.prev().length > 0 && $(item.prev().get(0)).attr("data-parent") === item.attr("data-parent")){
-                    if(item.next().length > 0 && $(item.next().get(0)).attr("data-parent") === $(item.prev().get(0)).attr("data-id")){
+            stop: function(event, ui) {
+                var item = $(ui.item);
+                if (item.prev().length > 0 && $(item.prev().get(0)).attr("data-parent") === item.attr("data-parent")) {
+                    if (item.next().length > 0 && $(item.next().get(0)).attr("data-parent") === $(item.prev().get(0)).attr("data-id")) {
                         return false;
                     }
-                    changePosition(toChange, 0, reverse);
+                    if(children.length > 0){
+                        var elm = item;
+                        $.each(children, function(idx, item){
+                            var mel = $('#'+item).remove();
+                            mel.insertAfter(elm);
+                            elm = mel;
+                        });
+                    }
+                    resetLayerPriority();
                     return true;
-                }else if(item.next().length > 0 && $(item.next().get(0)).attr("data-parent") === item.attr("data-parent")){
-                    changePosition(toChange, 0, reverse);
+                } else if (item.next().length > 0 && $(item.next().get(0)).attr("data-parent") === item.attr("data-parent")) {
+                    if(children.length > 0){
+                        var elm = item;
+                        $.each(children, function(idx, item){
+                            var mel = $('#'+item).remove();
+                            mel.insertAfter(elm);
+                            elm = mel;
+                        });
+                    }
+                    resetLayerPriority();
                     return true;
-                }else{
+                } else {
                     return false;
                 }
             }
--- src/Mapbender/WmsBundle/Controller/RepositoryController.php
+++ src/Mapbender/WmsBundle/Controller/RepositoryController.php
@@ -284,6 +284,7 @@ class RepositoryController extends Controller
                                 $layer_tmp->getInfo(), true));
                         $layer->setAllowinfo(Utils::getBool(
                                 $layer_tmp->getAllowinfo(), true));
+                        $layer->setPriority($layer_tmp->getPriority());
                         break;
                     }
                 }
