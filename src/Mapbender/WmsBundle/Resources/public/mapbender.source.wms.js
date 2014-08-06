539cde16008cc8b7b54e477d299a4fdcb772560f
Christian Wygoda <christian.wygoda@wheregroup.com>
2014-08-06 15:49:19 +0200

    Fix _removeProxy for WMS URLs

--- src/Mapbender/WmsBundle/Resources/public/mapbender.source.wms.js
+++ src/Mapbender/WmsBundle/Resources/public/mapbender.source.wms.js
@@ -73,7 +73,7 @@ $.extend(true, Mapbender, {
             },
             _removeProxy: function(url){
                 if(url.indexOf(OpenLayers.ProxyHost) === 0){
-                    return url.substring(OpenLayers.ProxyHost.length);
+                    return decodeURIComponent(url.substring(OpenLayers.ProxyHost.length));
                 }
                 return url;
             },
