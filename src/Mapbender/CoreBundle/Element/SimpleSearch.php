mapbender/mapbender

commit f6884b4599b4292bf8f360e1dc8e3ce768ec9512

migration to Symfony2.3: form parameter 'auto_initialize' => false added, controller 'forward' with 'subrequest' replaced

src/Mapbender/CoreBundle/Element/SimpleSearch.php
@@ -3,6 +3,7 @@ namespace Mapbender\CoreBundle\Element;
 
 use Mapbender\CoreBundle\Component\Element;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
  * Simple Search - Just type, select and show result
@@ -119,12 +120,13 @@ class SimpleSearch extends Element
         $url .= (false === strpos($url, '?') ? '?' : '&');
         $url .= $configuration['query_key'] . '=' . sprintf($qf, $q);
 
-        // Execute query
-        $response = $this->container->get('http_kernel')
-            ->forward('OwsProxy3CoreBundle:OwsProxy:genericProxy', array(
-                'url' => $url
-            ));
-
+        $path = array(
+            '_controller' => 'OwsProxy3CoreBundle:OwsProxy:genericProxy',
+            'url' => $url
+        );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
         // Dive into result JSON if needed (Solr for example 'response.docs')
         if('' !== $configuration['collection_path']) {
             $data = json_decode($response->getContent(), true);

src/Mapbender/CoreBundle/Element/PrintClient.php
@@ -3,6 +3,7 @@ namespace Mapbender\CoreBundle\Element;
 
 use Mapbender\CoreBundle\Component\Element;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Mapbender\PrintBundle\Component\OdgParser;
 
 /**
@@ -214,13 +215,14 @@ class PrintClient extends Element
                 $url = $this->container->get('router')->generate('mapbender_print_print_service',
                     array(), true);
 
-                return $this->container->get('http_kernel')->forward(
-                        'OwsProxy3CoreBundle:OwsProxy:genericProxy',
-                        array(
-                        'url' => $url,
-                        'content' => $content
-                        )
+                $path = array(
+                    '_controller' => 'OwsProxy3CoreBundle:OwsProxy:genericProxy',
+                    'url' => $url,
+                    'content' => $content
                 );
+                $subRequest = $request->duplicate(array(), null, $path);
+                return $this->container->get('http_kernel')->handle(
+                        $subRequest, HttpKernelInterface::SUB_REQUEST);
 
             case 'queued':
 

src/Mapbender/CoreBundle/Element/Type/BaseSourceSwitcherAdminType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mapbender\CoreBundle\Element\Type;
 
 use Mapbender\CoreBundle\Form\EventListener\BaseSourceSwitcherFieldSubscriber;
@@ -10,9 +11,9 @@ use Mapbender\CoreBundle\Component\ExtendedCollection;
 /**
  * 
  */
-class BaseSourceSwitcherAdminType extends AbstractType
-    implements ExtendedCollection
+class BaseSourceSwitcherAdminType extends AbstractType implements ExtendedCollection
 {
+
     public $hasSubForm = true;
 
     public function isSubForm()
@@ -56,7 +57,7 @@ class BaseSourceSwitcherAdminType extends AbstractType
                         if (intval($mapconfig['layerset']) === $layerset->getId()) {
                             foreach ($layerset->getInstances() as $instance) {
                                 if ($instance->getEnabled() && $instance->isBaseSource())
-                                        $instList[strval($instance->getId())] = $instance->getTitle();
+                                    $instList[strval($instance->getId())] = $instance->getTitle();
                             }
                             break;
                         }
@@ -67,19 +68,18 @@ class BaseSourceSwitcherAdminType extends AbstractType
         }
         $builder->add('title', 'text', array('required' => true))
             ->add('tooltip', 'text', array('required' => false))
-            ->add('target', 'target_element',
-                array(
+            ->add('target', 'target_element', array(
                 'element_class' => 'Mapbender\\CoreBundle\\Element\\Map',
                 'application' => $application,
                 'property_path' => '[target]',
                 'required' => false));
         if ($element !== null && $element->getId() !== null) {
-            $builder->add('sourcesets', "collection",
-                array(
+            $builder->add('sourcesets', "collection", array(
                 'property_path' => '[sourcesets]',
                 'type' => new SourceSetAdminType(),
                 'allow_add' => true,
                 'allow_delete' => true,
+                'auto_initialize' => false,
                 'options' => array(
                     'sources' => $instList
             )));

src/Mapbender/CoreBundle/Element/Type/SearchRouterAdminType.php
@@ -45,7 +45,8 @@ class SearchRouterAdminType extends AbstractType
             ->add($builder->create('routes', 'collection', array(
                 'type' => new SearchRouterRouteAdminType(),
                 'allow_add' => true,
-                'allow_delete' => true
+                'allow_delete' => true,
+                'auto_initialize' => false,
             ))->addViewTransformer(new SearchRouterRouteTransformer()));
     }
 

src/Mapbender/CoreBundle/Form/EventListener/PrintClientSubscriber.php
@@ -21,7 +21,7 @@ class PrintClientSubscriber implements EventSubscriberInterface
      * @var \Symfony\Component\Form\FormFactoryInterface 
      */
     private $factory;
-    
+
     /**
      * The application
      * 
@@ -59,37 +59,32 @@ class PrintClientSubscriber implements EventSubscriberInterface
     {
         $data = $event->getData();
         $form = $event->getForm();
-        if(null === $data)
-        {
+        if (null === $data) {
             return;
         }
-        if(key_exists("scales", $data) && is_string($data["scales"]))
-        {
+        if (key_exists("scales", $data) && is_string($data["scales"])) {
             $data["scales"] = preg_split("/\s?,\s?/", $data["scales"]);
             $event->setData($data);
         }
-        
-        if(key_exists("templates", $data) )
-        {
-            $form->add($this->factory->createNamed(
-                                'templates', "collection", null,
-                                array(
-                            'property_path' => '[templates]',
-                            'type' => new PrintClientTemplateAdminType(),
-                            'options' => array(
-                                ))));
-        }
-        if(key_exists("quality_levels", $data) )
-        {
-            $form->add($this->factory->createNamed(
-                                'quality_levels', "collection", null,
-                                array(
-                            'property_path' => '[quality_levels]',
-                            'type' => new PrintClientQualityAdminType(),
-                            'options' => array(
-                                ))));
-        }
 
+        if (key_exists("templates", $data)) {
+            $form->add($this->factory->createNamed(
+                    'templates', "collection", null, array(
+                    'property_path' => '[templates]',
+                    'auto_initialize' => false,
+                    'type' => new PrintClientTemplateAdminType(),
+                    'options' => array(
+            ))));
+        }
+        if (key_exists("quality_levels", $data)) {
+            $form->add($this->factory->createNamed(
+                    'quality_levels', "collection", null, array(
+                    'property_path' => '[quality_levels]',
+                    'auto_initialize' => false,
+                    'type' => new PrintClientQualityAdminType(),
+                    'options' => array(
+            ))));
+        }
     }
 
     /**
@@ -101,39 +96,34 @@ class PrintClientSubscriber implements EventSubscriberInterface
     {
         $data = $event->getData();
         $form = $event->getForm();
-        if(null === $data)
-        {
+        if (null === $data) {
             return;
         }
 
-        if(key_exists("scales", $data) && is_array($data["scales"]))
-        {
+        if (key_exists("scales", $data) && is_array($data["scales"])) {
             $data["scales"] = implode(",", $data["scales"]);
             $event->setData($data);
         }
-        
-        if(key_exists("templates", $data) )
-        {
+
+        if (key_exists("templates", $data)) {
             $form->add($this->factory->createNamed(
-                                'templates', "collection", null,
-                                array(
-                            'property_path' => '[templates]',
-                            'type' => new PrintClientTemplateAdminType(),
-                            'options' => array(
-                                ))));
+                    'templates', "collection", null, array(
+                    'property_path' => '[templates]',
+                    'auto_initialize' => false,
+                    'type' => new PrintClientTemplateAdminType(),
+                    'options' => array(
+            ))));
         }
-        
-        if(key_exists("quality_levels", $data) )
-        {
+
+        if (key_exists("quality_levels", $data)) {
             $form->add($this->factory->createNamed(
-                                'quality_levels', "collection", null,
-                                array(
-                            'property_path' => '[quality_levels]',
-                            'type' => new PrintClientQualityAdminType(),
-                            'options' => array(
-                                ))));
+                    'quality_levels', "collection", null, array(
+                    'property_path' => '[quality_levels]',
+                    'auto_initialize' => false,
+                    'type' => new PrintClientQualityAdminType(),
+                    'options' => array(
+            ))));
         }
-        
     }
 
 }

src/Mapbender/ManagerBundle/Controller/ApplicationController.php
@@ -14,6 +14,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Mapbender\CoreBundle\Entity\Application;
 use Mapbender\ManagerBundle\Form\Type\ApplicationCopyType;
 use Mapbender\ManagerBundle\Form\Type\ApplicationType;
@@ -730,8 +731,14 @@ class ApplicationController extends Controller
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$sourceInst->getSource()->getManagertype()];
 
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:deleteInstance", array("slug" => $slug, "instanceId" => $instanceId));
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:deleteInstance",
+            "slug" => $slug,
+            "instanceId" => $instanceId
+        );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /* Instance block end */

src/Mapbender/ManagerBundle/Controller/RepositoryController.php
@@ -12,6 +12,7 @@ use FOM\ManagerBundle\Configuration\Route as ManagerRoute;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Mapbender\WmsBundle\Entity\WmsSource;
 use Mapbender\CoreBundle\Entity\Source;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
@@ -96,12 +97,11 @@ class RepositoryController extends Controller
 
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$managertype];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:create", array()
-        );
-//        return array(
-//            'managers' => $managers
-//        );
+        
+        $path = array('_controller' => $manager['bundle'] . ":" . "Repository:create");
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /**
@@ -115,10 +115,13 @@ class RepositoryController extends Controller
                 ->getRepository("MapbenderCoreBundle:Source")->find($sourceId);
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$source->getManagertype()];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:view",
-                array("id" => $source->getId())
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:view",
+            "id" => $source->getId()
         );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /**
@@ -158,10 +161,14 @@ class RepositoryController extends Controller
 
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$source->getManagertype()];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:delete",
-                array("sourceId" => $source->getId())
+
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:delete",
+            "sourceId" => $source->getId()
         );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /**
@@ -185,10 +192,15 @@ class RepositoryController extends Controller
 
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$sourceInst->getManagertype()];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:instance",
-                array("slug" => $slug, "instanceId" => $sourceInst->getId())
+        
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:instance",
+            "slug" => $slug,
+            "instanceId" => $sourceInst->getId()
         );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /**
@@ -322,12 +334,16 @@ class RepositoryController extends Controller
             ->find($instanceId);
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$sourceInst->getManagertype()];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:instanceenabled",
-                array("slug" => $slug,
-                "layersetId" => $layersetId,
-                "instanceId" => $sourceInst->getId(),)
+
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:instanceenabled",
+            "slug" => $slug,
+            "layersetId" => $layersetId,
+            "instanceId" => $sourceInst->getId()
         );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
     /**
@@ -341,12 +357,16 @@ class RepositoryController extends Controller
             ->find($instanceId);
         $managers = $this->get('mapbender')->getRepositoryManagers();
         $manager = $managers[$sourceInst->getManagertype()];
-        return $this->forward(
-                $manager['bundle'] . ":" . "Repository:instancelayerpriority",
-                array("slug" => $slug,
-                "instanceId" => $sourceInst->getId(),
-                "instLayerId" => $instLayerId)
+        
+        $path = array(
+            '_controller' => $manager['bundle'] . ":" . "Repository:instancelayerpriority",
+            "slug" => $slug,
+            "instanceId" => $sourceInst->getId(),
+            "instLayerId" => $instLayerId
         );
+        $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
+        return $this->container->get('http_kernel')->handle(
+                $subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
 }

src/Mapbender/PrintBundle/Element/ImageExport.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Mapbender\PrintBundle\Element;
 
 use Mapbender\CoreBundle\Component\Element;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -100,8 +102,7 @@ class ImageExport extends Element
     public function render()
     {
         return $this->container->get('templating')
-                ->render('MapbenderPrintBundle:Element:imageexport.html.twig',
-                    array(
+                ->render('MapbenderPrintBundle:Element:imageexport.html.twig', array(
                     'id' => $this->getId(),
                     'title' => $this->getTitle(),
                     'configuration' => $this->getConfiguration()
@@ -118,18 +119,17 @@ class ImageExport extends Element
             case 'export':
                 $request = $this->container->get('request');
                 $data = $request->get('data');
-
                 // Forward to Printer Service URL using OWSProxy
-                $url = $this->container->get('router')->generate('mapbender_print_print_export',
-                    array(), true);
-
-                return $this->container->get('http_kernel')->forward(
-                        'OwsProxy3CoreBundle:OwsProxy:genericProxy',
-                        array(
-                        'url' => $url,
-                        'content' => $data
-                        )
+                $url = $this->container->get('router')->generate('mapbender_print_print_export', array(), true);
+                
+                $path = array(
+                    '_controller' => 'OwsProxy3CoreBundle:OwsProxy:genericProxy',
+                    'url' => $url,
+                    'content' => $data
                 );
+                $subRequest = $request->duplicate(array(), null, $path);
+                return $this->container->get('http_kernel')->handle(
+                        $subRequest, HttpKernelInterface::SUB_REQUEST);
         }
     }
 

commit df0b389d69eb3ca3b7c321eb346c484912d4cc17

Fix SimpleSearch broken in f6884b45

src/Mapbender/CoreBundle/Element/SimpleSearch.php
@@ -125,7 +125,7 @@ class SimpleSearch extends Element
             'url' => $url
         );
         $subRequest = $this->container->get('request')->duplicate(array(), null, $path);
-        return $this->container->get('http_kernel')->handle(
+        $response = $this->container->get('http_kernel')->handle(
                 $subRequest, HttpKernelInterface::SUB_REQUEST);
         // Dive into result JSON if needed (Solr for example 'response.docs')
         if('' !== $configuration['collection_path']) {