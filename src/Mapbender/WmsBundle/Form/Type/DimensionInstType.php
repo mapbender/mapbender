e63bb0d5f464ce484213e5aba71570df5ebe1670
Paul Schmidt <paul.schmidt@wheregroup.com>
2014-09-01 10:31:39 +0200

    code cleaned

--- src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
+++ src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
- * DimensionType class
+ * DimensionInstType class
  */
 class DimensionInstType extends AbstractType
 {
