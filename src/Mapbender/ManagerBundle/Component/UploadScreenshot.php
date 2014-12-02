5abe1db14d7ca8193a82c00137290101d1148a1a
Paul Schmidt <paul.schmidt@wheregroup.com>
2014-12-02 10:29:46 +0100

    merged with origin develop

--- src/Mapbender/ManagerBundle/Component/UploadScreenshot.php
+++ src/Mapbender/ManagerBundle/Component/UploadScreenshot.php
@@ -12,7 +12,7 @@ class UploadScreenshot
         $fileName = sprintf('screenshot-%d.%s', $application->getId(), $application->getScreenshotFile()->guessExtension());
         $fileExtension = strtolower($screenshotFile->guessExtension());
         $fullFilePath  = $filePath."/".$fileName;
-        $fileSize      = 500;
+        $fileSize      = 200;
 
         $screenshotFile->move($filePath,$fileName);
         $application->setScreenshot($fileName);
@@ -36,27 +36,44 @@ class UploadScreenshot
 
  private function resizeImage($source_image, $destination_width, $destination_height)
     {       
-        $width = imagesx($source_image);
-        $height = imagesy($source_image);
+//        $width = imagesx($source_image);
+//        $height = imagesy($source_image);
         
+       $source_width = imagesx($source_image);
+       $source_height = imagesy($source_image);
+       
+       if ($source_width === $destination_width && $source_height === $destination_height){
+            $destination_image = imagecreatetruecolor($destination_width,$destination_height);
+            imagealphablending($destination_image, false);
+            imagesavealpha($destination_image,true);
+            $transparent = imagecolorallocatealpha($destination_image, 255, 255, 255, 127);
+            imagefilledrectangle($destination_image, 0, 0, $destination_width, $destination_height, $transparent);
+            imagecopyresized($destination_image, $source_image, 0, 0, 0, 0, $destination_width, $destination_height, $source_width, $source_height);     
+       }else{
+           
+           $tmp_width = (int)($source_width / 100);
+           $tmp_height = (int)($source_height / 100);
+           
+           
+           $destinatin_scale = min($tmp_width,$tmp_height);
+           
+           $destination_width = $destinatin_scale * 100;
+           $destination_height = $destinatin_scale * 100;
+                      
+            
+           $dstX = (int)(($source_width-$destination_width)*0.5);
+           $dstY = (int)(($source_height-$destination_height)*0.5);
+           
+           $destination_image = imagecreatetruecolor($destination_width,$destination_height);
+           imagealphablending($destination_image, false);
+           imagesavealpha($destination_image,true);
+           $transparent = imagecolorallocatealpha($destination_image, 255, 255, 255, 127);
+           imagefilledrectangle($destination_image, 0, 0, $destination_width, $destination_height, $transparent);
+           imagecopyresampled($destination_image,$source_image,0,0,$dstX,$dstY, $source_width, $source_height,$source_width, $source_height);           
+       }
          
-        if ($height > $width)  {
-            $ratio = $destination_height / $height;
-            $newheight = $destination_height;
-            $newwidth = $width * $ratio;
-        } else {
-            $ratio = $destination_width / $width;
-            $newwidth = $destination_width;
-            $newheight = $height * $ratio;
-        }
-
-        $destination_image = imagecreatetruecolor($newwidth,$newheight);
-        imagealphablending($destination_image, false);
-        imagesavealpha($destination_image,true);
-        $transparent = imagecolorallocatealpha($destination_image, 255, 255, 255, 127);
-        imagefilledrectangle($destination_image, 0, 0, $width, $height, $transparent);
-        imagecopyresized($destination_image, $source_image, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);       
-        return $destination_image;
+          
+       return $destination_image;
     }
 }
 
