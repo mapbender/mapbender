50c94b8e250904987832e2e4ebd8a2b7f8bd1d46
Konstantin Dimow <konstantin.dimow@wheregroup.com>
2015-03-16 16:12:38 +0100

    test element update response

--- src/Mapbender/ManagerBundle/Controller/ElementController.php
+++ src/Mapbender/ManagerBundle/Controller/ElementController.php
@@ -231,6 +231,9 @@ class ElementController extends Controller
             $elComp->postSave();
             $this->get('session')->getFlashBag()->set('success',
                 'Your element has been saved.');
+            
+//            $response = new Response('', 205);
+//            $response->headers->set('Content-Type', 'text/html');
 
             return new Response('', 205);
         } else {
