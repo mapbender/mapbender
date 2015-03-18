7666e9b5ea75392ce8004a884683fb9e90e2df0e
Andriy Oblivantsev <eslider@gmail.com>
2015-03-18 13:26:54 +0100

    add Feature entity

---
+++ src/Mapbender/CoreBundle/Entity/Feature.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ * @author Andriy Oblivantsev <eslider@gmail.com>
+ * @copyright 18.03.2015 by WhereGroup GmbH & Co. KG
+ */
+
+namespace Mapbender\CoreBundle\Entity;
+
+
+class Feature {
+
+}
