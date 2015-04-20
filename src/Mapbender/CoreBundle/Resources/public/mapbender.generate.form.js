398b06bc20e525e553420695f25727f0ffcb7d2f
Andriy Oblivantsev <eslider@gmail.com>
2015-04-20 18:48:18 +0200

    pick current changes from digitizer branch into mapbender.generate.form.js

--- src/Mapbender/CoreBundle/Resources/public/mapbender.generate.form.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.generate.form.js
@@ -128,11 +128,19 @@
                 }
 
                 if(has(item, 'mandatory') && item.mandatory) {
-                    inputField.data('warn',function(){
-                        var hasValue = $.trim(inputField.val()) != '';
+                    inputField.data('warn',function(value){
+                        var hasValue = $.trim(value) != '';
+                        var isRegExp = item.mandatory !== true;
+                        var text = item.hasOwnProperty('mandatoryText')? item.mandatoryText: "Bitte übeprüfen!";
+
+                        if(isRegExp){
+                            hasValue = eval(item.mandatory).exec(value) != null;
+                        }
+
                         if(hasValue){
                             container.removeClass('has-error');
                         }else{
+                            $.notify( inputField, text, { position:"top right", autoHideDelay: 2000});
                             container.addClass('has-error');
                         }
                         return hasValue;
@@ -244,6 +252,7 @@
                 return container;
             },
             textArea:  function(item, declarations) {
+                var container = $('<div class="form-group"/>');
                 var input = $('<textarea class="form-control" rows="3"/>');
                 input.data('declaration',item);
 
@@ -257,12 +266,17 @@
                     }
                 });
 
-                return input;
+                if(has(item, 'text')) {
+                    container.append(declarations.label(item, declarations));
+                }
+
+                container.append(input);
+
+                return container;
             },
             select:    function(item, declarations) {
                 var container = $('<div class="form-group"/>');
                 var select = $('<select class="form-control"/>');
-
 
                 select.data('declaration',item);
 
