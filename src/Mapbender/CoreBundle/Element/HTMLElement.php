c190197e1319045595b9f9a585351eb0daeb9ad7
Andriy Oblivantsev <eslider@gmail.com>
2015-06-02 11:46:22 +0200

    add ability to define JS array in configuration of HTMLElement

--- src/Mapbender/CoreBundle/Element/HTMLElement.php
+++ src/Mapbender/CoreBundle/Element/HTMLElement.php
@@ -221,7 +221,11 @@ class HTMLElement extends Element
         $configuration = $this->getConfiguration();
         $assets        = $this::listAssets();
         if (isset($configuration['jsSrc'])) {
-            $assets['js'][] = $configuration['jsSrc'];
+            if (is_array($configuration['jsSrc'])) {
+                $assets['js'] = array_merge($assets['js'], $configuration['jsSrc']);
+            } else {
+                $assets['js'][] = $configuration['jsSrc'];
+            }
         }
         return $assets;
     }
