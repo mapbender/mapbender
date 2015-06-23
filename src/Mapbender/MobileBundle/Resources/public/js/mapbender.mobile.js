e07d0f907d38d2981a6265e4ce0e8d0383637137
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2015-06-23 13:36:24 +0200

    only show mobile FI if actual result is found

--- src/Mapbender/MobileBundle/Resources/public/js/mapbender.mobile.js
+++ src/Mapbender/MobileBundle/Resources/public/js/mapbender.mobile.js
@@ -1,11 +1,13 @@
 $(function(){
     $(document).on('mbfeatureinfofeatureinfo', function(e, options){
-        $.each($('#mobilePane .mobileContent').children(), function(idx, item){
-            $(item).addClass('hidden');
-        });
-        $('#' + options.id).removeClass('hidden');
-        $('#mobilePane .contentTitle').text($('#' + options.id).attr('data-title'));
-        $('#mobilePane').attr('data-state', 'opened');
+        if(options.action === 'haveresult') {
+            $.each($('#mobilePane .mobileContent').children(), function(idx, item){
+                $(item).addClass('hidden');
+            });
+            $('#' + options.id).removeClass('hidden');
+            $('#mobilePane .contentTitle').text($('#' + options.id).attr('data-title'));
+            $('#mobilePane').attr('data-state', 'opened');
+        }
     });
     $('#footer').on('click', '.mb-element,.mb-button', function(e){
         var button = $('#' + $(e.target).attr('for')).data('mapbenderMbButton');
