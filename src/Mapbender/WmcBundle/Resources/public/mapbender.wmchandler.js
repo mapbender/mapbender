24f2a3191cac128c8f73dc7bad46fda755bfd330
Paul Schmidt <paul.schmidt@wheregroup.com>
2015-08-14 15:19:26 +0200

    fix set new map extent, remove old sources

--- src/Mapbender/WmcBundle/Resources/public/mapbender.wmchandler.js
+++ src/Mapbender/WmcBundle/Resources/public/mapbender.wmchandler.js
@@ -75,26 +75,26 @@ Mapbender.WmcHandler = function(mapWidget, options){
         if(wmcProj === null){
             Mapbender.error(Mapbender.trans(Mapbender.trans("mb.wmc.element.wmchandler.error_srs", {"srs": state.extent.srs})));
         }else if(wmcProj.projCode === mapProj.projCode){
+            this.mapWidget.removeSources(toKeepSources);
             if(!this.options.keepExtent){
                 var boundsAr = [state.extent.minx, state.extent.miny, state.extent.maxx, state.extent.maxy];
                 this.mapWidget.zoomToExtent(OpenLayers.Bounds.fromArray(boundsAr), true);
             }
-            this.mapWidget.removeSources(toKeepSources);
             this._addWmcToMap(state.sources);
         }else{
+            this.mapWidget.removeSources(toKeepSources);
             model.changeProjection({projection: wmcProj});
             if(!this.options.keepExtent){
                 var boundsAr = [state.extent.minx, state.extent.miny, state.extent.maxx, state.extent.maxy];
                 this.mapWidget.zoomToExtent(OpenLayers.Bounds.fromArray(boundsAr), true);
             }
-            this.mapWidget.removeSources(toKeepSources);
             this._addWmcToMap(state.sources);
         }
     };
     
     this.removeFromMap = function(){
-        var model = this.mapWidget.getModel(),
-                toKeepSources = {};
+        var model = this.mapWidget.getModel();
+        var toKeepSources = {};
         if(this.options.keepSources === 'basesources'){
             for(var i = 0; i < model.sourceTree.length; i++){
                 var source = model.sourceTree[i];
