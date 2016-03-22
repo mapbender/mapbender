f7c595e2b8914bb38a9110a50f9253daa99c881e
Paul Schmidt <paul.schmidt@wheregroup.com>
2016-03-22 11:07:32 +0100

    Fix DataFixtures EPSG's import and implement EPSG's update

--- src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/LoadEpsgData.php
+++ src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/LoadEpsgData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mapbender\CoreBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
@@ -18,8 +19,12 @@ class LoadEpsgData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $filepath = __DIR__ . '/../../../Resources/proj4/proj4js_epsg.txt';
-        $file = @fopen($filepath, "r");
+        $filepath = __DIR__.'/../../../Resources/proj4/proj4js_epsg.txt';
+        $file     = @fopen($filepath, "r");
+        $repo     = $manager->getRepository($class    = get_class(new SRS()));
+        $imported = 0;
+        $updated = 0;
+        echo "EPSG ";
         while (!feof($file)) {
             $help = trim(str_ireplace("\n", "", fgets($file)));
             if (strlen($help) === 0) {
@@ -29,15 +34,20 @@ class LoadEpsgData implements FixtureInterface
             if ($temp[0] === null || strlen($temp[0]) === 0) {
                 continue;
             }
-            $srs = new SRS();
-            $srs->setName($temp[0]);
-            $srs->setTitle($temp[1]);
-            $srs->setDefinition($temp[2]);
-
+            if ($srs = $repo->findOneByName($temp[0])) {
+                $srs->setTitle($temp[1]);
+                $srs->setDefinition($temp[2]);
+                $updated++;
+            } else {
+                $srs->setName($temp[0]);
+                $srs->setTitle($temp[1]);
+                $srs->setDefinition($temp[2]);
+                $imported++;
+            }
             $manager->persist($srs);
         }
         $manager->flush();
+        echo "updated: " . $updated . ", imported: " . $imported . PHP_EOL;
         fclose($file);
     }
-
 }
