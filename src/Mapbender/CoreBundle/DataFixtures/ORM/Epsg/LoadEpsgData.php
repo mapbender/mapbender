1f9b81602feb6c4368a1a85d03821806256b71c1
Andriy Oblivantsev <eslider@gmail.com>
2016-04-11 18:43:41 +0200

    Fix load EPSG data

--- src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/LoadEpsgData.php
+++ src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/LoadEpsgData.php
@@ -8,7 +8,7 @@ use Mapbender\CoreBundle\Entity\SRS;
 
 /**
  * The LoadEpsgData loads the epsg parameter from a text file into a database table.
- * 
+ *
  * @author Paul Schmidt
  */
 class LoadEpsgData implements FixtureInterface
@@ -19,11 +19,11 @@ class LoadEpsgData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $filepath = __DIR__.'/../../../Resources/proj4/proj4js_epsg.txt';
+        $filepath = __DIR__ . '/../../../Resources/proj4/proj4js_epsg.txt';
         $file     = @fopen($filepath, "r");
-        $repo     = $manager->getRepository($class    = get_class(new SRS()));
+        $repo     = $manager->getRepository($class = get_class(new SRS()));
         $imported = 0;
-        $updated = 0;
+        $updated  = 0;
         echo "EPSG ";
         while (!feof($file)) {
             $help = trim(str_ireplace("\n", "", fgets($file)));
@@ -34,11 +34,13 @@ class LoadEpsgData implements FixtureInterface
             if ($temp[0] === null || strlen($temp[0]) === 0) {
                 continue;
             }
-            if ($srs = $repo->findOneByName($temp[0])) {
+            $srs = $repo->findOneByName($temp[0]);
+            if ($srs) {
                 $srs->setTitle($temp[1]);
                 $srs->setDefinition($temp[2]);
                 $updated++;
             } else {
+                $srs = new SRS();
                 $srs->setName($temp[0]);
                 $srs->setTitle($temp[1]);
                 $srs->setDefinition($temp[2]);
