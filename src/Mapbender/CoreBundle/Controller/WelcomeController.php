20cbdb5899b8973aebb8ccc34ab79adc2d1b5937
Andriy Oblivantsev <eslider@gmail.com>
2016-05-11 17:00:39 +0200

    Refactor WelcomeController

--- src/Mapbender/CoreBundle/Controller/WelcomeController.php
+++ src/Mapbender/CoreBundle/Controller/WelcomeController.php
@@ -1,9 +1,4 @@
 <?php
-
-/**
- * TODO: License
- */
-
 namespace Mapbender\CoreBundle\Controller;
 
 use Mapbender\CoreBundle\Component\Application as AppComponent;
@@ -16,27 +11,26 @@ use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
  * Welcome controller.
  *
  * This controller can be used to display an list of available applications.
- * It has been seperated in it's own class so it can easily be added or
+ * It has been separated in it's own class so it can easily be added or
  * removed from the routing.
  *
  * @author Christian Wygoda
+ * @author Paul Schmidt <paul.schmidt@wheregroup.com>
+ * @author Andriy Oblivantsev <andriy.oblivantsev@wheregroup.com>
  */
 class WelcomeController extends Controller
 {
     /**
      * List applications.
      *
-     * Lists all applications (TODO: available to the current user)
-     *
      * @Route("/")
      * @Template()
      */
     public function listAction()
     {
-        $securityContext = $this->get('security.context');
-        $oid = new ObjectIdentity('class', 'Mapbender\CoreBundle\Entity\Application');
-        $uploads_web_url = AppComponent::getUploadsUrl($this->container);
-        $applications = $this->get('mapbender')->getApplicationEntities();
+        $securityContext      = $this->get('security.context');
+        $oid                  = new ObjectIdentity('class', 'Mapbender\CoreBundle\Entity\Application');
+        $applications         = $this->get('mapbender')->getApplicationEntities();
         $allowed_applications = array();
         foreach ($applications as $application) {
             if ($application->isExcludedFromList()) {
@@ -52,10 +46,10 @@ class WelcomeController extends Controller
         }
 
         return array(
-            'applications' => $allowed_applications,
-            'uploads_web_url' => $uploads_web_url,
+            'applications'      => $allowed_applications,
+            'uploads_web_url'   => AppComponent::getUploadsUrl($this->container),
             'create_permission' => $securityContext->isGranted('CREATE', $oid),
-            'time' => new \DateTime()
+            'time'              => new \DateTime()
         );
     }
 }
