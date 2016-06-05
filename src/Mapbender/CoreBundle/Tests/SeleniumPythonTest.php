8b8e6717b0941797e5d6bdf084d1765e3ba7f05c
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2016-06-05 17:31:16 +0200

    trying to kill previous phantomjs processes

--- src/Mapbender/CoreBundle/Tests/SeleniumPythonTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPythonTest.php
@@ -5,11 +5,11 @@ namespace Mapbender\CoreBundle\Tests;
 use Symfony\Component\Process\Process;
 use FOM\Component\Test\SharedApplicationWebTestCase;
 
-
 class SeleniumPythonTest extends SharedApplicationWebTestCase
 {
-    public function setUp() {
-        if(PHP_MINOR_VERSION == 3) {
+    public function setUp()
+    {
+        if (PHP_MINOR_VERSION == 3) {
             $this->markTestIncomplete('This test does not run on PHP 5.3.');
             return;
         }
@@ -19,7 +19,7 @@ class SeleniumPythonTest extends SharedApplicationWebTestCase
     {
         $data = array();
         $glob = glob(dirname(__FILE__) . '/SeleniumIdeTests/*.py');
-        foreach($glob as $file) {
+        foreach ($glob as $file) {
             $data[] = array($file);
         }
         return $data;
@@ -30,12 +30,13 @@ class SeleniumPythonTest extends SharedApplicationWebTestCase
      */
     public function testSelenium($module)
     {
+        `killall phantomjs`;
         putenv('TEST_WEB_SERVER_HOST=' . TEST_WEB_SERVER_HOST);
         putenv('TEST_WEB_SERVER_PORT=' . TEST_WEB_SERVER_PORT);
         putenv('TEST_SCREENSHOT_PATH=' . TEST_SCREENSHOT_PATH);
 
         $process = new Process('python ' . $module);
-        $process->setTimeout(3600);  // @todo: Move into phpunit.xml via env or so
+        $process->setTimeout(3600);
         $process->run();
         $this->assertEquals(true, $process->isSuccessful(), $process->getErrorOutput());
     }
