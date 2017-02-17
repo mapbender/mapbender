0788a4222922873014b09b67c949393ebdb62e9a
Andriy Oblivantsev <eslider@gmail.com>
2017-02-17 12:26:07 +0100

    Fix OpenLayer2 MapQuery edition library binding

--- src/Mapbender/CoreBundle/Element/Map.php
+++ src/Mapbender/CoreBundle/Element/Map.php
@@ -79,10 +79,10 @@ class Map extends Element
     {
         return array(
             'js' => array(
-                'mapquery/lib/openlayers/OpenLayers.js',
+                '/../vendor/mapbender/mapquery/lib/openlayers/OpenLayers.js',
                 /* 'mapquery/lib/openlayers/lib/deprecated.js', */
-                'mapquery/lib/jquery/jquery.tmpl.js',
-                'mapquery/src/jquery.mapquery.core.js',
+                '/../vendor/mapbender/mapquery/lib/jquery/jquery.tmpl.js',
+                '/../vendor/mapbender/mapquery/src/jquery.mapquery.core.js',
                 'proj4js/proj4js-compressed.js',
                 'mapbender.element.map.js'),
             'css' => array('@MapbenderCoreBundle/Resources/public/sass/element/map.scss'));
