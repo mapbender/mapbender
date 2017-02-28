33cf97a5552ac30002e1232171348530cfa542a5
Andriy Oblivantsev <eslider@gmail.com>
2017-02-28 20:05:18 +0100

    Fix load core application by MapbenderYamlCompilerPass

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -20,11 +20,25 @@ use Mapbender\CoreBundle\DependencyInjection\Compiler\MapbenderYamlCompilerPass;
 class MapbenderCoreBundle extends MapbenderBundle
 {
 
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
 
-        $container->addCompilerPass(new MapbenderYamlCompilerPass());
+        $kernelPath = $container->getParameter('kernel.root_dir');
+        $container->addCompilerPass(new MapbenderYamlCompilerPass(
+                $kernelPath . "/config/applications",
+                $kernelPath . "/config/mapbender.yml")
+        );
     }
 
     /**
