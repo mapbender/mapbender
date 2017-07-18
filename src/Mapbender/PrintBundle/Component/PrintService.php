b549590fe1fd09aad19aca9927c32e80bb846cb6
Stefan Winkelmann <stefan.winkelmann@wheregroup.com>
2017-07-18 09:56:51 +0200

    Print: change axis order for some EPSG if WMS Version 1.3.0

--- src/Mapbender/PrintBundle/Component/PrintService.php
+++ src/Mapbender/PrintBundle/Component/PrintService.php
@@ -48,7 +48,7 @@ class PrintService
     public function doPrint($data)
     {
         $this->setup($data);
-       
+
         if ($data['rotation'] == 0) {
             $this->createFinalMapImage();
         } else {
@@ -65,13 +65,13 @@ class PrintService
         $this->tempDir = sys_get_temp_dir();
         // resource dir
         $this->resourceDir = $this->container->getParameter('kernel.root_dir') . '/Resources/MapbenderPrintBundle';
-        
+
         // get user
         /** @var SecurityContext $securityContext */
         $securityContext = $this->container->get('security.context');
         $token           = $securityContext->getToken();
         $this->user      = $token->getUser();
-        
+
         // data from client
         $this->data = $data;
 
@@ -89,59 +89,60 @@ class PrintService
             if ($layer['type'] != 'wms') {
                 continue;
             }
-            $url = strstr($data['layers'][$i]['url'], '&BBOX', true);
-            $this->mapRequests[$i] = $url;
-        }
+            $request = strstr($layer['url'], '&BBOX', true);
 
-        if(isset($this->data['replace_pattern'])){
-            $this->addReplacePattern();
-        }
+            $extentWidth = $data['extent']['width'];
+            $extentHeight = $data['extent']['height'];
+            $centerx = $data['center']['x'];
+            $centery = $data['center']['y'];
 
-        $this->rotation = $rotation = $data['rotation'];
-        $extentWidth = $data['extent']['width'];
-        $extentHeight = $data['extent']['height'];
-        $centerx = $data['center']['x'];
-        $centery = $data['center']['y'];
+            // switch axis for some EPSG if WMS Version 1.3.0
+            if ($layer['changeAxis']){
+                $extentWidth = $data['extent']['height'];
+                $extentHeight = $data['extent']['width'];
+                $centerx = $data['center']['y'];
+                $centery = $data['center']['x'];
+            }
 
-        // switch if image is rotated
-        if ($rotation == 0) {
-            // calculate needed bbox
-            $minX = $centerx - $extentWidth * 0.5;
-            $minY = $centery - $extentHeight * 0.5;
-            $maxX = $centerx + $extentWidth * 0.5;
-            $maxY = $centery + $extentHeight * 0.5;
+            // switch if image is rotated
+            $this->rotation = $rotation = $data['rotation'];
+            if ($rotation == 0) {
+                // calculate needed bbox
+                $minX = $centerx - $extentWidth * 0.5;
+                $minY = $centery - $extentHeight * 0.5;
+                $maxX = $centerx + $extentWidth * 0.5;
+                $maxY = $centery + $extentHeight * 0.5;
 
-            $width = '&WIDTH=' . $this->imageWidth;
-            $height =  '&HEIGHT=' . $this->imageHeight;
-        }else{
-            // calculate needed bbox 
-            $neededExtentWidth = abs(sin(deg2rad($rotation)) * $extentHeight) +
-                abs(cos(deg2rad($rotation)) * $extentWidth);
-            $neededExtentHeight = abs(sin(deg2rad($rotation)) * $extentWidth) +
-                abs(cos(deg2rad($rotation)) * $extentHeight);
+                $width = '&WIDTH=' . $this->imageWidth;
+                $height =  '&HEIGHT=' . $this->imageHeight;
+            }else{
+                // calculate needed bbox
+                $neededExtentWidth = abs(sin(deg2rad($rotation)) * $extentHeight) +
+                    abs(cos(deg2rad($rotation)) * $extentWidth);
+                $neededExtentHeight = abs(sin(deg2rad($rotation)) * $extentWidth) +
+                    abs(cos(deg2rad($rotation)) * $extentHeight);
 
-            $this->neededExtentWidth = $neededExtentWidth;
-            $this->neededExtentHeight = $neededExtentHeight;
+                $this->neededExtentWidth = $neededExtentWidth;
+                $this->neededExtentHeight = $neededExtentHeight;
 
-            $minX = $centerx - $neededExtentWidth * 0.5;
-            $minY = $centery - $neededExtentHeight * 0.5;
-            $maxX = $centerx + $neededExtentWidth * 0.5;
-            $maxY = $centery + $neededExtentHeight * 0.5;
+                $minX = $centerx - $neededExtentWidth * 0.5;
+                $minY = $centery - $neededExtentHeight * 0.5;
+                $maxX = $centerx + $neededExtentWidth * 0.5;
+                $maxY = $centery + $neededExtentHeight * 0.5;
 
-            // calculate needed image size
-            $neededImageWidth = round(abs(sin(deg2rad($rotation)) * $this->imageHeight) +
-                abs(cos(deg2rad($rotation)) * $this->imageWidth));
-            $neededImageHeight = round(abs(sin(deg2rad($rotation)) * $this->imageWidth) +
-                abs(cos(deg2rad($rotation)) * $this->imageHeight));
+                // calculate needed image size
+                $neededImageWidth = round(abs(sin(deg2rad($rotation)) * $this->imageHeight) +
+                    abs(cos(deg2rad($rotation)) * $this->imageWidth));
+                $neededImageHeight = round(abs(sin(deg2rad($rotation)) * $this->imageWidth) +
+                    abs(cos(deg2rad($rotation)) * $this->imageHeight));
 
-            $this->neededImageWidth = $neededImageWidth;
-            $this->neededImageHeight = $neededImageHeight;
+                $this->neededImageWidth = $neededImageWidth;
+                $this->neededImageHeight = $neededImageHeight;
 
-            $width = '&WIDTH=' . $neededImageWidth;
-            $height =  '&HEIGHT=' . $neededImageHeight;
-        }
+                $width = '&WIDTH=' . $neededImageWidth;
+                $height =  '&HEIGHT=' . $neededImageHeight;
+            }
 
-        foreach ($this->mapRequests as $i => $request) {
             $request .= '&BBOX=' . $minX . ',' . $minY . ',' . $maxX . ',' . $maxY;
             $request .= $width . $height;
 
@@ -153,6 +154,11 @@ class PrintService
 
             $this->mapRequests[$i] = $request;
         }
+
+        if(isset($this->data['replace_pattern'])){
+            $this->addReplacePattern();
+        }
+
     }
 
     private function addReplacePattern()
@@ -475,26 +481,26 @@ class PrintService
         if (isset($this->conf['scalebar']) ) {
             $this->addScaleBar();
         }
-        
+
         // add coordinates
-        if (isset($this->conf['fields']['extent_ur_x']) && isset($this->conf['fields']['extent_ur_y']) 
+        if (isset($this->conf['fields']['extent_ur_x']) && isset($this->conf['fields']['extent_ur_y'])
                 && isset($this->conf['fields']['extent_ll_x']) && isset($this->conf['fields']['extent_ll_y']))
         {
             $this->addCoordinates();
         }
-        
+
         // add dynamic logo
         if (isset($this->conf['dynamic_image']) && $this->conf['dynamic_image']){
             $this->addDynamicImage();
         }
-        
+
         // add dynamic text
         if (isset($this->conf['fields'])
             && isset($this->conf['fields']['dynamic_text'])
             && $this->conf['fields']['dynamic_text']){
             $this->addDynamicText();
         }
-        
+
         // add legend
         if (isset($this->data['legends']) && !empty($this->data['legends'])){
             $this->addLegend();
@@ -544,17 +550,6 @@ class PrintService
 
     private function addOverviewMap()
     {
-        // calculate needed bbox
-        $ovWidth = $this->conf['overview']['width'] * $this->data['overview'][0]['scale'] / 1000;
-        $ovHeight = $this->conf['overview']['height'] * $this->data['overview'][0]['scale'] / 1000;
-        $centerx = $this->data['center']['x'];
-        $centery = $this->data['center']['y'];
-        $minX = $centerx - $ovWidth * 0.5;
-        $minY = $centery - $ovHeight * 0.5;
-        $maxX = $centerx + $ovWidth * 0.5;
-        $maxY = $centery + $ovHeight * 0.5;
-        $bbox = '&BBOX=' . $minX . ',' . $minY . ',' . $maxX . ',' . $maxY;
-
         // calculate needed image size
         $quality = $this->data['quality'];
         $ovImageWidth = round($this->conf['overview']['width'] / 25.4 * $quality);
@@ -562,11 +557,33 @@ class PrintService
         $width = '&WIDTH=' . $ovImageWidth;
         $height = '&HEIGHT=' . $ovImageHeight;
 
+        $changeAxis = false;
+
         // get images
         $tempNames = array();
         $logger = $this->container->get("logger");
         foreach ($this->data['overview'] as $i => $layer) {
-            $url = strstr($this->data['overview'][$i]['url'], '&BBOX', true);
+            // calculate needed bbox
+            $ovWidth = $this->conf['overview']['width'] * $layer['scale'] / 1000;
+            $ovHeight = $this->conf['overview']['height'] * $layer['scale'] / 1000;
+            $centerx = $this->data['center']['x'];
+            $centery = $this->data['center']['y'];
+
+            if ($layer['changeAxis']){
+                $ovWidth = $this->conf['overview']['height'] * $layer['scale'] / 1000;
+                $ovHeight = $this->conf['overview']['width'] * $layer['scale'] / 1000;
+                $centerx = $this->data['center']['y'];
+                $centery = $this->data['center']['x'];
+                $changeAxis = true;
+            }
+
+            $minX = $centerx - $ovWidth * 0.5;
+            $minY = $centery - $ovHeight * 0.5;
+            $maxX = $centerx + $ovWidth * 0.5;
+            $maxY = $centery + $ovHeight * 0.5;
+            $bbox = '&BBOX=' . $minX . ',' . $minY . ',' . $maxX . ',' . $maxY;
+
+            $url = strstr($layer['url'], '&BBOX', true);
             $url .= $bbox . $width . $height;
 
             $logger->debug("Print Overview Request Nr.: " . $i . ' ' . $url);
@@ -637,20 +654,37 @@ class PrintService
         $image = imagecreatefrompng($finalImageName);
 
         // add red extent rectangle
-        $ll_x = $this->data['extent_feature'][3]['x'];
-        $ll_y = $this->data['extent_feature'][3]['y'];
-        $ul_x = $this->data['extent_feature'][0]['x'];
-        $ul_y = $this->data['extent_feature'][0]['y'];
+        if (!$changeAxis) {
+            $ll_x = $this->data['extent_feature'][3]['x'];
+            $ll_y = $this->data['extent_feature'][3]['y'];
+            $ul_x = $this->data['extent_feature'][0]['x'];
+            $ul_y = $this->data['extent_feature'][0]['y'];
 
-        $lr_x = $this->data['extent_feature'][2]['x'];
-        $lr_y = $this->data['extent_feature'][2]['y'];
-        $ur_x = $this->data['extent_feature'][1]['x'];
-        $ur_y = $this->data['extent_feature'][1]['y'];
+            $lr_x = $this->data['extent_feature'][2]['x'];
+            $lr_y = $this->data['extent_feature'][2]['y'];
+            $ur_x = $this->data['extent_feature'][1]['x'];
+            $ur_y = $this->data['extent_feature'][1]['y'];
 
-        $p1 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ll_x, $ll_y);
-        $p2 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ul_x, $ul_y);
-        $p3 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ur_x, $ur_y);
-        $p4 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $lr_x, $lr_y);
+            $p1 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ll_x, $ll_y);
+            $p2 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ul_x, $ul_y);
+            $p3 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $ur_x, $ur_y);
+            $p4 = $this->realWorld2ovMapPos($ovWidth, $ovHeight, $lr_x, $lr_y);
+        } else {
+            $ul_x = $this->data['extent_feature'][3]['x'];
+            $ul_y = $this->data['extent_feature'][3]['y'];
+            $ll_x = $this->data['extent_feature'][0]['x'];
+            $ll_y = $this->data['extent_feature'][0]['y'];
+
+            $ur_x = $this->data['extent_feature'][2]['x'];
+            $ur_y = $this->data['extent_feature'][2]['y'];
+            $lr_x = $this->data['extent_feature'][1]['x'];
+            $lr_y = $this->data['extent_feature'][1]['y'];
+
+            $p1 = $this->realWorld2ovMapPos($ovHeight, $ovWidth, $ll_x, $ll_y);
+            $p2 = $this->realWorld2ovMapPos($ovHeight, $ovWidth, $ul_x, $ul_y);
+            $p3 = $this->realWorld2ovMapPos($ovHeight, $ovWidth, $ur_x, $ur_y);
+            $p4 = $this->realWorld2ovMapPos($ovHeight, $ovWidth, $lr_x, $lr_y);
+        }
 
         $red = ImageColorAllocate($image,255,0,0);
         imageline ( $image, $p1[0], $p1[1], $p2[0], $p2[1], $red);
@@ -700,11 +734,11 @@ class PrintService
         $pdf->SetFillColor(0,0,0);
         $pdf->Rect($this->conf['scalebar']['x'] + 40  , $this->conf['scalebar']['y'], 10, 2, 'FD');
     }
-    
+
     private function addCoordinates()
     {
         $pdf = $this->pdf;
-        
+
         $corrFactor = 2;
         $precision = 2;
         // correction factor and round precision if WGS84
@@ -712,7 +746,7 @@ class PrintService
              $corrFactor = 3;
              $precision = 6;
         }
-        
+
         // upper right Y
         $pdf->SetFont('Arial', '', $this->conf['fields']['extent_ur_y']['fontsize']);
         $pdf->Text($this->conf['fields']['extent_ur_y']['x'] + $corrFactor,
@@ -737,7 +771,7 @@ class PrintService
                     $this->conf['fields']['extent_ll_x']['y'] + 30,
                     round($this->data['extent_feature'][0]['x'], $precision),'U');
     }
-    
+
     private function addDynamicImage()
     {
         if($this->user == 'anon.'){
@@ -746,11 +780,11 @@ class PrintService
 
         $groups = $this->user->getGroups();
         $group = $groups[0];
-        
+
         if(!isset($group)){
             return;
         }
-        
+
         $dynImage = $this->resourceDir . '/images/' . $group->getTitle() . '.png';
         if(file_exists ($dynImage)){
             $this->pdf->Image($dynImage,
@@ -763,25 +797,25 @@ class PrintService
         }
 
     }
-    
+
     private function addDynamicText()
     {
         if($this->user == 'anon.'){
             return;
         }
-        
+
         $groups = $this->user->getGroups();
         $group = $groups[0];
-        
+
         if(!isset($group)){
             return;
         }
-        
+
         $this->pdf->SetFont('Arial', '', $this->conf['fields']['dynamic_text']['fontsize']);
         $this->pdf->MultiCell($this->conf['fields']['dynamic_text']['width'],
                 $this->conf['fields']['dynamic_text']['height'],
                 utf8_decode($group->getDescription()));
-        
+
     }
 
     private function getFeature($schemaName, $featureId)
@@ -917,7 +951,7 @@ class PrintService
             imageline($image, $from[0], $from[1], $to[0], $to[1], $color);
         }
     }
-	
+
     private function drawMultiLineString($geometry, $image)
     {
         $style = $this->getStyle($geometry);
@@ -929,9 +963,9 @@ class PrintService
             return;
         }
         imagesetthickness($image, $style['strokeWidth']);
-	
+
 		foreach($geometry['coordinates'] as $coords) {
-		
+
 			for($i = 1; $i < count($coords); $i++) {
 
 				if($this->rotation == 0){
@@ -1042,7 +1076,7 @@ class PrintService
           $height = $this->pdf->getHeight();
           $width = $this->pdf->getWidth();
           $legendConf = false;
-          if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+          if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
              $this->addLegendPageImage();
           }
         }
@@ -1068,16 +1102,16 @@ class PrintService
                     if($y + $tempY + 10 > ($this->pdf->getHeight()) && $legendConf == false){
                         $x += 105;
                         $y = 10;
-                        if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                        if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                            $this->addLegendPageImage();
-                        } 
+                        }
                         if($x + 20 > ($this->pdf->getWidth())){
                             $this->pdf->addPage('P');
                             $x = 5;
                             $y = 10;
-                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                                $this->addLegendPageImage();
-                            } 
+                            }
                         }
                     }
 
@@ -1091,18 +1125,18 @@ class PrintService
                             $x = 5;
                             $y = 10;
                             $legendConf = false;
-                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                                $this->addLegendPageImage();
-                            } 
+                            }
                         }
                     }else if (($y-$yStartPosition) + $tempY + 10 > $height && $legendConf == true){
                             $this->pdf->addPage('P');
                             $x = 5;
                             $y = 10;
                             $legendConf = false;
-                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                                $this->addLegendPageImage();
-                            } 
+                            }
                     }
                 }
 
@@ -1130,9 +1164,9 @@ class PrintService
                             $height = $this->pdf->getHeight();
                             $width = $this->pdf->getWidth();
                             $legendConf = false;
-                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                                $this->addLegendPageImage();
-                            } 
+                            }
                         }
 
                   }else{
@@ -1144,17 +1178,17 @@ class PrintService
                       $y += round($size[1] * 25.4 / 96) + 10;
                       if($y > ($this->pdf->getHeight())){
                           $x += 105;
-                          $y = 10; 
+                          $y = 10;
                       }
                       if($x + 20 > ($this->pdf->getWidth()) && $c < $arraySize){
                           $this->pdf->addPage('P');
                           $x = 5;
                           $y = 10;
-                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){ 
+                            if(isset($this->conf['legendpage_image']) && $this->conf['legendpage_image']){
                                $this->addLegendPageImage();
-                            } 
+                            }
                       }
-                       
+
                   }
 
                 unlink($image);
@@ -1223,10 +1257,10 @@ class PrintService
             $legendpageImage = $this->resourceDir . '/images/' . 'legendpage_image'. '.png';
         }else{
           $groups = $this->user->getGroups();
-          $group = $groups[0]; 
-        
+          $group = $groups[0];
+
           if(isset($group)){
-              $legendpageImage = $this->resourceDir . '/images/' . $group->getTitle() . '.png'; 
+              $legendpageImage = $this->resourceDir . '/images/' . $group->getTitle() . '.png';
           }
         }
 
