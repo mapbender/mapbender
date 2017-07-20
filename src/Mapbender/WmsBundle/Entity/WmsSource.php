0aa060aa681c26effe0fabb67764ffa6749f9315
wg-rolf <rolf.neuberger@wheregroup.com>
2017-07-20 15:17:11 +0200

    Fix annotations

--- src/Mapbender/WmsBundle/Entity/WmsSource.php
+++ src/Mapbender/WmsBundle/Entity/WmsSource.php
@@ -771,7 +771,7 @@ class WmsSource extends Source implements ContainingKeyword
     /**
      * Get username
      *
-     * 
+     * @return string
      */
     public function getUsername()
     {
@@ -793,7 +793,7 @@ class WmsSource extends Source implements ContainingKeyword
     /**
      * Get password
      *
-     * 
+     * @return string
      */
     public function getPassword()
     {
