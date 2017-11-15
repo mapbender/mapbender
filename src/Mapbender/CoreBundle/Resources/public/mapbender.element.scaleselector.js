dae7b80b9e9db809e0b4b1d64643fbfb65191ab3
Andriy Oblivantsev <eslider@gmail.com>
2017-11-15 22:24:30 +0100

    Fix displaying scale value in scale selector Clean up mapbender.element.scaleselector.js code
    Closes: #647
    Associated: #595

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.scaleselector.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.scaleselector.js
@@ -1,65 +1,152 @@
 (function($) {
 
     $.widget("mapbender.mbScaleSelector", {
-        options:      {
+
+        options: {
+            /**
+             * Target widget id string
+             */
             target: null
         },
-        elementUrl:   null,
-        _create:      function() {
-            var widget = this;
-            if(!Mapbender.checkTarget("mbScaleSelector", widget.options.target)) {
-                return;
-            }
-            var element = $(widget.element);
-            widget.elementUrl = Mapbender.configuration.elementPath + element.attr('id') + '/';
-            Mapbender.elementRegistry.onElementReady(widget.options.target, $.proxy(widget._setup, widget));
-        },
-        _setup:       function() {
+
+        elementUrl: null,
+
+        /**
+         * Map target instance of OpenLayers.Map
+         *
+         * @var {OpenLayers.Map}
+         */
+        olMap: null,
+
+        /**
+         * Mapbender map widget
+         */
+        map: null,
+
+        /**
+         * Ready event listeners
+         *
+         * @var {Array<Function>}
+         */
+        readyCallbacks: [],
+
+        /**
+         * Constructor
+         *
+         * @private
+         */
+        _create: function() {
             var widget = this;
             var options = widget.options;
-            var mbMap = $('#' + options.target).data('mapbenderMbMap');
-            var scale = mbMap.model.getScale();
-            var scales = mbMap.scales();
+            var target = options.target;
+
+            if(!Mapbender.checkTarget("mbScaleSelector", target)) {
+                return;
+            }
+
+            var element = $(widget.element);
+
+            widget.elementUrl = Mapbender.configuration.elementPath + element.attr('id') + '/';
+            Mapbender.elementRegistry.onElementReady(target, $.proxy(widget._setup, widget));
+        },
+
+        _setup: function() {
+            var widget = this;
+            var options = widget.options;
+            var mbMap = widget.map = $('#' + options.target).data('mapbenderMbMap');
+            var olMap = widget.olMap = mbMap.map.olMap;
+            var scale = Math.round(olMap.getScale());
             var select = $("select", $(widget.element));
-            $.each(scales, function(idx, value) {
-                value = Math.round(value);
-                select.append('<option value="' + value + '">' + value + '</option>');
+
+            _.each(mbMap.scales(), function(value) {
+                var option = $("<option/>");
+                var formattedValue = Math.round(value);
+
+                option.attr('value', formattedValue).html(formattedValue);
+                select.append(option);
             });
 
             select.change($.proxy(widget._zoomToScale, widget));
-            $('option[value="' + scale + '"]', select).attr('selected', true);
+            select.val(scale);
+
             initDropdown.call(this.element.get(0));
+
             widget._updateScale();
-            mbMap.map.olMap.events.register('zoomend', widget, $.proxy(widget._updateScale, widget));
+
+            olMap.events.register('zoomend', widget, $.proxy(widget._updateScale, widget));
+
             widget._trigger('ready');
             widget._ready();
         },
+
+        /**
+         * Zoom to scale event handler
+         * @private
+         */
         _zoomToScale: function() {
-            var scale = $("#" + $(this.element).attr('id') + " select").val();
-            var map = $('#' + this.options.target).data('mapbenderMbMap');
+            var widget = this;
+            var element = $(widget.element);
+            var scale = $("> select", element).val();
+            var map = widget.map;
+
             map.zoomToScale(scale, true);
         },
+
+        /**
+         * Update scale drop down view
+         *
+         * @private
+         */
         _updateScale: function() {
-            var mbMap = $('#' + this.options.target).data('mapbenderMbMap');
-            var scale = mbMap.model.getScale();
-            var select = $("#" + $(this.element).attr('id') + " select");
-            select.val(scale).siblings(".dropdownValue").text(scale);
+            var widget = this;
+            var olMap = widget.olMap;
+            var scale = Math.round(olMap.getScale());
+            var element = $(widget.element);
+            var select = $("> select", element);
+
+            select
+                .val(scale)
+                .siblings(".dropdownValue")
+                .text(scale);
         },
-        ready:        function(callback) {
-            if(this.readyState === true) {
+
+        /**
+         * Adds event listener callback function
+         * Called on widget is ready
+         *
+         * @param {Function} callback
+         */
+        ready: function(callback) {
+            var widget = this;
+            if(widget.readyState === true) {
                 callback();
             } else {
-                this.readyCallbacks.push(callback);
+                widget.readyCallbacks.push(callback);
             }
         },
-        _ready:       function() {
-            for (callback in this.readyCallbacks) {
-                callback();
-                delete(this.readyCallbacks[callback]);
-            }
-            this.readyState = true;
+
+        /**
+         * On ready event handler
+         *
+         * @private
+         */
+        _ready: function() {
+            var widget = this;
+            var callbacks = widget.readyCallbacks;
+
+            _.each(callbacks, function(callback) {
+
+                // Call and change context to widget
+                callback.call(widget);
+
+                // Remove from array
+                callbacks.splice(callbacks.indexOf(callback), 1);
+            });
+
+            widget.readyState = true;
         },
-        _destroy:     $.noop
+
+        _destroy: $.noop
     });
 
 })(jQuery);
