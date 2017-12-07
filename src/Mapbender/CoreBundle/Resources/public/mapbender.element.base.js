e60e5281eb62ce8929c941ad66a8f4121a805c95
Svitlana Aliferova <svitlana.aliferova@wheregroup.com>
2017-12-07 09:12:25 +0100

    Refactoring of container info helper

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.base.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.base.js
@@ -23,6 +23,15 @@
          *
          * @private
          */
+        destroy: function() {
+            this.functionIsDeprecated();
+        },
+
+        /**
+         * Private destroy
+         *
+         * @private
+         */
         _destroy: function() {
             this.functionIsDeprecated();
         },
@@ -36,3 +45,36 @@
     });
 
 })(jQuery);
+
+
+if (!Object.keys) {
+    Object.keys = function(obj) {
+        if (obj !== Object(obj)) {
+            throw new TypeError('Object.keys called on a non-object');
+        }
+
+        var keys=[], property;
+        for (property in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj,property)) {
+                keys.push(property);
+            }
+        }
+
+        return keys;
+    };
+}
+
+
+if (!Object.entries) {
+    Object.entries = function (obj) {
+        var ownProps = Object.keys(obj),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+
+        while (i--) {
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+        }
+
+        return resArray;
+    };
+}
