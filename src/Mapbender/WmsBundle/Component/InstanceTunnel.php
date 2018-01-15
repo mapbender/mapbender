28224514a6c7bc1a9f5661f7938338849ef465f7
wg-rolf <rolf.neuberger@wheregroup.com>
2018-01-15 12:04:25 +0100

    InstanceTunnel: absorb public url generation duties

--- src/Mapbender/WmsBundle/Component/InstanceTunnel.php
+++ src/Mapbender/WmsBundle/Component/InstanceTunnel.php
@@ -8,6 +8,7 @@ use Mapbender\CoreBundle\Entity\Source;
 use Mapbender\CoreBundle\Entity\SourceInstance;
 use Mapbender\CoreBundle\Utils\RequestUtil;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class InstanceTunnel
 {
@@ -76,5 +77,49 @@ class InstanceTunnel
             case 'GetLegendGraphic':
                 return WmsInstanceLayerEntityHandler::getLegendGraphicUrl($layerSource);
         }
+    }
+
+    /**
+     * Returns the URL base the Browser / JS client should use to access the tunnel.
+     *
+     * @todo: TBD: bind Router (or entire Container) via constructor?
+     *
+     * @param UrlGeneratorInterface $router
+     * @return string
+     */
+    public function getPublicBaseUrl(UrlGeneratorInterface $router)
+    {
+        return $router->generate(
+            'mapbender_core_application_instancetunnel',
+            array(
+                'slug' => $this->instance->getLayerset()->getApplication()->getSlug(),
+                'instanceId' => $this->instance->getId()),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+    }
+
+    /**
+     * Returns the URL the Browser / JS client should use to access a specific WMS function (by given URL) via
+     * the tunnel.
+     *
+     * @param UrlGeneratorInterface $router
+     * @param string $url NOTE: scheme/host/path completely ignored, only query string is relevant
+     * @return string
+     * @throws \RuntimeException if no REQUEST=... in given $url
+     */
+    public function generatePublicUrl(UrlGeneratorInterface $router, $url)
+    {
+        // require a "request" param, the tunnel action doesn't function without it
+        $params = array();
+        parse_str(parse_url($url, PHP_URL_QUERY), $params);
+        foreach ($params as $name => $value) {
+            if (strtolower($name) == 'request') {
+                // @todo: validate if request value is in our supported set (GetMap, GetLegendGraphic, GetFeatureInfo)?
+                $fullQueryString = strstr($url, '?', false);
+                // forward ALL GET parameters in input url
+                return $this->getPublicBaseUrl($router) . $fullQueryString;
+            }
+        }
+        throw new \RuntimeException('Failed to tunnelify url, no `request` param found: ' . var_export($url, true));
     }
 }
