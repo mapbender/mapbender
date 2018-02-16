cad013347ad53e972fdc82388cbc011029ac1716
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-16 18:56:10 +0100

    Added --format=<yaml,json,table> option to mapbender:inspect:source:usage

--- src/Mapbender/IntrospectionBundle/Command/SourcesCommand.php
+++ src/Mapbender/IntrospectionBundle/Command/SourcesCommand.php
@@ -4,9 +4,10 @@
 namespace Mapbender\IntrospectionBundle\Command;
 
 
-use Mapbender\CoreBundle\Entity\Source;
+use Mapbender\IntrospectionBundle\Component\Aggregator\Application;
 use Mapbender\IntrospectionBundle\Component\Aggregator\Relation\ApplicationToSources;
 use Mapbender\IntrospectionBundle\Component\Aggregator\Relation\SourceToApplications;
+use Mapbender\IntrospectionBundle\Component\Aggregator\Source;
 use Mapbender\IntrospectionBundle\Component\Collector;
 use Mapbender\IntrospectionBundle\Entity\Utils\Command\DataGroup;
 use Mapbender\IntrospectionBundle\Entity\Utils\Command\DataItem;
@@ -18,6 +19,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Yaml\Yaml;
 
 class SourcesCommand extends ContainerAwareCommand
 {
@@ -31,10 +33,19 @@ class SourcesCommand extends ContainerAwareCommand
         $this->addOption('by-source', null, InputOption::VALUE_NONE, 'Group by source');
         $this->addOption('unused-only', null, InputOption::VALUE_NONE, 'Display only unused sources');
         $this->addOption('no-unused', null, InputOption::VALUE_NONE, 'Do not display unused sources');
+        $this->addOption('format', null, InputOption::VALUE_REQUIRED, 'Output format "table" (default), "yaml" or "json"');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
+        $formatOption = $input->getOption('format');
+        if ($formatOption) {
+            if (!in_array($formatOption, array('table', 'yaml', 'yml', 'json'))) {
+                throw new \UnexpectedValueException("Unsupported format " . var_export($formatOption, true));
+            }
+        } else {
+            $input->setOption('format', 'table');
+        }
         if ($input->getOption('by-source')) {
             if ($input->getOption('by-app')) {
                 throw new \RuntimeException("Options --by-app and --by-source are mutually exclusive!");
@@ -52,9 +63,12 @@ class SourcesCommand extends ContainerAwareCommand
         $output->getFormatter()->setStyle('note', $noteStyle);
     }
 
-    protected function executeByApp(InputInterface $input, OutputInterface $output)
+    /**
+     * @param Collector $collector
+     * @return array
+     */
+    protected function buildAppTree(Collector $collector)
     {
-        $collector = new Collector($this->getContainer());
         $aggregate = $collector->collectApplicationInfo();
         $headers = array(
             'Application',
@@ -68,18 +82,19 @@ class SourcesCommand extends ContainerAwareCommand
         foreach ($aggregate->getRelations(false) as $appInfo) {
             $tree->addItem($this->collectAppRelation($appInfo));
         }
-
-        if (!$input->getOption('unused-only')) {
-            $this->renderTable($output, $headers, $tree->toGrid());
-        }
-        if (!$input->getOption('no-unused')) {
-            $this->displayUnusedSources($output, $aggregate->getUnusedSources());
-        }
+        return array(
+            'tree'      => $tree,
+            'headers'   => $headers,
+            'aggregate' => $aggregate,
+        );
     }
 
-    protected function executeBySource(InputInterface $input, OutputInterface $output)
+    /**
+     * @param Collector $collector
+     * @return array
+     */
+    protected function buildSourceTree(Collector $collector)
     {
-        $collector = new Collector($this->getContainer());
         $aggregate = $collector->collectSourceInfo();
         $headers = array(
             'Source',
@@ -91,17 +106,16 @@ class SourcesCommand extends ContainerAwareCommand
         foreach ($aggregate->getRelations() as $srcInfo) {
             $tree->addItem($this->collectSourceRelation($srcInfo));
         }
-        if (!$input->getOption('unused-only')) {
-            $this->renderTable($output, $headers, $tree->toGrid());
-        }
-        if (!$input->getOption('no-unused')) {
-            $this->displayUnusedSources($output, $aggregate->getUnusedSources());
-        }
+        return array(
+            'tree'      => $tree,
+            'headers'   => $headers,
+            'aggregate' => $aggregate,
+        );
     }
 
     /**
      * @param OutputInterface $output
-     * @param Source[] $sources
+     * @param \Mapbender\CoreBundle\Entity\Source[] $sources
      */
     protected function displayUnusedSources(OutputInterface $output, $sources)
     {
@@ -118,10 +132,48 @@ class SourcesCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $collector = new Collector($this->getContainer());
         if ($this->bucketBy == 'application') {
-            $this->executeByApp($input, $output);
+            $result = $this->buildAppTree($collector);
+            $sectionName0 = 'applications';
+            $arraySubHeadings = array('sources', 'instances');
+
         } else {
-            $this->executeBySource($input, $output);
+            $result = $this->buildSourceTree($collector);
+            $sectionName0 = 'sources';
+            $arraySubHeadings = array('applications', 'instances');
+        }
+
+        /** @var DataItem $tree */
+        $tree = $result['tree'];
+        /** @var Application|Source $aggregate */
+        $aggregate = $result['aggregate'];
+
+        $outputFormat = $input->getOption('format');
+        if ($outputFormat == 'table') {
+            if (!$input->getOption('unused-only')) {
+                $tree = $result['tree'];
+                $this->renderTable($output, $result['headers'], $tree->toGrid());
+            }
+            if (!$input->getOption('no-unused')) {
+                /** @var Application|Source $aggregate */
+                $aggregate = $result['aggregate'];
+                $this->displayUnusedSources($output, $aggregate->getUnusedSources());
+            }
+        } else {
+            // Yaml and Json both require building an array representation
+            $dataArray = array();
+            if (!$input->getOption('unused-only')) {
+                $dataArray[$sectionName0] = $tree->toArray($arraySubHeadings);
+            }
+            if (!$input->getOption('no-unused')) {
+                $dataArray['unusedSources'] = $this->sourcesToArray($aggregate->getUnusedSources());
+            }
+            if ($outputFormat == 'json') {
+                $output->writeln(json_encode($dataArray));
+            } else {
+                $output->writeln(Yaml::dump($dataArray, 6000, 4));
+            }
         }
     }
 
@@ -169,6 +221,22 @@ class SourcesCommand extends ContainerAwareCommand
             }
         }
         return $sourceGroup;
+    }
+
+    /**
+     * @param \Mapbender\CoreBundle\Entity\Source[] $sources
+     * @return string[][]
+     */
+    protected function sourcesToArray($sources)
+    {
+        $rv = array();
+        foreach ($sources as $source) {
+            $rv[] = array(
+                'id' => strval($source->getId()),
+                'name' => strval($source->getTitle()),
+            );
+        }
+        return $rv;
     }
 
     /**
