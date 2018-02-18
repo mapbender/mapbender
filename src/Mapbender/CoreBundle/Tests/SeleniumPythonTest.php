4073aa7eb7bb48f89ef0c384935097fb1b85bfa9
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-18 18:03:10 +0100

    Add unit test precondition to check for installed python + selenium module

--- src/Mapbender/CoreBundle/Tests/SeleniumPythonTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPythonTest.php
@@ -12,6 +12,11 @@ class SeleniumPythonTest extends WebTestCase
         if (!version_compare(PHP_VERSION, '5.4', '>=')) {
             $this->markTestSkipped('This test requires PHP >= 5.4');
         }
+        $retCode = null;
+        passthru('python -c "import selenium" 2>/dev/null', $retCode);
+        if ($retCode !== 0) {
+            $this->markTestSkipped('This test requires Python and the selenium Python package');
+        }
     }
 
     public function moduleProvider()
