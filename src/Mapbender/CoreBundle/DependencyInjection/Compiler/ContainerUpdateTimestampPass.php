3408aefcd9958e44619760a2955280157dcb89a6
wg-rolf <rolf.neuberger@wheregroup.com>
2018-04-17 10:49:39 +0200

    [NFC] extend documentation

--- src/Mapbender/CoreBundle/DependencyInjection/Compiler/ContainerUpdateTimestampPass.php
+++ src/Mapbender/CoreBundle/DependencyInjection/Compiler/ContainerUpdateTimestampPass.php
@@ -9,6 +9,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 /**
  * Pseudo-compiler pass that persists the information WHEN the container has been compiled.
  * This information is not accessible via standard Symfony machinery, even though it is generated and used internally.
+ *
+ * The container compilation time is used to invalidate cached data after any configuration changes, including (but
+ * not limited to) changes in yaml-based applications.
  */
 class ContainerUpdateTimestampPass implements CompilerPassInterface
 {
