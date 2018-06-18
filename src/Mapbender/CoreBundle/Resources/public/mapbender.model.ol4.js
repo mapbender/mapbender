7f7a07749989da9f702269375326c38e97821280
David Patzke <david.patzke@wheregroup.com>
2018-06-18 15:40:38 +0200

    Change Model to prototype based object

--- src/Mapbender/CoreBundle/Resources/public/mapbender.model.ol4.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.model.ol4.js
@@ -1,192 +1,69 @@
-Mapbender.Model = {
-    map:            null,
-    mapElement:     null,
-    currentProj:    null,
-    /**
-     *
-     * @param mbMap
-     * @returns {boolean}
-     */
-    init:           function(mbMap) {
+Mapbender.Model = function() {
+    'use strict';
+    this.map = new ol.Map({
+        view:   new ol.View({
+            center: [0, 0],
+            zoom:   1
+        }),
+        layers: [new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })],
+        target: 'Map'
+    });
 
-        this.map = new ol.CanvasMap({
-            view:   new ol.View({
-                center: [0, 0],
-                zoom:   1
-            }),
-            layers: [new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })],
-            target: 'Map'
-        });
-
-        return true;
-    },
-    /**
-     *
-     * @param callback
-     */
-    onMapClick:     function(callback) {
-
-    },
-    /**
-     *
-     * @param callback
-     */
-    onFeatureClick: function(callback) {
-    },
-    /**
-     *
-     * @param layerId
-     * @param styleMap
-     */
-    setLayerStyle:  function(layerId, styleMap) {
-    },
-    /**
-     *
-     * @param styleJson
-     */
-    createStyle:    function(styleJson) {
-    },
-
-    /**
-     *
-     * @returns {*}
-     */
-
-    getActiveLayers: function() {
-        return this.activeLayer;
-    },
-
-    /**
-     *
-     * @param layerId
-     * @param requestParameterBag
-     */
-    setRequestParameter: function(layerId, requestParameterBag) {
-
-    },
-    /**
-     *
-     * @param addLayers
-     */
-    setView:             function(addLayers) {
-
-    },
-    /**
-     *
-     * @returns {*}
-     */
-    getCurrentProj:      function() {
-
-        /** @type String */
-        return this.currentProj;
-    },
-    /**
-     *
-     * @returns {null|*}
-     */
-    getAllSrs:           function() {
-        return this.srsDefs;
-    },
-
-    /**
-     *
-     */
-    getMapExtent: function() {
-
-    },
-    /**
-     *
-     */
-    getMapState:  function() {
-
-    },
-
-    /**
-     * Returns the current map scale
-     */
-    getScale: function() {
-
-    },
-
-
-
-    /**
-     *
-     */
-    center: function(options) {
-
-    },
-
-    /**
-     *
-     */
-    highlightOn:      function(features, options) {
-
-    },
-    /**
-     *
-     */
-    highlightOff:     function(features) {
-
-    },
-    setOpacity:       function(source, opacity) {
-
-    },
-    /**
-     * Zooms to layer
-     * @param {object} options of form { sourceId: XXX, layerId: XXX, inherit: BOOL }
-     */
-    zoomToLayer:      function(options) {
-
-    },
-    getLayerExtents:  function(options) {
-
-    },
-    /**
-     *
-     */
-    addSource:        function(addOptions) {
-
-    },
-    /**
-     *
-     */
-    removeSource:     function(options) {
-
-    },
-    /**
-     *
-     */
-    removeSources:    function(keepSources) {
-
-    },
-    /**
-     *
-     * @param {Object} sourceIdObject in form of:
-     * - source id -> {id: MYSOURCEID}
-     * - mapqyery id -> {mqlid: MYSOURCEMAPQUERYID}
-     * - openlayers id -> {ollid: MYSOURCEOPENLAYERSID}
-     * - origin id -> {ollid: MYSOURCEORIGINID}
-     * @param {Object} options in form of:
-     * {layers:{'LAYERNAME': {options:{treeOptions:{selected: bool,info: bool}}}}}
-     */
-    changeLayerState: function(id, state) {
-
-    },
-    /*
-     * Changes the map's projection.
-     */
-    changeProjection: function(srs) {
-
-    },
-
-    /**
-     *
-     */
-    parseURL: function() {
-
-    }
-
+    return this;
 };
+
+Mapbender.Model.prototype.map = null;
+Mapbender.Model.prototype.mapElement = null;
+Mapbender.Model.prototype.currentProj = null;
+Mapbender.Model.prototype.parseURL = function parseURL() {
+};
+Mapbender.Model.prototype.onMapClick = function onMapClick() {
+    'use strict';
+    return this;
+};
+Mapbender.Model.prototype.onFeatureClick = function onFeatureClick() {
+    'use strict';
+    return this;
+};
+Mapbender.Model.prototype.setLayerStyle = function setLayerStyle() {
+};
+Mapbender.Model.prototype.createStyle = function createStyle() {
+};
+Mapbender.Model.prototype.getActiveLayers = function getActiveLayers() {
+};
+Mapbender.Model.prototype.setRequestParameter = function setRequestParameter() {
+};
+Mapbender.Model.prototype.getCurrentProj = function getCurrentProj() {
+    'use strict';
+    return this.currentProj;
+};
+Mapbender.Model.prototype.getAllSrs = function getAllSrs() {
+};
+Mapbender.Model.prototype.getMapExtent = function getMapExtent() {
+};
+Mapbender.Model.prototype.getScale = function getScale() {
+};
+
+Mapbender.Model.prototype.center = function center() {
+};
+
+Mapbender.Model.prototype.addSource = function addSource() {
+};
+Mapbender.Model.prototype.removeSource = function removeSource() {
+};
+Mapbender.Model.prototype.setLayerOpacity = function setLayerOpacity() {
+};
+Mapbender.Model.prototype.changeProjection = function changeProjection() {
+};
+
+
+
+
+
+
+
+
+
+
