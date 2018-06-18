842e94954899d721949caabf97252bfbf1a4c634
wg-rolf <rolf.neuberger@wheregroup.com>
2018-06-18 08:45:36 +0200

    Remove Map::listAssets

--- src/Mapbender/CoreBundle/Element/Map.php
+++ src/Mapbender/CoreBundle/Element/Map.php
@@ -75,7 +75,7 @@ class Map extends Element
     /**
      * @inheritdoc
      */
-    public static function listAssets()
+    public function getAssets()
     {
         return array(
             'js' => array(
