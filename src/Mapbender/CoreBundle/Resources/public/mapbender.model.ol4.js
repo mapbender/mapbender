f53fab932bfd3226d4a2d1783d3ebb7df7344c88
David Patzke <david.patzke@wheregroup.com>
2018-06-18 11:03:40 +0200

    Add new ol4 model

---
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.model.ol4.js
@@ -0,0 +1,23 @@
+Mapbender.Model = {
+    map : null,
+    mapElement: null,
+    initMap: function(mbMap) {
+
+
+
+    this.map =  new ol.CanvasMap({
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 1
+            }),
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            target: 'map'
+        });
+
+    return true;
+    }
+}
