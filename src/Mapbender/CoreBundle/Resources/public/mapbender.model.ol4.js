fac19b382663a0b4286a17862d98fe8566e03965
Robert Klemm <robert.klemm@wheregroup.com>
2018-06-26 10:15:04 +0200

    fix opt_scaleRating to '000' or '000000'
    https://github.com/mapbender/mapbender/issues/941
    
    This Function to use in mapbender.element.scaledisplay.js return "X000" or "X000000"

--- src/Mapbender/CoreBundle/Resources/public/mapbender.model.ol4.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.model.ol4.js
@@ -143,20 +143,20 @@ Mapbender.Model.prototype.getMapExtent = function getMapExtent() {
 /**
  *
  * @param {number} dpi default 72 DPI
- * @param {boolean} opt_round Whether to round the scale or not.
- * @param {boolean} opt_scaleRating Whether to round the scale rating or not. K:X.000 and M:X.000.000
+ * @param {boolean} optRound Whether to round the scale or not.
+ * @param {boolean} optScaleRating Whether to round the scale rating or not. K:X000 and M:X000000
  * @returns {number}
  */
-Mapbender.Model.prototype.getScale = function getScale(dpi, opt_round, opt_scaleRating) {
+Mapbender.Model.prototype.getScale = function getScale(dpi, optRound, optScaleRating) {
     var resolution = this.map.getView().getResolution();
     var scaleCalc = this.resolutionToScale(resolution, dpi);
-    var scale = opt_round ? Math.round(scaleCalc) : scaleCalc;
+    var scale = optRound ? Math.round(scaleCalc) : scaleCalc;
 
-    if (opt_scaleRating){
+    if (optScaleRating){
         if (scale >= 9500 && scale <= 950000) {
-            scale = Math.round(scale/ 1000) + "K";
+            scale = Math.round(scale/ 1000) + '000';
         } else if (scale >= 950000) {
-            scale = Math.round(scale / 1000000) + "M";
+            scale = Math.round(scale / 1000000) + '000000';
         } else {
             scale = Math.round(scale);
         }
