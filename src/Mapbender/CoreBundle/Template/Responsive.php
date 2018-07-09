4298bb8c2c0638376508dfbb07dda550d8a2e0c0
wg-rolf <rolf.neuberger@wheregroup.com>
2018-07-09 17:52:44 +0200

    Fix dangling references to old datatables js

--- src/Mapbender/CoreBundle/Template/Responsive.php
+++ src/Mapbender/CoreBundle/Template/Responsive.php
@@ -21,7 +21,7 @@ class Responsive extends Fullscreen
         '@FOMCoreBundle/Resources/public/js/frontend/sidepane.js',
         '@FOMCoreBundle/Resources/public/js/frontend/tabcontainer.js',
         '@MapbenderCoreBundle/Resources/public/regional/vendor/notify.0.3.2.min.js',
-        "/components/datatables/media/js/jquery.dataTables.min.js",
+        "/components/datatables/media/js/dataTables.jqueryui.min.js",
         '/components/jquerydialogextendjs/jquerydialogextendjs-built.js',
         "/components/vis-ui.js/vis-ui.js-built.js",
         '@MapbenderCoreBundle/Resources/public/js/responsive.js'
