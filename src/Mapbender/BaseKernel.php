796514abc38c1ab34f1d97dbc64bcc19a8f77c73
wg-rolf <rolf.neuberger@wheregroup.com>
2018-08-01 16:39:40 +0200

    No sensio distribution bundle here

--- src/Mapbender/BaseKernel.php
+++ src/Mapbender/BaseKernel.php
@@ -104,7 +104,6 @@ abstract class BaseKernel extends Kernel
 
         if ($isDevKernel) {
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
