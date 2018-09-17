28cd08850f99ace26542009ccd83c9cfb25691dd
wg-rolf <rolf.neuberger@wheregroup.com>
2018-09-17 15:50:18 +0200

    Fix legends again!

--- src/Mapbender/WmsBundle/Component/WmsInstanceLayerEntityHandler.php
+++ src/Mapbender/WmsBundle/Component/WmsInstanceLayerEntityHandler.php
@@ -361,10 +361,6 @@ class WmsInstanceLayerEntityHandler extends SourceInstanceItemEntityHandler
                 } else {
                     $publicLegendUrl = $glgLegendUrl;
                 }
-                if (false === strpos('?', $publicLegendUrl)) {
-                    $publicLegendUrl .= '?';
-                }
-                $publicLegendUrl = rtrim($publicLegendUrl, '&') . '&layer=' . urlencode($layerName);
                 return array(
                     // this entry in the emitted config is only evaluated by the legend element if configured with
                     // "generateLegendUrl": true
