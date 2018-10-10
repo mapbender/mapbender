4a4b42a3fd5b9b98d745d342a46155f3750bd0a9
David Patzke <david.patzke@wheregroup.com>
2018-10-10 12:02:28 +0200

    Change check for this.popup to this.popupWindow
    property popup is a function so it always will be trueish. To check if the popup is already open we have to check against
    property popupWindow. The old code can cause errors if the legend element is used in a button group and breaks the functionality
    of other buttons in this group.

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.legend.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.legend.js
@@ -433,13 +433,10 @@
 
             if (widget.isPopUpDialog) {
 
-                if (widget.popup) {
-
-                    if (widget.popupWindow.$element) {
+                    if (widget.popupWindow && widget.popupWindow.$element) {
                         widget.popupWindow.destroy();
                         widget.popupWindow = null;
                     }
-                }
 
             }
             if (widget.callback) {
