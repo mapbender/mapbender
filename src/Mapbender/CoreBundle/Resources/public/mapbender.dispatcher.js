6e12ad225f60ddbdbcd171ea5581fe8b7489ca24
Simon Seyock <simon.seyock@benndorf.de>
2018-10-29 16:59:37 +0100

    addDispatcher function

---
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.dispatcher.js
@@ -0,0 +1,3 @@
+(function() {
+    Mapbender.Util.addDispatcher(document);
+})();
--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.featureInfo.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.featureInfo.js
@@ -249,6 +249,7 @@
                         self._addContent(mqLayer, iframe);
                         var doc = document.getElementById(uuid).contentWindow.document;
                         iframe.on('load', function(){
+                            Mapbender.Util.addDispatcher(doc);
                             iframe.data('loaded', true);
                             $('#' + self._getContentManager().headerId(mqLayer.id), self.element).click();
                             iframe.contents().find("body").css("background","transparent");
