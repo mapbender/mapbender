41eb9a489472bd45980a5c687763e7acb02a6e34
wg-rolf <rolf.neuberger@wheregroup.com>
2018-11-14 11:11:34 +0100

    Clean up application asset collection

--- src/Mapbender/CoreBundle/Controller/ApplicationController.php
+++ src/Mapbender/CoreBundle/Controller/ApplicationController.php
@@ -97,7 +97,7 @@ class ApplicationController extends Controller
                 $refs[] = $custom;
             }
             /** @todo: use route to assets action, not REQUEST_URI, so this can move away from here */
-            $factory = new AssetFactory($this->container, $refs, 'css', $request->server->get('REQUEST_URI'), empty($sourcePath) ? "." : $sourcePath);
+            $factory = new AssetFactory($this->container, $refs, 'css', $request->server->get('REQUEST_URI'), $sourcePath ?: ".");
             $content = $factory->compile();
         } else {
             $cache   = new ApplicationAssetCache($this->container, $refs, $type);
