fa0f5428cf1525a67c8e00b92c7f7f6461318086
wg-rolf <rolf.neuberger@wheregroup.com>
2018-11-18 17:03:05 +0100

    Restore asset dependency generation from ManagerTemplate

--- src/Mapbender/ManagerBundle/Template/ManagerTemplate.php
+++ src/Mapbender/ManagerBundle/Template/ManagerTemplate.php
@@ -11,38 +11,48 @@ use Mapbender\CoreBundle\Component\Template;
  */
 class ManagerTemplate extends Template
 {
-    protected static $css = array(
-        '@MapbenderManagerBundle/Resources/public/sass/manager/applications.scss',
-    );
-
-    protected static $js = array(
-        '/components/underscore/underscore-min.js',
-        '/bundles/mapbendercore/regional/vendor/notify.0.3.2.min.js',
-        '/components/datatables/media/js/jquery.dataTables.min.js',
-        '/components/jquerydialogextendjs/jquerydialogextendjs-built.js',
-        '/components/vis-ui.js/vis-ui.js-built.js',
-        '/bundles/fosjsrouting/js/router.js',
-        '@MapbenderManagerBundle/Resources/public/js/SymfonyAjaxManager.js',
-
-        '@MapbenderCoreBundle/Resources/public/widgets/mapbender.popup.js',
-        '@FOMCoreBundle/Resources/public/js/widgets/popup.js',
-        '@FOMCoreBundle/Resources/public/js/widgets/dropdown.js',
-        '@FOMCoreBundle/Resources/public/js/widgets/checkbox.js',
-        '@FOMCoreBundle/Resources/public/js/widgets/radiobuttonExtended.js',
-        '@FOMCoreBundle/Resources/public/js/components.js',
-        '@FOMCoreBundle/Resources/public/js/widgets/collection.js',
-        '@MapbenderCoreBundle/Resources/public/mapbender.trans.js',
-    );
-
-    protected static $translations = array(
-        '@MapbenderManagerBundle/Resources/views/translations.json.twig'
-    );
-
-    /**
-     * @inheritdoc
-     */
-    public function render($format = 'html', $html = true, $css = true, $js = true)
+    public function getAssets($type)
     {
-        return "";
+        switch ($type) {
+            case 'css':
+                return array(
+                    '@MapbenderManagerBundle/Resources/public/sass/manager/applications.scss',
+                );
+            case 'js':
+                return array(
+                    '/components/underscore/underscore-min.js',
+                    '/bundles/mapbendercore/regional/vendor/notify.0.3.2.min.js',
+                    '/components/datatables/media/js/jquery.dataTables.min.js',
+                    '/components/jquerydialogextendjs/jquerydialogextendjs-built.js',
+                    '/components/vis-ui.js/vis-ui.js-built.js',
+                    '/bundles/fosjsrouting/js/router.js',
+                    '@MapbenderManagerBundle/Resources/public/js/SymfonyAjaxManager.js',
+
+                    '@MapbenderCoreBundle/Resources/public/widgets/mapbender.popup.js',
+                    '@FOMCoreBundle/Resources/public/js/widgets/popup.js',
+                    '@FOMCoreBundle/Resources/public/js/widgets/dropdown.js',
+                    '@FOMCoreBundle/Resources/public/js/widgets/checkbox.js',
+                    '@FOMCoreBundle/Resources/public/js/widgets/radiobuttonExtended.js',
+                    '@FOMCoreBundle/Resources/public/js/components.js',
+                    '@FOMCoreBundle/Resources/public/js/widgets/collection.js',
+                    '@MapbenderCoreBundle/Resources/public/mapbender.trans.js',
+                );
+            case 'trans':
+                return array(
+                    '@MapbenderManagerBundle/Resources/views/translations.json.twig',
+                );
+            default:
+                return parent::getAssets($type);
+        }
+    }
+
+    public function getTwigTemplate()
+    {
+        // NOTE: Old versions had an overriden render method that returns an empty string.
+        //       Url path /application/manager itself never functioned
+        //       Actual rendering of the backend is in WelcomeController::listAction, which uses the template
+        //       MapbenderCoreBundle:Welcome:list.html.twig
+        //       Url paths /application/manager/assets/js et al
+        throw new \RuntimeException("ManagerTemplate does not support standard rendering, only provides asset dependencies");
     }
 }
