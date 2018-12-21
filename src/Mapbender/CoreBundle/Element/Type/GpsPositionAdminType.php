20cb50bdb7334293b62c1e6204b84aae50cff073
inachtigall <igor.nachtigall@wheregroup.com>
2018-12-21 11:06:20 +0100

    remove duplicate use of OptionsResolver

--- src/Mapbender/CoreBundle/Element/Type/GpsPositionAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/GpsPositionAdminType.php
@@ -5,7 +5,6 @@ namespace Mapbender\CoreBundle\Element\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class GpsPositionAdminType
