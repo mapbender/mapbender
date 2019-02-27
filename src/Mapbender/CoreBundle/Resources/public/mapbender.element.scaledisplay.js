195223612fd7e3d8b02cb1ec84c2254df9cb0712
wg-rolf <rolf.neuberger@wheregroup.com>
2019-02-27 16:25:08 +0100

    Clean up / document scaledisplay control monkey-patching

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.scaledisplay.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.scaledisplay.js
@@ -5,6 +5,7 @@
 //            unitPrefix: false
         },
         scaledisplay: null,
+        preDisplay: null,
 
         /**
          * Creates the scale display
@@ -14,6 +15,7 @@
                 return;
             }
             var self = this;
+            this.preDisplay = document.createElement('div');
             Mapbender.elementRegistry.onElementReady(this.options.target, $.proxy(self._setup, self));
         },
         
@@ -25,42 +27,45 @@
                 this.options.unitPrefix = false;
             var mbMap = $('#' + this.options.target).data('mapbenderMbMap');
             
-            var projection = mbMap.map.olMap.getProjectionObject();
-            var options = {
-                geodesic: projection.units === 'degrees' ? true : false
-            };
-            options["updateScale"] =  $.proxy(this._updateScale, this);
-            this.scaledisplay = new OpenLayers.Control.Scale($(this.element).find("span").get(0), options);
-            
+            this.scaledisplay = this._initializeControl();
             mbMap.map.olMap.addControl(this.scaledisplay);
             $(document).bind('mbmapsrschanged', $.proxy(this._changeSrs, this));
             this._trigger('ready');
         },
-        _updateScale: function(){
-            var scale = this.scaledisplay.map.getScale();
-            if (!scale) {
-                return;
-            }
-            if(this.options.unitPrefix){
-                if (scale >= 9500 && scale <= 950000) {
-                    scale = Math.round(scale / 1000) + "K";
-                } else if (scale >= 950000) {
-                    scale = Math.round(scale / 1000000) + "M";
-                } else {
-                    scale = Math.round(scale);
-                }    
-            } else{
-                scale = Math.round(scale);
-            }
-            this.scaledisplay.element.innerHTML = OpenLayers.i18n(" 1 : ${scaleDenom}", {'scaleDenom':scale});
+        _initializeControl: function() {
+            var control = new OpenLayers.Control.Scale(this.preDisplay);
+            var targetElement = $('>span', this.element).get(0);
+            var updateText = this._cleanupDisplayText.bind(this);
+            // Monkey-patch .updateScale method to massage the displayed text and forward it
+            // to targetElement
+            control.updateScale = function() {
+                /** @type {OpenLayers.Control.Scale} this */
+                OpenLayers.Control.Scale.prototype.updateScale.call(this);
+                // noinspection JSPotentiallyInvalidUsageOfThis
+                targetElement.innerHTML = updateText(this.element.innerHTML);
+            };
+            return control;
         },
-        /**
-         * Cahnges the scale bar srs
-         */
-        _changeSrs: function(event, srs){
-            this.scaledisplay.updateScale();
+        _cleanupDisplayText: function(textIn) {
+            // Openlayers 2.13.1 does not (yet) support a customizable template
+            // strip off the hard-coded, untranslatable "Scale = " prefix
+            // see https://github.com/openlayers/ol2/blob/release-2.13.1/lib/OpenLayers/Control/Scale.js#L95
+            var textOut = textIn.replace(/^Scale\s*=\s*/, '');
+            if (!this.options.unitPrefix) {
+                // Convert unit suffixes back to digits
+                // see https://github.com/openlayers/ol2/blob/release-2.13.1/lib/OpenLayers/Control/Scale.js#L87
+                textOut = textOut.replace(/(\d*)([MK])/g, function(match, digits, suffix) {
+                    if (suffix === 'M') {
+                        return digits + '000000';
+                    } else if (suffix === 'K') {
+                        return digits + '000';
+                    } else {
+                        return digits;
+                    }
+                });
+            }
+            return textOut;
         }
-        
     });
 
 })(jQuery);
