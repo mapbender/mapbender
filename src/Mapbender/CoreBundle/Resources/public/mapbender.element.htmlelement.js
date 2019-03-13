0c7029f367588c83ed76713e0e314493fb894a1f
wg-rolf <rolf.neuberger@wheregroup.com>
2019-03-13 19:47:20 +0100

    Revert "add mapbender.generate.form.js and extend mapbender html element"
    This reverts commit 0f89796303ebf06e2ca3538921def1df521c662b.

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.htmlelement.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.htmlelement.js
@@ -1,22 +1,3 @@
 (function($) {
-    $.widget("mapbender.mbHTMLElement", {
-        /**
-         * @todo 3.0.8.0: this widget should do absolutely nothing. Its 'content' is rendered server side.
-         *   mapbender/data-source's "BaseElement" is the lowest point where vis-ui may be required, not
-         *   core mapbender, not in a simple HTML element.
-         */
-        _create: function() {
-            var widget = this;
-            var element = $(widget.element);
-            var options = widget.options;
-
-            if(options.hasOwnProperty('items')) {
-                var items = $.isArray(options.items) ? options.items : [options.items];
-                if(items[0].type == "popup") {
-                    element = $("<div/>");
-                }
-                element.generateElements({items: items});
-            }
-        }
-    });
+    $.widget("mapbender.mbHTMLElement", {});
 })(jQuery);
