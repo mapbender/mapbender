5d991c01c2b2d83ea5e43306c6ef9d2ed5958516
wg-rolf <rolf.neuberger@wheregroup.com>
2019-04-02 15:08:25 +0200

    [OL2] move Wms layer creation to MapEngine

--- src/Mapbender/CoreBundle/Resources/public/mapbender-model/MapEngineOl2.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender-model/MapEngineOl2.js
@@ -21,6 +21,46 @@ window.Mapbender.MapEngineOl2 = (function() {
             // Allow drag pan motion to continue outside of map div. Great for multi-monitor setups.
             OpenLayers.Control.Navigation.prototype.documentDrag = true;
             Mapbender.MapEngine.prototype.patchGlobals.apply(this, arguments);
+        },
+        createWmsLayer: function(source) {
+            var options = getNativeLayerOptions(source);
+            var params = getNativeLayerParams(source);
+            var url = source.configuration.options.url;
+            var name = source.title;
+            return new OpenLayers.Layer.WMS(name, url, params, options);
+
+            function getNativeLayerOptions(source) {
+                var rootLayer = source.configuration.children[0];
+                var bufferConfig = source.configuration.options.buffer;
+                var ratioConfig = source.configuration.options.ratio;
+                var opts = {
+                    isBaseLayer: false,
+                    opacity: source.configuration.options.opacity,
+                    visibility: source.configuration.options.visible,
+                    singleTile: !source.configuration.options.tiled,
+                    noMagic: true,
+                    minScale: rootLayer.minScale,
+                    maxScale: rootLayer.maxScale
+                };
+                if (opts.singleTile) {
+                    opts.ratio = parseFloat(ratioConfig) || 1.0;
+                } else {
+                    opts.buffer = parseInt(bufferConfig) || 0;
+                }
+                return opts;
+            }
+            function getNativeLayerParams(source) {
+                var params = $.extend({}, source.customParams, {
+                    transparent: source.configuration.options.transparent,
+                    format: source.configuration.options.format,
+                    version: source.configuration.options.version
+                });
+                var exceptionFormatConfig = source.configuration.options.exception_format;
+                if (exceptionFormatConfig) {
+                    params.exceptions = exceptionFormatConfig;
+                }
+                return params;
+            }
         }
     });
     window.Mapbender.MapEngine.typeMap['ol2'] = MapEngineOl2;
