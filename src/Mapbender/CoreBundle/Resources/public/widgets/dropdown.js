44bf36193768eb80163ebf95f728b74cc5d5d0d0
werrolf <rolf.neuberger@wheregroup.com>
2019-06-17 16:50:09 +0200

    Support grouped options in from theme

--- src/Mapbender/CoreBundle/Resources/public/widgets/dropdown.js
+++ src/Mapbender/CoreBundle/Resources/public/widgets/dropdown.js
@@ -2,55 +2,77 @@
  * Migrated to Mapbender from FOM v3.0.6.3
  * See https://github.com/mapbender/fom/tree/v3.0.6.3/src/FOM/CoreBundle/Resources/public/js/widgets
  */
-var initDropdown = function () {
-    var me = $(this);
-    var dropdownList = me.find(".dropdownList");
-    if (dropdownList.children().length === 0) {
-        me.find("option").each(function (i, e) {
-            $(e).addClass("opt-" + i);
-            var node = $('<li class="item-' + i + '"></li>');
-            dropdownList.append(node);
-            node.text($(e).text());
+$(function () {
+    function fixOptions(scope) {
+        // Update (potentially runtime generated) dropdown markup,
+        // replace correlating opt-... and item-... classes with
+        // a 'data-value' attribute and a 'choice' class on the display item
+        // matching requires an implicit hyphen, see https://api.jquery.com/attribute-contains-prefix-selector/
+        $('select option[class|="opt"]', scope).each(function() {
+            var $opt = $(this);
+            var optClass = ($opt.attr('class').match(/opt-\d+/) || [])[0];
+
+            if (optClass) {
+                $opt.removeClass(optClass);
+                var itemClass = optClass.replace('opt-', 'item-');
+                var $displayItem = $('.' + itemClass, scope);
+                $displayItem.attr('data-value', $opt.attr('value'));
+                $displayItem.addClass('choice');
+                $displayItem.removeClass(itemClass);
+            }
         });
     }
-    var select = me.find("select").val();
-    me.find(".dropdownValue").text(me.find('option[value="'+select+'"]').text())
-};
-$(function () {
+    function initDropdown() {
+        fixOptions(this);
+        var $select = $('select', this);
+
+        var me = $(this);
+        var dropdownList = me.find(".dropdownList");
+        if (dropdownList.children().length === 0) {
+            me.find("option").each(function (i, e) {
+                $(e).addClass("opt-" + i);
+                var node = $('<li class="item-' + i + '"></li>');
+                dropdownList.append(node);
+                node.text($(e).text());
+            });
+        }
+        $(".dropdownValue", this).text($('option:selected', $select).text());
+    }
     // init dropdown list --------------------------------------------------------------------
 
-    var toggleList = function () {
+    function toggleList() {
+        fixOptions(this);
         var me = $(this);
-        var list = me.find(".dropdownList");
-        var opts = me.find(".hiddenDropdown");
-        if (list.css("display") == "block") {
+        var list = $('.dropdownList', this);
+        var opts = $('.hiddenDropdown', this);
+        if (list.css("display") === "block") {
             list.hide();
         } else {
-            $(".dropdownList").hide();
-            list.show();
-            list.find("li").one("click", function (event) {
+            list.one('click', 'li.choice', function (event) {
+                var $target = $(this);
+                var val = $target.attr('data-value');
                 event.stopPropagation();
-                list.hide().find("li").off("click");
-                var me2 = $(this);
-                var opt = me2.attr("class").replace("item", "opt");
-                me.find(".dropdownValue").text(me2.text());
-                var val = opts.find("." + opt).prop("selected", true).val();
-                opts.val(val).trigger('change');
-            })
+                var opt = $('option[value="' + val.replace('"', '\\"') + '"]', opts);
+                me.find(".dropdownValue").text(opt.text());
+                opts.val(opt.val());
+                opts.trigger('change');
+                list.hide();
+            });
+            list.show();
         }
 
         $(document).one("click", function () {
-            list.hide().find("li").off("mouseout").off("click");
+            list.hide().find("li").off("click", 'li.choice');
         });
         return false;
     }
     $('.dropdown').each(function () {
         initDropdown.call(this);
     });
+    window.initDropdown = initDropdown;
     $(document).on("click", ".dropdown", toggleList);
     $(document).on('mousewheel scroll DOMMouseScroll', '.dropdownList', function(e) {
         var delta = e.originalEvent.detail;
-        var height = this.scrollTopMax;
         var atTop = this.scrollTop === 0;
         var atBottom = this.scrollTop === this.scrollTopMax;
 
