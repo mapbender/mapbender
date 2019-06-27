a11cf0656091acbad0736009e4857ebceed579b8
werrolf <rolf.neuberger@wheregroup.com>
2019-06-27 09:25:57 +0200

    Make form of newly added BaseSourceSwitcher work immediately

--- src/Mapbender/CoreBundle/Element/Type/BaseSourceSwitcherAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/BaseSourceSwitcherAdminType.php
@@ -2,10 +2,8 @@
 
 namespace Mapbender\CoreBundle\Element\Type;
 
-use Doctrine\Common\Collections\Criteria;
 use Mapbender\CoreBundle\Component\ExtendedCollection;
-use Mapbender\CoreBundle\Entity\Application;
-use Mapbender\CoreBundle\Entity\Element;
+use Mapbender\CoreBundle\Element\Type\Subscriber\BaseSourceSwitcherMapTargetSubscriber;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -40,50 +38,16 @@ class BaseSourceSwitcherAdminType extends AbstractType implements ExtendedCollec
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        /** @var Application $application */
-        $application = $options["application"];
-        $element = $options["element"];
-        $instances = array();
-        if ($element !== null && $element->getId() !== null) {
-            $configuration = $element->getConfiguration();
-            $targetId = intval($configuration['target']);
-            $targetCriteria = Criteria::create()->where(Criteria::expr()->eq('id', $targetId));
-            $targetElement = $application->getElements()->matching($targetCriteria)->first();
-            if ($targetElement) {
-                /** @var Element $targetElement */
-                $mapconfig = $targetElement->getConfiguration();
-                foreach ($application->getLayersets() as $layerset_) {
-                    if ((isset($mapconfig['layerset'])
-                        && strval($mapconfig['layerset']) === strval($layerset_->getId()))
-                        || (isset($mapconfig['layersets'])
-                        && in_array($layerset_->getId(), $mapconfig['layersets']))) {
-                        foreach ($layerset_->getInstances() as $instance) {
-                            if ($instance->isBasesource() && $instance->getEnabled()) {
-                                $instances[strval($instance->getId())] = $instance->getTitle();
-                            }
-                        }
-                    }
-                }
-            }
-        }
         $builder
-            ->add('title', 'text', array('required' => true))
-            ->add('tooltip', 'text', array('required' => false))
-            ->add('target', 'target_element',
-                  array(
+            ->add('title', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('required' => true))
+            ->add('tooltip', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('required' => false))
+            ->add('target', 'target_element', array(
                 'element_class' => 'Mapbender\\CoreBundle\\Element\\Map',
-                'application' => $application,
+                'application' => $options['application'],
                 'property_path' => '[target]',
-                'required' => false));
-        if (count($instances) > 0) {
-            $builder->add('instancesets', "collection", array(
-                'property_path' => '[instancesets]',
-                'type' => new InstanceSetAdminType(),
-                'allow_add' => true,
-                'allow_delete' => true,
-                'auto_initialize' => false,
-                'options' => array('instances' => $instances)
-            ));
-        }
+                'required' => false,
+            ))
+        ;
+        $builder->addEventSubscriber(new BaseSourceSwitcherMapTargetSubscriber($options['application']));
     }
 }
