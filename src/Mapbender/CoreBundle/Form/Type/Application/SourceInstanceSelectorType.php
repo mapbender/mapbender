d98ba7e51fbd12e7aa7c58563c9021d2848ae6d5
werrolf <rolf.neuberger@wheregroup.com>
2019-09-27 04:53:01 +0200

    Extract base class RelatedObjectChoiceType from SourceInstanceSelectorType

--- src/Mapbender/CoreBundle/Form/Type/Application/SourceInstanceSelectorType.php
+++ src/Mapbender/CoreBundle/Form/Type/Application/SourceInstanceSelectorType.php
@@ -4,7 +4,8 @@
 namespace Mapbender\CoreBundle\Form\Type\Application;
 
 use Mapbender\CoreBundle\Entity\Application;
-use Symfony\Component\Form\AbstractType;
+use Mapbender\CoreBundle\Entity\SourceInstance;
+use Mapbender\CoreBundle\Form\Type\RelatedObjectChoiceType;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +14,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  * Application entity must be passed in options under 'application'.
  * Submit value is the SourceInstance's id.
  */
-class SourceInstanceSelectorType extends AbstractType
+class SourceInstanceSelectorType extends RelatedObjectChoiceType
 {
     public function getName()
     {
         return 'application_source_instance_selector';
-    }
-
-    public function getParent()
-    {
-        return 'choice';
     }
 
     /**
@@ -30,34 +26,40 @@ class SourceInstanceSelectorType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        // thank you PHP for not allowing "use $this" in a lambda
-        $self = $this;
-        $resolver->setDefaults(array(
-            'choices' => function (Options $options) use ($self) {
-                return $self->getOptions($options['application']);
-            },
-            'choices_as_values' => true,
-            'placeholder' => null,
-            'required' => true,
-        ));
+        parent::configureOptions($resolver);
         $resolver->setRequired(array(
-            'application'
+            'application',
+        ));
+        $resolver->setDefaults(array(
+            'label_with_layerset_prefix' => true,
+            'choice_label' => function(Options $options) {
+                if ($options['label_with_layerset_prefix']) {
+                    return function($choice) {
+                        /** @var SourceInstance $choice*/
+                        $label = ltrim($choice->getLayerset()->getTitle() . ': ', ' :');
+                        $label .= $choice->getTitle();
+                        return $label;
+                    };
+                } else {
+                    return 'title';
+                }
+            },
+            'parent_object' => function(Options $options) {
+                return $options['application'];
+            },
         ));
     }
 
-    protected function getOptions(Application $application)
+    protected function getRelatedObjectCollection($parentObject)
     {
-        $options = array();
-        foreach ($application->getLayersets() as $layerset) {
-            $lsTitle = $layerset->getTitle();
+        $instances = array();
+        /** @var Application $parentObject */
+        foreach ($parentObject->getLayersets() as $layerset) {
             foreach ($layerset->getInstances() as $instance) {
-                $instTitle = $instance->getTitle();
-                $label = "{$lsTitle}: {$instTitle}";
-                $value = $instance->getId();
-                $options[$label] = $value;
+                $instances[$instance->getId()] = $instance;
             }
         }
-        ksort($options);
-        return $options;
+        ksort($instances);
+        return $instances;
     }
 }
