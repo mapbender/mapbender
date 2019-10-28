7287cc92a85b1a5620bd67b0c53e2aa9c3271f1f
werrolf <rolf.neuberger@wheregroup.com>
2019-10-28 07:43:39 +0100

    [NFC] Make ScssValidator usage explicit

--- src/Mapbender/CoreBundle/Validator/Constraints/Scss.php
+++ src/Mapbender/CoreBundle/Validator/Constraints/Scss.php
@@ -11,17 +11,8 @@ use Symfony\Component\Validator\Constraint;
  */
 class Scss extends Constraint
 {
-    /**
-     * Returns the name of the class that validates this constraint.
-     *
-     * By default, this is the fully qualified name of the constraint class
-     * suffixed with "Validator". You can override this method to change that
-     * behaviour.
-     *
-     * @return string
-     */
     public function validatedBy()
     {
-        return get_class($this) . 'Validator';
+        return 'Mapbender\CoreBundle\Validator\Constraints\ScssValidator';
     }
 }
