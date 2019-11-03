e042aa3caedcc04c03082bd903d1624ef082b4d6
werrolf <rolf.neuberger@wheregroup.com>
2019-11-03 20:50:29 +0100

    Supply validation error messages (line + snippet) for yaml-type form fields

--- src/Mapbender/ManagerBundle/Form/Type/YAMLConfigurationType.php
+++ src/Mapbender/ManagerBundle/Form/Type/YAMLConfigurationType.php
@@ -3,17 +3,22 @@
 namespace Mapbender\ManagerBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 use Symfony\Component\Form\FormBuilderInterface;
 use Mapbender\ManagerBundle\Form\DataTransformer\YAMLDataTransformer;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 class YAMLConfigurationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->resetViewTransformers()
-            ->addViewTransformer(new YAMLDataTransformer());
+        $builder->addViewTransformer(new YAMLDataTransformer());
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'preSubmit'));
     }
 
     public function getParent()
@@ -26,6 +31,17 @@ class YAMLConfigurationType extends AbstractType
         return 'yaml_configuration';
     }
 
+    public function preSubmit(FormEvent $event)
+    {
+        try {
+            Yaml::parse($event->getData() ?: '');
+        } catch (ParseException $e) {
+            $event->getForm()->addError(new FormError($e->getMessage()));
+            // prevent further processing
+            throw new TransformationFailedException($e->getMessage(), 0, $e);
+        }
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
@@ -36,4 +52,3 @@ class YAMLConfigurationType extends AbstractType
         ));
     }
 }
-
