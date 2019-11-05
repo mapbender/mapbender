a0af61e1ed245a169ca2fefc97cc9d98fda6ecd0
werrolf <rolf.neuberger@wheregroup.com>
2019-11-05 08:01:28 +0100

    Reverse ElementFormFactory deprecation strategy: FQCNs good, named services bad; return FQCNs instead of instances for Symfony 3 compat

--- src/Mapbender/ManagerBundle/Component/ElementFormFactory.php
+++ src/Mapbender/ManagerBundle/Component/ElementFormFactory.php
@@ -96,7 +96,7 @@ class ElementFormFactory extends BaseElementFactory
 
     /**
      * @param Element $element
-     * @return string
+     * @return string|\Mapbender\CoreBundle\Component\Element
      */
     protected function getComponentClass(Element $element)
     {
@@ -105,31 +105,35 @@ class ElementFormFactory extends BaseElementFactory
 
     /**
      * @param Element $element
-     * @return FormTypeInterface|null
+     * @return string|null
      * @throws \RuntimeException
      * @throws ServiceNotFoundException
      */
     public function getConfigurationFormType(Element $element)
     {
         $componentClassName = $this->getComponentClass($element);
-        $type = $this->getServicyConfigurationFormType($element) ?: ($componentClassName::getType());
-        if ($type === null) {
-            return null;
-        }
-
-        if (is_string($type)) {
-            if (is_a($type, 'Symfony\Component\Form\FormTypeInterface', true)) {
-                return new $type();
-            } else {
-                throw new \RuntimeException("No such form type " . print_r($type, true));
+        $typeName = $componentClassName::getType();
+        if (is_string($typeName) && preg_match('#^[\w\d]+(\\\\[\w\d]+)+$#', $typeName)) {
+            // typename is a fully qualified class name, which is good (forward compatible with Symfony 3)
+            if (!is_a($typeName, 'Symfony\Component\Form\FormTypeInterface', true)) {
+                throw new \RuntimeException("Not a valid form type " . print_r($typeName, true));
             }
+            return $typeName;
         }
 
-        if ($type instanceof FormTypeInterface) {
-            return $type;
-        } else {
-            throw new \RuntimeException("Invalid form type from " . print_r($componentClassName, true));
+        $type = $this->getServicyConfigurationFormType($element);
+        if ($type) {
+            $this->deprecated(
+                "Detected element form type service for {$componentClassName}. Please stop relying on "
+              . "re-plugging form type services, and instead use form type extensions to transparently alter existing types. "
+              . "See https://symfony.com/doc/2.8/form/create_form_type_extension.html");
+
+            if (!($type instanceof FormTypeInterface)) {
+                throw new \RuntimeException("Invalid form type " . get_class($type) . " from " . print_r($componentClassName, true));
+            }
+            return \get_class($type);
         }
+        return null;
     }
 
     public function getFallbackConfigurationFormType(Element $element)
@@ -157,12 +161,13 @@ class ElementFormFactory extends BaseElementFactory
         if (is_string($typeDeclaration) && false !== strpos($typeDeclaration, '.')) {
             return $this->container->get($typeDeclaration);
         }
+        /** @todo: remove automatic service name inflection magic */
         $classNamespaceParts = explode('\\', $componentClassName);
         $baseName = strtolower(array_pop($classNamespaceParts));
         $automaticServiceId = 'mapbender.form_type.element.' . $baseName;
         $automaticService = $this->container->get($automaticServiceId, ContainerInterface::NULL_ON_INVALID_REFERENCE);
         if ($automaticService && $typeDeclaration !== $automaticServiceId) {
-            $this->deprecated("Located undeclared servicy form type {$automaticServiceId} for {$componentClassName}, please return the full service name explicitly from your getType implementation");
+            $this->deprecated("Located undeclared servicy form type {$automaticServiceId} for {$componentClassName}; return the fully qualified class name of the form type from your element's getType instead");
         }
         return $automaticService; // may also be null
     }
