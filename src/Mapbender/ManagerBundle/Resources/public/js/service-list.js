0f06667507e393b89cb6ceb5c2aacd452f6bab6c
werrolf <rolf.neuberger@wheregroup.com>
2019-11-27 07:07:05 +0100

    Show list of affected applications in source delete confirmation popup

--- src/Mapbender/ManagerBundle/Resources/public/js/service-list.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/service-list.js
@@ -1,13 +1,21 @@
 $(function() {
     $('#listFilterServices').on('click', '.iconRemove[data-url]', function() {
         var $el = $(this);
-        Mapbender.Manager.confirmDelete($el, $el.attr('data-url'), {
-            // @todo: bring your own translation string
-            title: "mb.manager.components.popup.delete_element.title",
-            // @todo: bring your own translation string
-            cancel: "mb.manager.components.popup.delete_element.btn.cancel",
-            // @todo: bring your own translation string
-            confirm: "mb.manager.components.popup.delete_element.btn.ok"
+        var url = $el.attr('data-url');
+        var preText = $el.attr('title');    // HACK
+        $.ajax(url, {
+            method: 'GET'
+        }).then(function(response) {
+            var content = ['<p>', preText, '</p>', response].join('');
+            var stringMap = {
+                // @todo: bring your own translation string
+                title: "mb.manager.components.popup.delete_element.title",
+                // @todo: bring your own translation string
+                cancel: "mb.manager.components.popup.delete_element.btn.cancel",
+                // @todo: bring your own translation string
+                confirm: "mb.manager.components.popup.delete_element.btn.ok"
+            };
+            Mapbender.Manager.confirmDelete($el, url, stringMap, content);
         });
         return false;
     });
