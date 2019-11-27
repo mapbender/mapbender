8e99d2b37224ebdc027d7f2c8b199e9132470ce3
werrolf <rolf.neuberger@wheregroup.com>
2019-11-27 07:11:59 +0100

    Fix Source confirmdelete.html.twig template

--- src/Mapbender/ManagerBundle/Resources/public/js/service-list.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/service-list.js
@@ -2,11 +2,9 @@ $(function() {
     $('#listFilterServices').on('click', '.iconRemove[data-url]', function() {
         var $el = $(this);
         var url = $el.attr('data-url');
-        var preText = $el.attr('title');    // HACK
         $.ajax(url, {
             method: 'GET'
         }).then(function(response) {
-            var content = ['<p>', preText, '</p>', response].join('');
             var stringMap = {
                 // @todo: bring your own translation string
                 title: "mb.manager.components.popup.delete_element.title",
@@ -15,7 +13,7 @@ $(function() {
                 // @todo: bring your own translation string
                 confirm: "mb.manager.components.popup.delete_element.btn.ok"
             };
-            Mapbender.Manager.confirmDelete($el, url, stringMap, content);
+            Mapbender.Manager.confirmDelete($el, url, stringMap, response);
         });
         return false;
     });
