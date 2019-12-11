c57fa0f6c7db51e73ae931ff8ea2b881720b218c
werrolf <rolf.neuberger@wheregroup.com>
2019-12-11 10:43:59 +0100

    [NFC] document wmsloader instance detection logic conflicts with reusable source instances

--- src/Mapbender/WmsBundle/Component/Presenter/WmsSourceService.php
+++ src/Mapbender/WmsBundle/Component/Presenter/WmsSourceService.php
@@ -338,6 +338,7 @@ class WmsSourceService extends SourceService
     public function useTunnel(SourceInstance $sourceInstance)
     {
         if ($sourceInstance->getLayerset()) {
+            // @todo: reusable source instances: use a proper detection method for wmsloader; this logic is conflicting with Instances that are no longer owned by a single Layerset
             /** @var WmsInstance $sourceInstance */
             $vsHandler = new VendorSpecificHandler();
             return (!!$sourceInstance->getSource()->getUsername()) || $vsHandler->hasHiddenParams($sourceInstance);
@@ -357,6 +358,7 @@ class WmsSourceService extends SourceService
             return false;
         } else {
             if ($sourceInstance->getSource()->getUsername() && !$sourceInstance->getLayerset()) {
+                // @todo: reusable source instances: use a proper detection method for wmsloader; this logic is conflicting with Instances that are no longer owned by a single Layerset
                 // WmsLoader special: proxify url with embedded credentials to bypass browser
                 // filtering of basic auth in img tags.
                 // see https://stackoverflow.com/questions/3823357/how-to-set-the-img-tag-with-basic-authentication
