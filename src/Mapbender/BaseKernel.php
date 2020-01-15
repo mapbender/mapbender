d4e60530ca3078f96f5474ac8f988141c6ff747b
werrolf <rolf.neuberger@wheregroup.com>
2020-01-15 11:57:01 +0100

    Remove AsseticBundle

--- src/Mapbender/BaseKernel.php
+++ src/Mapbender/BaseKernel.php
@@ -87,7 +87,6 @@ abstract class BaseKernel extends Kernel
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
