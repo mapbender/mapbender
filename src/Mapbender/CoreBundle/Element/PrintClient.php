7d922fa6c58f7afcc2d8e1907638e40df6a8f4bc
werrolf <rolf.neuberger@wheregroup.com>
2020-06-02 16:58:52 +0200

    Revert "Nuke deprecated class" (Symfony 3 debug class loader does not like)
    This reverts commit c60d8835f2374aa13536224f29cee9c5c2ebd820.

---
+++ src/Mapbender/CoreBundle/Element/PrintClient.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Mapbender\CoreBundle\Element;
+
+/**
+ * @deprecated Provided for compatibility with existing installations only
+ * Cannot be added to applications (not listed in Bundle's getElements)
+ */
+class PrintClient extends \Mapbender\PrintBundle\Element\PrintClient
+{
+}
