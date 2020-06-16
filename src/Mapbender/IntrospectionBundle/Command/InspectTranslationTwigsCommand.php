cfe186cc5681a75b0651c3ea277ef47eb2fd7e64
werrolf <rolf.neuberger@wheregroup.com>
2020-06-16 16:49:55 +0200

    Fix twig inspection errors vs Application-dependent asset steering

--- src/Mapbender/IntrospectionBundle/Command/InspectTranslationTwigsCommand.php
+++ src/Mapbender/IntrospectionBundle/Command/InspectTranslationTwigsCommand.php
@@ -100,6 +100,7 @@ class InspectTranslationTwigsCommand extends ContainerAwareCommand
 
     protected function collectElementResourcePaths()
     {
+        $dummyApplication = new Application();
         $twigPaths = array();
         /** @var ElementInventoryService $service */
         $service = $this->getContainer()->get('mapbender.element_inventory.service');
@@ -107,7 +108,7 @@ class InspectTranslationTwigsCommand extends ContainerAwareCommand
         $factory = $this->getContainer()->get('mapbender.element_factory.service');
         $classNames = $service->getRawInventory();
         foreach ($classNames as $className) {
-            $element = $factory->newEntity($className, 'content');
+            $element = $factory->newEntity($className, 'content', $dummyApplication);
             $componentDummy = $factory->componentFromEntity($element);
             $assetDependencies = $componentDummy->getAssets();
             if (!empty($assetDependencies['trans'])) {
