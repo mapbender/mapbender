7fa94ddaebe58e473b97b69f76684abff82a01fe
werrolf <rolf.neuberger@wheregroup.com>
2020-07-17 11:01:54 +0200

    [reusable source instances] fold copy&paste, fix doc blocks

--- src/Mapbender/CoreBundle/Entity/Repository/ApplicationRepository.php
+++ src/Mapbender/CoreBundle/Entity/Repository/ApplicationRepository.php
@@ -24,8 +24,8 @@ class ApplicationRepository extends EntityRepository
      * @param Source $source
      * @param array|null $criteria
      * @param array|null $orderBy
-     * @param null $limit
-     * @param null $offset
+     * @param int|null $limit
+     * @param int|null $offset
      * @return Application[]
      */
     public function findWithInstancesOf(Source $source, array $criteria=null, array $orderBy = null, $limit = null, $offset = null)
@@ -49,19 +49,19 @@ class ApplicationRepository extends EntityRepository
             }
         }
         $applications = array_values($applications);
-        if ($orderBy) {
-            $applications = new ArrayCollection($applications);
-            $applications = $applications->matching(Criteria::create()->orderBy($orderBy))->getValues();
-        }
-        if ($offset) {
-            $applications = array_slice($applications, $offset);
-        }
-        if ($limit) {
-            $applications = array_slice($applications, $limit);
-        }
+        $applications = $this->applyOrderBy($applications, $orderBy);
+        $applications = $this->sliceOffsetLimit($applications, $offset, $limit);
         return $applications;
     }
 
+    /**
+     * @param SourceInstance $instance
+     * @param array|null $criteria
+     * @param array|null $orderBy
+     * @param int|null $limit
+     * @param int|null $offset
+     * @return Application[]
+     */
     public function findWithSourceInstance(SourceInstance $instance, array $criteria = null, array $orderBy = null, $limit = null, $offset = null)
     {
         /** @var LayersetRepository $layersetRepository */
@@ -83,10 +83,33 @@ class ApplicationRepository extends EntityRepository
             }
         }
         $applications = array_values($applications);
+        $applications = $this->applyOrderBy($applications, $orderBy);
+        $applications = $this->sliceOffsetLimit($applications, $offset, $limit);
+        return $applications;
+    }
+
+    /**
+     * @param Application[] $applications
+     * @param array|null $orderBy
+     * @return Application[]
+     */
+    protected function applyOrderBy($applications, $orderBy)
+    {
         if ($orderBy) {
             $applications = new ArrayCollection($applications);
             $applications = $applications->matching(Criteria::create()->orderBy($orderBy))->getValues();
         }
+        return $applications;
+    }
+
+    /**
+     * @param Application[] $applications
+     * @param int|null $limit
+     * @param int|null $offset
+     * @return Application[] array
+     */
+    protected function sliceOffsetLimit($applications, $offset, $limit)
+    {
         if ($offset) {
             $applications = array_slice($applications, $offset);
         }
