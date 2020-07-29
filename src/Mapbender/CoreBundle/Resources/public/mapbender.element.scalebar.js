62db8a5d2ab3c26244b0e70106b67475eef00e41
werrolf <rolf.neuberger@wheregroup.com>
2020-07-29 16:51:09 +0200

    Fix ScaleBar minWidth option on Openlayers 6

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.scalebar.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.scalebar.js
@@ -35,7 +35,7 @@
         _setupOl4: function() {
             var control = new ol.control.ScaleLine({
                 target: this.element.attr('id'),
-                'minWidth': '64',
+                minWidth: '' + Math.max(64, parseInt(this.options.maxWidth) / 3),
                 geodesic: true,
                 units: this.options.units === 'ml' ? 'imperial' : 'metric'
             });
