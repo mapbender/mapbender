4a5171ab891cfacfb024c990235438f64aafc6c8
werrolf <rolf.neuberger@wheregroup.com>
2020-11-11 13:12:46 +0100

    Drop redundant / conflicting sidepane hacks

--- src/Mapbender/CoreBundle/Resources/public/widgets/sidepane.js
+++ src/Mapbender/CoreBundle/Resources/public/widgets/sidepane.js
@@ -6,53 +6,29 @@ $(function() {
 
     var switchButton = $(".toggleSideBar");
     var sidePane = switchButton.closest("div.sidePane");
-    var speed = 300;
-    var animation = {};
-
-    sidePane.css({
-        '-webkit-transition': 'none !important',
-        '-moz-transition':    'none !important',
-        '-o-transition':      'none !important',
-        '-ms-transition':     'none !important',
-        'transition':         'none !important'
-    });
-
-    // closing bugfix
-    sidePane.width(sidePane.width());
 
     if (sidePane.hasClass('closed')) {
-        sidePane.css({
-            'transition': 'none'
-        });
-
         if (sidePane.hasClass("right")) {
             sidePane.css({right: (sidePane.outerWidth(true) * -1) + "px"});
         } else {
             sidePane.css({left: (sidePane.outerWidth(true) * -1) + "px"});
         }
     }
-    sidePane.width(sidePane.width());
-    sidePane.show(0);
 
     switchButton.on('click', function() {
         var wasOpen = !sidePane.hasClass('closed');
+        var animation = {};
         var align = sidePane.hasClass('right') ? 'right' : 'left';
-        var onProgress = function(now, fx) {
-            sidePane.trigger("animate");
-        };
         if (wasOpen) {
-            animation[align] = "-" + sidePane.outerWidth(true) + "px"; //, "swing"];
+            animation[align] = "-" + sidePane.outerWidth(true) + "px";
         } else {
             animation[align] = "0px";
         }
 
         sidePane.animate(animation, {
-            duration: speed,
-            progress: onProgress,
+            duration: 300,
             complete: function() {
                 sidePane.toggleClass('closed', wasOpen);
-                sidePane.trigger("animate");
-                sidePane.trigger("switchSidepane");
             }
         });
     });
