9a662671ab8511eac683b2b6d71e62e33dc7999b
werrolf <rolf.neuberger@wheregroup.com>
2020-12-22 12:53:07 +0100

    Fold copy&pasted parts of Button / AboutDialog / GpsPosition admin types onto new BaseButtonType

--- src/Mapbender/CoreBundle/Element/Type/AboutDialogAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/AboutDialogAdminType.php
@@ -3,18 +3,13 @@ namespace Mapbender\CoreBundle\Element\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AboutDialogAdminType extends AbstractType
 {
-    /**
-     * @inheritdoc
-     */
-    public function configureOptions(OptionsResolver $resolver)
+
+    public function getParent()
     {
-        $resolver->setDefaults(array(
-            'application' => null
-        ));
+        return 'Mapbender\CoreBundle\Element\Type\BaseButtonAdminType';
     }
 
     /**
@@ -22,15 +17,10 @@ class AboutDialogAdminType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('label', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-                'required' => false,
-                'label' => 'mb.core.admin.button.show_label',
-            ))
-            ->add('tooltip', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'required' => false,
-            ))
-        ;
+        // Icon is hard-coded, remove upstream icon field.
+        // @todo: allow configuration, after providing the previously hard-coded setting as a default
+        if ($builder->has('icon')) {
+            $builder->remove('icon');
+        }
     }
-
 }
--- src/Mapbender/CoreBundle/Element/Type/ControlButtonAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/ControlButtonAdminType.php
@@ -1,14 +1,17 @@
 <?php
+
+
 namespace Mapbender\CoreBundle\Element\Type;
+
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ButtonAdminType extends AbstractType
+class ControlButtonAdminType extends AbstractType
 {
     public function getParent()
     {
-        return 'Mapbender\CoreBundle\Element\Type\ControlButtonAdminType';
+        return 'Mapbender\CoreBundle\Element\Type\BaseButtonAdminType';
     }
 
     /**
@@ -17,16 +20,13 @@ class ButtonAdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('click', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('group', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                 'required' => false,
             ))
-            ->add('action', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'required' => false,
-            ))
-            ->add('deactivate', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('target', 'Mapbender\CoreBundle\Element\Type\TargetElementType', array(
+                'application' => $options['application'],
                 'required' => false,
             ))
         ;
     }
-
 }
