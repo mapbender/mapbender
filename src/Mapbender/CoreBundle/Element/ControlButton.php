6897ed6e6bab8a55a65b45b152d1d9dca8205fcb
werrolf <rolf.neuberger@wheregroup.com>
2020-12-23 12:50:06 +0100

    Support falling back to target Element title when rendering ControlButton in frontend

--- src/Mapbender/CoreBundle/Element/ControlButton.php
+++ src/Mapbender/CoreBundle/Element/ControlButton.php
@@ -4,6 +4,8 @@
 namespace Mapbender\CoreBundle\Element;
 
 
+use Mapbender\CoreBundle\Component\ElementBase\MinimalInterface;
+
 class ControlButton extends BaseButton
 {
     // Disable being targetted by any other Button
@@ -60,4 +62,26 @@ class ControlButton extends BaseButton
         return 'MapbenderCoreBundle:Element:control_button.html.twig';
     }
 
+    public function getFrontendTemplateVars()
+    {
+        $title = $this->entity->getTitle();
+        if (!$title) {
+            $target = $this->entity->getTargetElement('target');
+            if ($target) {
+                $title = $target->getTitle();
+                if ($target && $target->getClass()) {
+                    /** @var MinimalInterface|string $targetClass */
+                    $targetClass = $target->getClass();
+                    $title = $targetClass::getClassTitle();
+                }
+            }
+        }
+        if (!$title) {
+            $title = $this->getClassTitle();
+        }
+        return array(
+            'configuration' => $this->entity->getConfiguration(),
+            'title' => $title,
+        );
+    }
 }
