ff973d28b384737721cfc011467181aa9d272ba4
werrolf <rolf.neuberger@wheregroup.com>
2021-01-20 16:22:48 +0100

    Fix DimensionSetDimensionChoice form transformer integration

--- src/Mapbender/WmsBundle/Element/Type/Transformer/DimensionSetTransformer.php
+++ src/Mapbender/WmsBundle/Element/Type/Transformer/DimensionSetTransformer.php
@@ -18,29 +18,8 @@ class DimensionSetTransformer implements DataTransformerInterface
         $this->instances = $dimensionInstances;
     }
 
-    public static function scalarFromInstance(DimensionInst $inst)
-    {
-        return $inst->id . "-" . $inst->getName() . "-" . $inst->getType();
-    }
-
-    public function instanceFromScalar($value)
-    {
-        foreach ($this->instances as $inst) {
-            if ($this->scalarFromInstance($inst) == $value) {
-                return $inst;
-            }
-        }
-        return null;
-    }
-
     public function transform($value)
     {
-        if ($value && !empty($value['group'])) {
-            foreach (array_keys($value['group']) as $key) {
-                $dimValue = $value['group'][$key];
-                $value['group'][$key] = $this->instanceFromScalar($dimValue);
-            }
-        }
         if ($value && !empty($value['dimension'])) {
             if (is_object($value['dimension']) && ($value['dimension'] instanceof DimensionInst)) {
                 $value['dimension'] = $value['dimension']->getConfiguration();
@@ -54,12 +33,6 @@ class DimensionSetTransformer implements DataTransformerInterface
 
     public function reverseTransform($value)
     {
-        if ($value && !empty($value['group'])) {
-            foreach (array_keys($value['group']) as $key) {
-                $dimValue = $value['group'][$key];
-                $value['group'][$key] = $this->scalarFromInstance($dimValue);
-            }
-        }
         if ($value && !empty($value['dimension'])) {
             if (is_string($value['dimension'])) {
                 $value['dimension'] = json_decode($value['dimension'], true);
