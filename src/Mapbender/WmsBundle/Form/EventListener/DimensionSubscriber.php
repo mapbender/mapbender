891d2b2a585a054336b7607d89dbc8c40226ac65
werrolf <rolf.neuberger@wheregroup.com>
2021-01-21 11:31:31 +0100

    Initialize static instance dimension fields statically

--- src/Mapbender/WmsBundle/Form/EventListener/DimensionSubscriber.php
+++ src/Mapbender/WmsBundle/Form/EventListener/DimensionSubscriber.php
@@ -43,54 +43,6 @@ class DimensionSubscriber implements EventSubscriberInterface
      */
     protected function addFields($form, $data)
     {
-        $form
-            ->add('type', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
-                'auto_initialize' => false,
-                'required' => true,
-                'attr' => array(
-                    'readonly' => 'readonly',
-                ),
-            ))
-            ->add('name', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'auto_initialize' => false,
-                'required' => true,
-                'attr' => array(
-                    'readonly' => 'readonly',
-                ),
-            ))
-            ->add('units', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'auto_initialize' => false,
-                'required' => false,
-                'attr' => array(
-                    'readonly' => 'readonly',
-                ),
-            ))
-            ->add('unitSymbol', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'auto_initialize' => false,
-                'required' => false,
-                'attr' => array(
-                    'readonly' => 'readonly',
-                ),
-            ))
-            ->add('multipleValues', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-                'auto_initialize' => false,
-                'label' => 'multiple',
-                'disabled' => true,
-                'required' => false,
-            ))
-            ->add('nearestValue', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-                'auto_initialize' => false,
-                'label' => 'nearest',
-                'disabled' => true,
-                'required' => false,
-            ))
-            ->add('current', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-                'auto_initialize' => false,
-                'label' => 'current',
-                'disabled' => true,
-                'required' => false,
-            ))
-        ;
         $this->addExtentFields($form, $data);
 
         switch ($data->getType()) {
@@ -142,24 +94,6 @@ class DimensionSubscriber implements EventSubscriberInterface
      */
     protected function addExtentFields($form, $data)
     {
-        $form
-            ->add('extent', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
-                'required' => true,
-                'auto_initialize' => false,
-                'attr' => array(
-                    'data-extent' => 'group-dimension-extent',
-                ),
-            ))
-            ->add('origextent', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
-                'required' => true,
-                'auto_initialize' => false,
-                'mapped' => false,
-                'attr' => array(
-                    'data-extent' => 'group-dimension-origextent',
-                ),
-            ))
-        ;
-
         $dimJs = $data->getConfiguration();
         $form
             ->add('json', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
--- src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
+++ src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
@@ -19,10 +19,72 @@ class DimensionInstType extends AbstractType
         $builder->addEventSubscriber($subscriber);
         $transformer = new DimensionTransformer();
         $builder->addModelTransformer($transformer);
-        $builder->add('active', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-            'required' => true,
-            'label' => 'active',
-        ));
+        $builder
+            ->add('active', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
+                'required' => true,
+                'label' => 'active',
+            ))
+            ->add('type', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
+                'auto_initialize' => false,
+                'required' => true,
+                'attr' => array(
+                    'readonly' => 'readonly',
+                ),
+            ))
+            ->add('name', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                'auto_initialize' => false,
+                'required' => true,
+                'attr' => array(
+                    'readonly' => 'readonly',
+                ),
+            ))
+            ->add('units', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                'auto_initialize' => false,
+                'required' => false,
+                'attr' => array(
+                    'readonly' => 'readonly',
+                ),
+            ))
+            ->add('unitSymbol', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                'auto_initialize' => false,
+                'required' => false,
+                'attr' => array(
+                    'readonly' => 'readonly',
+                ),
+            ))
+            ->add('multipleValues', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
+                'auto_initialize' => false,
+                'label' => 'multiple',
+                'disabled' => true,
+                'required' => false,
+            ))
+            ->add('nearestValue', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
+                'auto_initialize' => false,
+                'label' => 'nearest',
+                'disabled' => true,
+                'required' => false,
+            ))
+            ->add('current', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
+                'auto_initialize' => false,
+                'label' => 'current',
+                'disabled' => true,
+                'required' => false,
+            ))
+            ->add('extent', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
+                'required' => true,
+                'auto_initialize' => false,
+                'attr' => array(
+                    'data-extent' => 'group-dimension-extent',
+                ),
+            ))
+            ->add('origextent', 'Symfony\Component\Form\Extension\Core\Type\HiddenType', array(
+                'required' => true,
+                'auto_initialize' => false,
+                'mapped' => false,
+                'attr' => array(
+                    'data-extent' => 'group-dimension-origextent',
+                ),
+            ))
+        ;
     }
-
 }
