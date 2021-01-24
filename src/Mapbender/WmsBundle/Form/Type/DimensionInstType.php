796caab58c08cc81cc8fc685ebfb3b2383339c9a
werrolf <rolf.neuberger@wheregroup.com>
2021-01-24 15:35:48 +0100

    Absorb DimensionSubscriber into DimensionInstType

--- src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
+++ src/Mapbender/WmsBundle/Form/Type/DimensionInstType.php
@@ -5,14 +5,16 @@ namespace Mapbender\WmsBundle\Form\Type;
 use Mapbender\WmsBundle\Component\DimensionInst;
 use Mapbender\WmsBundle\Entity\WmsInstance;
 use Mapbender\WmsBundle\Form\DataTransformer\DimensionTransformer;
-use Mapbender\WmsBundle\Form\EventListener\DimensionSubscriber;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DimensionInstType extends AbstractType
+class DimensionInstType extends AbstractType implements EventSubscriberInterface
 {
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -25,8 +27,7 @@ class DimensionInstType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $subscriber = new DimensionSubscriber();
-        $builder->addEventSubscriber($subscriber);
+        $builder->addEventSubscriber($this);
         $transformer = new DimensionTransformer();
         $builder->addModelTransformer($transformer);
         $builder
@@ -74,6 +75,78 @@ class DimensionInstType extends AbstractType
                 'required' => false,
             ))
         ;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(FormEvents::PRE_SET_DATA => 'preSetData');
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data) {
+            return;
+        }
+        $this->addFields($form, $data);
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param DimensionInst $data
+     */
+    protected function addFields($form, $data)
+    {
+        $instance = $form->getConfig()->getOption('instance');
+        $originalExtent = $this->getOriginalExtent($instance, $data->getName());
+        $ranges = explode(',', $originalExtent);
+
+        $multipleRanges = count($ranges) > 1;
+        if ($multipleRanges) {
+            $extentType = 'Symfony\Component\Form\Extension\Core\Type\HiddenType';
+        } else {
+            $extentType = 'Symfony\Component\Form\Extension\Core\Type\TextType';
+        }
+        $form
+            ->add('extent', $extentType, array(
+                'required' => true,
+                'auto_initialize' => false,
+                'attr' => array(
+                    'readonly' => 'readonly',
+                ),
+                'label' => 'Extent',
+            ))
+        ;
+        if ($multipleRanges) {
+            $choices = array_combine($ranges, $ranges);
+            $form->add('extentRanges', 'Symfony\Component\Form\Extension\Core\Type\ChoiceType', array(
+                'data' => explode(',', $data->getExtent()),
+                'mapped' => false,
+                'choices' => $choices,
+                'choices_as_values' => true,
+                'label' => $form->get('extent')->getConfig()->getOption('label'),
+                'auto_initialize' => false,
+                'multiple' => true,
+                'required' => true,
+            ));
+            $form->add('default', 'Symfony\Component\Form\Extension\Core\Type\ChoiceType', array(
+                'choices' => $choices,
+                'choices_as_values' => true,
+                'auto_initialize' => false,
+            ));
+        } else {
+            if (count($ranges) > 0 && count(explode('/', $ranges[0])) > 1) {
+                $form->add('default', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                    'auto_initialize' => false,
+                    'required' => false,
+                    'attr' => array(
+                        'readonly' => 'readonly',
+                    ),
+                ));
+            }
+        }
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
