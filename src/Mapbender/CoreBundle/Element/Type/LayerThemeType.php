2126c90add18533ae715ed2cf7cbafa38eacf4ad
werrolf <rolf.neuberger@wheregroup.com>
2021-01-25 05:43:47 +0100

    Reduce form theme violations in Layertree backend form

--- src/Mapbender/CoreBundle/Element/Type/LayerThemeType.php
+++ src/Mapbender/CoreBundle/Element/Type/LayerThemeType.php
@@ -30,15 +30,31 @@ class LayerThemeType extends AbstractType
         $builder
             ->add('useTheme', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
                 'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'title' => 'mb.core.admin.layertree.label.theme.useTheme',
+                ),
             ))
             ->add('opened', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
                 'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'title' => 'mb.core.admin.layertree.label.theme.opened',
+                ),
             ))
             ->add('sourceVisibility', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
                 'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'title' => 'mb.core.admin.layertree.label.theme.sourceVisibility',
+                ),
             ))
             ->add('allSelected', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
                 'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'title' => 'mb.core.admin.layertree.label.theme.allSelected',
+                ),
             ))
         ;
     }
