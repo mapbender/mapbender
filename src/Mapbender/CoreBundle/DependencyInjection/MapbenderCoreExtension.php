439d8dba6a10ec3c7a841b968056a4b9c0aa084a
werrolf <rolf.neuberger@wheregroup.com>
2021-02-04 12:27:27 +0100

    Remove custom configuration patching of (external) doctrine:migration:* commands

--- src/Mapbender/CoreBundle/DependencyInjection/MapbenderCoreExtension.php
+++ src/Mapbender/CoreBundle/DependencyInjection/MapbenderCoreExtension.php
@@ -98,7 +98,6 @@ class MapbenderCoreExtension extends Extension
             'mapbender.yml',
             'constraints.yml',
             'formTypes.yml',
-            'migrations.yml',
         ];
     }
 
