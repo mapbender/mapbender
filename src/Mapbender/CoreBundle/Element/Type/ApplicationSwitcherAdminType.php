396cb324e6e36dcd15f2c5e42ca86d36a9520fe4
werrolf <rolf.neuberger@wheregroup.com>
2021-02-16 13:11:52 +0100

    Reorder ApplicationSwitcher form

--- src/Mapbender/CoreBundle/Element/Type/ApplicationSwitcherAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/ApplicationSwitcherAdminType.php
@@ -13,16 +13,16 @@ class ApplicationSwitcherAdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('open_in_new_tab', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
-                'required' => false,
-                'label' => 'mb.core.applicationSwitcher.admin.open_in_new_tab',
-            ))
             ->add('applications', 'Mapbender\ManagerBundle\Form\Type\ApplicationChoiceType', array(
                 'multiple' => true,
                 'label' => 'mb.terms.application.plural',
                 'attr' => array(
                     'size' => 20,
                 ),
+            ))
+            ->add('open_in_new_tab', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array(
+                'required' => false,
+                'label' => 'mb.core.applicationSwitcher.admin.open_in_new_tab',
             ))
         ;
     }
