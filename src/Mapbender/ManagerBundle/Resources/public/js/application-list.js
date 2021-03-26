dd844560c9fbb8f9a2815b261e6549ab30ac7373
werrolf <rolf.neuberger@wheregroup.com>
2021-03-26 11:59:58 +0100

    Extend application list button clickable areas to whole button

--- src/Mapbender/ManagerBundle/Resources/public/js/application-list.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/application-list.js
@@ -1,34 +1,28 @@
 $(function(){
     var $appList = $('#listFilterApplications');
     // Switch application state via Ajax when the current state icon is clicked
-    $appList.on('click', '.iconPublish[data-url],.iconPublishActive[data-url]', function() {
+    $appList.on('click', '.-fn-toggle-publish[data-url]', function() {
         var $this = $(this);
         var url = $this.attr('data-url');
-        var requestedState;
-
-        if (!$this.hasClass("disabled")){
-            requestedState = "disabled";
-        }else{
-            requestedState = "enabled";
-        }
+        var requestedState = $this.toggleClass('-js-on').hasClass('-js-on') && 'enabled' || 'disabled';
 
         $.ajax({
             url: url,
             type: 'POST',
             data: {state: requestedState}
         }).done(function(response) {
-            if (response.newState === 'enabled') {
-                $this.removeClass("disabled").addClass('enabled iconPublishActive');
-            } else {
-                $this.removeClass('enabled iconPublishActive').addClass('disabled iconPublish');
-            }
+            var enabled = response.newState === 'enabled';
+            $('i', $this)
+                .toggleClass('fa-eye-slash', !enabled)
+                .toggleClass('fa-eye', enabled)
+            ;
         }).fail(function(jqXHR, textStatus, errorThrown) {
             Mapbender.error(errorThrown);
         });
         return false;
     });
 
-    $appList.on('click', '.iconRemove[data-url]', function() {
+    $appList.on('click', '.-fn-delete[data-url]', function() {
         var $el = $(this);
         Mapbender.Manager.confirmDelete($el, $el.attr('data-url'), {
             // @todo: bring your own translation string
