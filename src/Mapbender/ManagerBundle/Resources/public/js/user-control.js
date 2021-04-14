b157c25c67c29b1a1a1f5290dd0f1c091ba2eddc
werrolf <rolf.neuberger@wheregroup.com>
2021-04-14 11:50:22 +0200

    Eliminate iconSmall

--- src/Mapbender/ManagerBundle/Resources/public/js/user-control.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/user-control.js
@@ -9,17 +9,7 @@ $(function(){
     });
 
     // Delete group via Ajax
-    $('#listFilterGroups').on("click", '.iconRemove[data-url]', function() {
-        var $el = $(this);
-        Mapbender.Manager.confirmDelete($el, $el.attr('data-url'), {
-            // @todo: bring your own translation string
-            title: "mb.manager.components.popup.delete_element.title",
-            confirm: "mb.actions.delete",
-            cancel: "mb.actions.cancel"
-        });
-        return false;
-    });
-    $('#listFilterUsers').on("click", ".iconRemove[data-url]", function() {
+    $('#listFilterGroups, #listFilterUsers').on("click", '.-fn-delete[data-url]', function() {
         var $el = $(this);
         Mapbender.Manager.confirmDelete($el, $el.attr('data-url'), {
             // @todo: bring your own translation string
