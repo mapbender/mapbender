3b539e7e3140fa46dd8261c32257f3ad2de9be8a
werrolf <rolf.neuberger@wheregroup.com>
2021-04-29 10:44:40 +0200

    Fold separate backend menus for users, groups and acls into single security menu

--- src/FOM/UserBundle/FOMUserBundle.php
+++ src/FOM/UserBundle/FOMUserBundle.php
@@ -28,28 +28,30 @@ class FOMUserBundle extends ManagerBundle
 
     protected function addMenu(ContainerBuilder $container)
     {
-        $userItem = MenuItem::create('fom.user.userbundle.users', 'fom_user_user_index')
+        $securityItem = MenuItem::create('mb.terms.security', false)
             ->setWeight(100)
             ->addChildren(array(
+                MenuItem::create('fom.user.userbundle.users', 'fom_user_user_index'),
                 MenuItem::create('fom.user.userbundle.new_user', 'fom_user_user_create')
                     ->requireEntityGrant('FOM\UserBundle\Entity\User', 'CREATE'),
+                MenuItem::create('fom.user.userbundle.groups', 'fom_user_group_index')
+                    ->requireEntityGrant('FOM\UserBundle\Entity\Group', 'VIEW'),
+                MenuItem::create('fom.user.userbundle.new_group', 'fom_user_group_create')
+                    ->requireEntityGrant('FOM\UserBundle\Entity\Group', 'CREATE'),
+                MenuItem::create('fom.user.userbundle.acls', 'fom_user_acl_index')
+                    ->requireEntityGrant('Symfony\Component\Security\Acl\Domain\Acl', 'EDIT'),
             ))
         ;
         $groupItem = MenuItem::create('fom.user.userbundle.groups', 'fom_user_group_index')
             ->setWeight(110)
-            ->requireEntityGrant('FOM\UserBundle\Entity\Group', 'VIEW')
             ->addChildren(array(
-                MenuItem::create('fom.user.userbundle.new_group', 'fom_user_group_create')
-                    ->requireEntityGrant('FOM\UserBundle\Entity\Group', 'CREATE'),
             ))
         ;
         $aclItem = MenuItem::create('fom.user.userbundle.acls', 'fom_user_acl_index')
             ->setWeight(120)
             ->requireEntityGrant('Symfony\Component\Security\Acl\Domain\Acl', 'EDIT')
         ;
-        $container->addCompilerPass(new RegisterMenuRoutesPass($userItem));
-        $container->addCompilerPass(new RegisterMenuRoutesPass($groupItem));
-        $container->addCompilerPass(new RegisterMenuRoutesPass($aclItem));
+        $container->addCompilerPass(new RegisterMenuRoutesPass($securityItem));
     }
 
     public function getACLClasses()
