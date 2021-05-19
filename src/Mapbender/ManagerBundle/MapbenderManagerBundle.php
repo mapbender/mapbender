f807b9bc3cad1ce333c97c8cbc12da37d57a1399
werrolf <rolf.neuberger@wheregroup.com>
2021-05-19 11:05:23 +0200

    Drop standalone application import main menu item

--- src/Mapbender/ManagerBundle/MapbenderManagerBundle.php
+++ src/Mapbender/ManagerBundle/MapbenderManagerBundle.php
@@ -30,8 +30,6 @@ class MapbenderManagerBundle extends ManagerBundle
             ->addChildren(array(
                 MenuItem::create('mb.manager.managerbundle.new_application', 'mapbender_manager_application_new')
                     ->requireEntityGrant('Mapbender\CoreBundle\Entity\Application', 'CREATE'),
-                MenuItem::create('mb.manager.managerbundle.import_application', 'mapbender_manager_application_import')
-                    ->requireEntityGrant('Mapbender\CoreBundle\Entity\Application', 'CREATE'),
             ))
         ;
         $sourceMenu = MenuItem::create('mb.terms.source.plural', 'mapbender_manager_repository_index')
