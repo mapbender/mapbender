84c881df45508d668b1ed57672a2329fe50806de
werrolf <rolf.neuberger@wheregroup.com>
2021-06-07 11:13:01 +0200

    Add directory support to mapbender:application:import

--- src/Mapbender/CoreBundle/Command/ApplicationImportCommand.php
+++ src/Mapbender/CoreBundle/Command/ApplicationImportCommand.php
@@ -7,6 +7,8 @@ namespace Mapbender\CoreBundle\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\OutputStyle;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 
 class ApplicationImportCommand extends AbstractApplicationTransportCommand
@@ -14,19 +16,49 @@ class ApplicationImportCommand extends AbstractApplicationTransportCommand
     protected function configure()
     {
         $this->setName('mapbender:application:import');
-        $this->addArgument('input', InputArgument::REQUIRED, 'File name (`-` for stdin)');
+        $this->addArgument('input', InputArgument::REQUIRED, 'File name (`-` for stdin) or directory');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $root = $this->getRootUser();
-        $fileName = $input->getArgument('input');
-        if ($fileName === '-') {
-            $content = file_get_contents('php://stdin');
+        $output = new SymfonyStyle($input, $output);
+        $inputArg = $input->getArgument('input');
+        if ($inputArg === '-') {
+            $this->importOne(file_get_contents('php://stdin'), $input, $output);
         } else {
-            $content = file_get_contents($fileName);
+            if (@\file_exists($inputArg)) {
+                if (\is_dir($inputArg)) {
+                    $inputPaths = array();
+                    foreach (\scandir($inputArg) as $file) {
+                        if (preg_match('#\.(yml|yaml)$#', $file)) {
+                            $inputPaths[] = realpath($inputArg) . '/' . $file;
+                        }
+                    }
+                    if (!$inputPaths) {
+                        $output->warning("Input directory {$inputArg} is empty");
+                    }
+                } else {
+                    $inputPaths = array($inputArg);
+                }
+                foreach ($inputPaths as $path) {
+                    // @todo Sf4: support yaml application definition directly (to replace fixture-based mechanism)
+                    $this->importOne(\file_get_contents($path), $input, $output);
+                }
+            } else {
+                throw new \InvalidArgumentException("Input path {$inputArg} does not exist");
+            }
         }
+    }
 
+    /**
+     * @param string $content
+     * @param InputInterface $input
+     * @param OutputStyle $output
+     * @throws \Mapbender\ManagerBundle\Component\Exception\ImportException
+     */
+    protected function importOne($content, InputInterface $input, OutputStyle $output)
+    {
+        $root = $this->getRootUser();
         $importHandler = $this->getApplicationImporter();
         $importArray = $importHandler->parseImportData($content);
         $em = $this->getDefaultEntityManager();
