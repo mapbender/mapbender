7e290799dfe45801666883203120c729b3a4e72d
werrolf <rolf.neuberger@wheregroup.com>
2021-06-15 12:03:25 +0200

    Switch ControlButtonAdminType to use ControlTargetType instead of legacy TargetElementType

--- src/Mapbender/CoreBundle/Element/Type/ControlButtonAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/ControlButtonAdminType.php
@@ -6,7 +6,6 @@ namespace Mapbender\CoreBundle\Element\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ControlButtonAdminType extends AbstractType
@@ -14,13 +13,6 @@ class ControlButtonAdminType extends AbstractType
     public function getParent()
     {
         return 'Mapbender\CoreBundle\Element\Type\BaseButtonAdminType';
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'application' => null,
-        ));
     }
 
     /**
@@ -32,9 +24,7 @@ class ControlButtonAdminType extends AbstractType
             ->add('group', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                 'required' => false,
             ))
-            ->add('target', 'Mapbender\CoreBundle\Element\Type\TargetElementType', array(
-                'application' => $options['application'],
-                'required' => true,
+            ->add('target', 'Mapbender\ManagerBundle\Form\Type\Element\ControlTargetType', array(
                 'constraints' => array(new NotBlank()),
             ))
         ;
