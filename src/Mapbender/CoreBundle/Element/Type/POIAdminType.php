f9d6405ca90e0bd1f72d42a7a6a80caffe080f1a
werrolf <rolf.neuberger@wheregroup.com>
2021-06-15 12:38:35 +0200

    Switch POIAdminType to use ControlTargetType (for gps integration) instead of legacy TargetElementType

--- src/Mapbender/CoreBundle/Element/Type/POIAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/POIAdminType.php
@@ -2,6 +2,7 @@
 
 namespace Mapbender\CoreBundle\Element\Type;
 
+use Mapbender\CoreBundle\Entity\Element;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,10 +35,14 @@ class POIAdminType extends AbstractType
             ->add('body', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                 'required' => true,
             ))
-            ->add('gps', 'Mapbender\CoreBundle\Element\Type\TargetElementType', array(
-                'element_class' => 'Mapbender\\CoreBundle\\Element\\GpsPosition',
-                'application' => $options['application'],
+            ->add('gps', 'Mapbender\ManagerBundle\Form\Type\Element\ControlTargetType', array(
                 'required' => false,
+                'include_buttons' => true,      // NOTE: GpsPosition is a button-type
+                'element_filter_function' => function(Element $element) {
+                    return \is_a($element->getClass(), 'Mapbender\CoreBundle\Element\GpsPosition', true);
+                },
+                // @todo: provide placeholder translations
+                'placeholder' => 'Choose an option',
             ))
             ->add('target', 'Mapbender\ManagerBundle\Form\Type\Element\MapTargetType')
         ;
