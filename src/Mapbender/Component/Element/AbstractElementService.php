8d11df1dae89b223d197b2022abc9838bae1b28d
werrolf <rolf.neuberger@wheregroup.com>
2021-06-25 12:50:09 +0200

    [NFC] add doc blocks listing abstract / interface methods to override for service-type elements

--- src/Mapbender/Component/Element/AbstractElementService.php
+++ src/Mapbender/Component/Element/AbstractElementService.php
@@ -6,6 +6,22 @@ namespace Mapbender\Component\Element;
 
 use Mapbender\CoreBundle\Entity\Element;
 
+/**
+ * Simple base class for service-type elements
+ * Methods left to implement for non-abstract child classes
+ * * static getClassTitle() => string
+ * * static getClassDescription() => string
+ * * static getFormTemplate() => string or falsy
+ * * static getType() => string (form type FQCN)
+ * * static getDefaultConfiguration() => array
+ * * getView(Element) => ElementView or falsy
+ * * getRequiredAssets(Element) => array
+ * * getWidgetName(Element) => string or falsy
+ *
+ * Optional overrides:
+ * * getClientConfiguration(Element) => array (default: full configuration array from Element entity)
+ * * getHttpHandler(Element) => ElementHttpHandlerInterface
+ */
 abstract class AbstractElementService
     implements ElementServiceInterface, HttpHandlerProvider
 
