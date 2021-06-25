520aacf5dece4400d18cb557a0d838cc720e0ffa
werrolf <rolf.neuberger@wheregroup.com>
2021-06-25 10:49:11 +0200

    Add evaluation and getter for "canonical" option on mapbender.element tag

--- src/Mapbender/FrameworkBundle/DependencyInjection/Compiler/RegisterElementServicesPass.php
+++ src/Mapbender/FrameworkBundle/DependencyInjection/Compiler/RegisterElementServicesPass.php
@@ -16,13 +16,17 @@ class RegisterElementServicesPass implements CompilerPassInterface
         foreach ($container->findTaggedServiceIds('mapbender.element') as $id => $tagInfo) {
             $definition = $container->getDefinition($id);
             $handledClassNames = array($definition->getClass());
+            $canonical = null;
             foreach ($tagInfo as $attributes) {
                 if (!empty($attributes['replaces'])) {
                     $handledClassNames = array_merge($handledClassNames, explode(',', $attributes['replaces']));
                 }
+                if (!empty($attributes['canonical'])) {
+                    $canonical = $attributes['canonical'];
+                }
             }
             /** @see \Mapbender\CoreBundle\Component\ElementInventoryService::registerService */
-            $inventoryDefinition->addMethodCall('registerService', array(new Reference($id), $handledClassNames));
+            $inventoryDefinition->addMethodCall('registerService', array(new Reference($id), $handledClassNames, $canonical));
         }
     }
 }
