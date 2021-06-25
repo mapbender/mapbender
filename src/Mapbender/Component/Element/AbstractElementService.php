a39fca14f7fdeb31c9bb88494e411bda01e6928d
werrolf <rolf.neuberger@wheregroup.com>
2021-06-25 12:43:27 +0200

    Remove default implementation for getRequiredAssets

--- src/Mapbender/Component/Element/AbstractElementService.php
+++ src/Mapbender/Component/Element/AbstractElementService.php
@@ -20,23 +20,6 @@ abstract class AbstractElementService
     }
 
     /**
-     * Get lists of paths to required assets.
-     *
-     * Return should be a 2D array with top-level keys 'js', 'css' and 'trans'.
-     * Each sub-array lists the required assets.
-     *
-     * Use bundle-style paths ('@MapbenderSomethingBundle/Resources/public/...') to reference assets from a different
-     * bundle. Use absolute paths ('/components/something/...') to reference a file in the web folder.
-     *
-     * @param Element $element
-     * @return string[][]
-     */
-    public function getRequiredAssets(Element $element)
-    {
-        return array();
-    }
-
-    /**
      * @param Element $element
      * @return ElementHttpHandlerInterface|null
      */
