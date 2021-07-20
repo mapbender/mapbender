b7aaa3f74bf6e6a5400f0e47627828a9c9b795e2
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2021-07-20 14:32:14 +0200

    #1181 Wmc-Editor: Aus PHP Klasse WmcType die Definitionen der InputFelder des Popups entfernt.

--- src/Mapbender/WmcBundle/Form/Type/WmcType.php
+++ src/Mapbender/WmcBundle/Form/Type/WmcType.php
@@ -21,21 +21,27 @@ class WmcType extends AbstractType
             ->add('state', 'Mapbender\CoreBundle\Form\Type\StateType', array(
                 'data_class' => 'Mapbender\CoreBundle\Entity\State',
             ))
-            ->add('keywords', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'required' => false,
-            ))
+            // removed: ticket https://repo.wheregroup.com/db/tickets/-/issues/118
+            // 2021-07-20 arno
+            //->add('keywords', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            //    'required' => false,
+            //))
             ->add('abstract', 'Symfony\Component\Form\Extension\Core\Type\TextareaType', array(
                 'required' => false,
             ))
-            ->add('logourl', 'Mapbender\WmsBundle\Form\Type\LegendUrlType', array(
-                'data_class' => 'Mapbender\WmsBundle\Component\LegendUrl',
-            ))
+            // removed: ticket https://repo.wheregroup.com/db/tickets/-/issues/1181
+            // 2021-07-20 arno
+            //->add('logourl', 'Mapbender\WmsBundle\Form\Type\LegendUrlType', array(
+            //    'data_class' => 'Mapbender\WmsBundle\Component\LegendUrl',
+            //))
             ->add('screenshot', 'Symfony\Component\Form\Extension\Core\Type\FileType', array(
                 'required' => false,
             ))
-            ->add('descriptionurl', 'Mapbender\WmsBundle\Form\Type\OnlineResourceType', array(
-                'data_class' => 'Mapbender\WmsBundle\Component\OnlineResource',
-            ))
+            // removed: ticket https://repo.wheregroup.com/db/tickets/-/issues/1181
+            // 2021-07-20 arno
+            //->add('descriptionurl', 'Mapbender\WmsBundle\Form\Type\OnlineResourceType', array(
+            //    'data_class' => 'Mapbender\WmsBundle\Component\OnlineResource',
+            //))
         ;
     }
 
