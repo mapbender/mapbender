b1e0d69a049c9e1006b7da647d8aefa95bc5e97c
werrolf <rolf.neuberger@wheregroup.com>
2021-08-18 12:41:16 +0200

    [NFC] prune use statement

--- src/Mapbender/Component/Collections/YamlElementCollection.php
+++ src/Mapbender/Component/Collections/YamlElementCollection.php
@@ -8,7 +8,6 @@ use Doctrine\Common\Collections\AbstractLazyCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\Collections\Selectable;
-use Mapbender\CoreBundle\Component\ElementBase\MinimalInterface;
 use Mapbender\CoreBundle\Component\Exception\ElementErrorException;
 use Mapbender\CoreBundle\Entity\Application;
 use Mapbender\CoreBundle\Entity\Element;
