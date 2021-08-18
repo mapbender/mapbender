39ce703cf540a887bf3b791543155a345ee08c64
werrolf <rolf.neuberger@wheregroup.com>
2021-08-18 15:21:26 +0200

    Optimize Symfony container build params

--- src/Mapbender/FrameworkBundle/MapbenderFrameworkBundle.php
+++ src/Mapbender/FrameworkBundle/MapbenderFrameworkBundle.php
@@ -20,6 +20,7 @@ class MapbenderFrameworkBundle extends Bundle
         $configLocator = new FileLocator(__DIR__ . '/Resources/config');
         $loader = new XmlFileLoader($container, $configLocator);
         $loader->load('services.xml');
+        $loader->load('symfony.xml');
         // Register service elements
         // Run pass with reduced priority, so it happens after non-service inventory building has completed
         $container->addCompilerPass(new RegisterElementServicesPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -1);
