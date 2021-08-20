1ebd06643d2651554184d1ad87c85f5c91f16ba1
werrolf <rolf.neuberger@wheregroup.com>
2021-08-20 11:11:47 +0200

    Register Acl bundle

--- src/Mapbender/BaseKernel.php
+++ src/Mapbender/BaseKernel.php
@@ -84,6 +84,7 @@ abstract class BaseKernel extends Kernel
         $bundles = array(
             // Standard Symfony2 bundles
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\AclBundle\AclBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
