b6e16af03647c53eb2dc8270d72ca820d6fa5d76
werrolf <rolf.neuberger@wheregroup.com>
2022-05-24 14:47:27 +0200

    Decouple DimensionsHandler form type from MapTargetType

--- src/Mapbender/WmsBundle/Element/Type/DimensionsHandlerAdminType.php
+++ src/Mapbender/WmsBundle/Element/Type/DimensionsHandlerAdminType.php
@@ -2,14 +2,22 @@
 
 namespace Mapbender\WmsBundle\Element\Type;
 
-use Mapbender\WmsBundle\Element\Type\Subscriber\DimensionsHandlerMapTargetSubscriber;
+use Mapbender\Component\ClassUtil;
+use Mapbender\CoreBundle\Entity\Element;
+use Mapbender\CoreBundle\Entity\Layerset;
+use Mapbender\CoreBundle\Utils\ArrayUtil;
+use Mapbender\WmsBundle\Component\DimensionInst;
+use Mapbender\WmsBundle\Entity\WmsInstance;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 /**
  * @author Paul Schmidt
  */
-class DimensionsHandlerAdminType extends AbstractType
+class DimensionsHandlerAdminType extends AbstractType implements EventSubscriberInterface
 {
     /**
      * @inheritdoc
@@ -20,8 +28,80 @@ class DimensionsHandlerAdminType extends AbstractType
             ->add('tooltip', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                 'required' => false,
             ))
-            ->add('target', 'Mapbender\ManagerBundle\Form\Type\Element\MapTargetType')
         ;
-        $builder->get('target')->addEventSubscriber(new DimensionsHandlerMapTargetSubscriber());
+        $builder->addEventSubscriber($this);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(FormEvents::PRE_SET_DATA => 'preSetData');
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        /** @var Element $element */
+        $element = $event->getForm()->getParent()->getData();
+        $application = $element->getApplication();
+        $dimensions = array();
+        if ($application) {
+            foreach ($application->getElements() as $otherElement) {
+                $otherClassName = $otherElement->getClass();
+                if ($otherClassName && ClassUtil::exists($otherClassName) && \is_a($otherClassName, 'Mapbender\Component\Element\MainMapElementInterface', true)) {
+                    // Map found
+                    $dimensions = $this->collectDimensions($otherElement);
+                    break;
+                }
+            }
+        }
+        $event->getForm()
+            ->add('dimensionsets', "Symfony\Component\Form\Extension\Core\Type\CollectionType", array(
+                'entry_type' => 'Mapbender\WmsBundle\Element\Type\DimensionSetAdminType',
+                'allow_add' => !!count($dimensions),
+                'allow_delete' => true,
+                'auto_initialize' => false,
+                'entry_options' => array(
+                    'dimensions' => $dimensions,
+                ),
+            ))
+        ;
+    }
+
+    /**
+     * @param Element $map
+     * @return DimensionInst[]
+     */
+    protected function collectDimensions(Element $map)
+    {
+        $dimensions = array();
+        foreach ($this->getMapLayersets($map) as $layerset) {
+            foreach ($layerset->getInstances(true) as $instance) {
+                if ($instance->getEnabled() && ($instance instanceof WmsInstance)) {
+                    foreach ($instance->getDimensions() ?: array() as $ix => $dimension) {
+                        /** @var DimensionInst $dimension */
+                        $key = "{$instance->getId()}-{$ix}";
+                        $dimension->id = $key;
+                        $dimensions[$key] = $dimension;
+                    }
+                }
+            }
+        }
+        return $dimensions;
+    }
+
+    /**
+     * @param Element $map
+     * @return Layerset[]
+     */
+    protected function getMapLayersets(Element $map)
+    {
+        $mapConfig = $map->getConfiguration();
+        $layersetIds = array_map('strval', ArrayUtil::getDefault($mapConfig, 'layersets', array()));
+        $layersets = array();
+        foreach ($map->getApplication()->getLayersets() as $layerset) {
+            if (in_array(strval($layerset->getId()), $layersetIds)) {
+                $layersets[] = $layerset;
+            }
+        }
+        return $layersets;
     }
 }
