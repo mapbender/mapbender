e4159bc7255fc09606caab30208b29ad52f928cd
werrolf <rolf.neuberger@wheregroup.com>
2022-06-02 13:19:52 +0200

    Defer print queue commands doctrine access

--- src/Mapbender/PrintBundle/Command/AbstractPrintQueueCommand.php
+++ src/Mapbender/PrintBundle/Command/AbstractPrintQueueCommand.php
@@ -5,18 +5,19 @@ namespace Mapbender\PrintBundle\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
-use Mapbender\PrintBundle\Component\Service\PrintServiceInterface;
 use Mapbender\PrintBundle\Entity\QueuedPrintJob;
 use Mapbender\PrintBundle\Repository\QueuedPrintJobRepository;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 abstract class AbstractPrintQueueCommand extends Command
 {
+    /** @var ManagerRegistry */
+    protected $managerRegistry;
     /** @var Filesystem */
     protected $filesystem;
-    /** @var PrintServiceInterface */
-    protected $printService;
     /** @var EntityManagerInterface */
     protected $entityManager;
     /** @var QueuedPrintJobRepository */
@@ -28,11 +29,17 @@ abstract class AbstractPrintQueueCommand extends Command
                                 Filesystem $filesystem,
                                 $storagePath)
     {
-        $this->repository = $managerRegistry->getRepository('MapbenderPrintBundle:QueuedPrintJob');
-        $this->entityManager = $managerRegistry->getManager();
+        $this->managerRegistry = $managerRegistry;
         $this->filesystem = $filesystem;
         $this->storagePath = $storagePath;
         parent::__construct(null);
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+        $this->repository = $this->managerRegistry->getRepository('MapbenderPrintBundle:QueuedPrintJob');
+        $this->entityManager = $this->managerRegistry->getManager();
     }
 
     /**
