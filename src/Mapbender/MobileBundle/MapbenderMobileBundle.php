85155c624d3c0974d0ec859cdb7dcb3ca7f9895e
werrolf <rolf.neuberger@wheregroup.com>
2022-07-29 12:19:10 +0200

    Redeclare templates as tagged services

--- src/Mapbender/MobileBundle/MapbenderMobileBundle.php
+++ src/Mapbender/MobileBundle/MapbenderMobileBundle.php
@@ -2,7 +2,11 @@
 
 namespace Mapbender\MobileBundle;
 
-use Mapbender\CoreBundle\Component\MapbenderBundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class MapbenderMobileBundle
@@ -11,24 +15,18 @@ use Mapbender\CoreBundle\Component\MapbenderBundle;
  * @author Paul Schmidt <paul.schmidt@wheregroup.com>
  * @author Andriy Oblivantsev <eslider@gmail.com>
  */
-class MapbenderMobileBundle extends MapbenderBundle
+class MapbenderMobileBundle extends Bundle
 {
-
-    /**
-     * @inheritdoc
-     */
-    public function getTemplates()
+    public function build(ContainerBuilder $container)
     {
-        return array('Mapbender\MobileBundle\Template\Mobile');
+        $configLocator = new FileLocator(__DIR__ . '/Resources/config');
+        $loader = new XmlFileLoader($container, $configLocator);
+        $loader->load('templates.xml');
+        $container->addResource(new FileResource($configLocator->locate('templates.xml')));
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function getElements()
+    public function getContainerExtension()
     {
-        return array(
-
-        );
+        return null;
     }
 }
