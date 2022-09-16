f9ccfb496a7b59ac75873e74bea24d2dd913263e
werrolf <rolf.neuberger@wheregroup.com>
2022-09-16 14:23:14 +0200

    Silence missing ext-dom IDE warnings in capabilities parsers

--- src/Mapbender/WmsBundle/Component/WmsCapabilitiesParser130.php
+++ src/Mapbender/WmsBundle/Component/WmsCapabilitiesParser130.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpComposerExtensionStubsInspection */
 
 namespace Mapbender\WmsBundle\Component;
 
--- src/Mapbender/WmtsBundle/Component/WmtsCapabilitiesParser100.php
+++ src/Mapbender/WmtsBundle/Component/WmtsCapabilitiesParser100.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpComposerExtensionStubsInspection */
 
 namespace Mapbender\WmtsBundle\Component;
 
