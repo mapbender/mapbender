3a0db3edafdc32c217449fb853b3eaf4f64cb798
werrolf <rolf.neuberger@wheregroup.com>
2022-09-22 13:29:35 +0200

    Update / decouple SecurityController for deprecations / future errors

--- src/FOM/UserBundle/Controller/SecurityController.php
+++ src/FOM/UserBundle/Controller/SecurityController.php
@@ -4,20 +4,40 @@
 namespace FOM\UserBundle\Controller;
 
 
+use Doctrine\Persistence\ManagerRegistry;
+use FOM\UserBundle\Entity\Group;
+use FOM\UserBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
-class SecurityController extends UserControllerBase
+class SecurityController
 {
+    /** @var ManagerRegistry */
+    protected $managerRegistry;
+    /** @var AuthorizationCheckerInterface */
+    protected $authorizationChecker;
+    /** @var \Twig\Environment */
+    protected $twig;
+    /** @var string */
+    protected $userEntityClass;
+    /** @var string[] */
     protected $aclClasses;
 
-    public function __construct($userEntityClass, array $aclClasses)
+    public function __construct(ManagerRegistry $managerRegistry,
+                                AuthorizationCheckerInterface $authorizationChecker,
+                                \Twig\Environment $twig,
+                                $userEntityClass,
+                                array $aclClasses)
     {
-        parent::__construct($userEntityClass);
+        $this->managerRegistry = $managerRegistry;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->twig = $twig;
+        $this->userEntityClass = $userEntityClass;
         $this->aclClasses = $aclClasses;
     }
 
@@ -33,9 +53,9 @@ class SecurityController extends UserControllerBase
         $aclOid = new ObjectIdentity('class', 'Symfony\Component\Security\Acl\Domain\Acl');
 
         $grants = array(
-            'users' => $this->isGranted('VIEW', $userOid),
-            'groups' => $this->isGranted('VIEW', $groupOid),
-            'acl' => $this->isGranted('EDIT', $aclOid),
+            'users' => $this->authorizationChecker->isGranted('VIEW', $userOid),
+            'groups' => $this->authorizationChecker->isGranted('VIEW', $groupOid),
+            'acl' => $this->authorizationChecker->isGranted('EDIT', $aclOid),
         );
 
         if (!array_filter($grants)) {
@@ -50,18 +70,18 @@ class SecurityController extends UserControllerBase
             'users' => array(),
             'groups' => array(),
         );
-        foreach ($this->getUserRepository()->findAll() as $user) {
-            if ($this->isGranted('VIEW', $user)) {
+        foreach ($this->managerRegistry->getRepository(User::class)->findAll() as $user) {
+            if ($this->authorizationChecker->isGranted('VIEW', $user)) {
                 $vars['users'][] = $user;
             }
         }
         if ($grants['groups']) {
-            $vars['groups'] = $this->getDoctrine()->getRepository('FOM\UserBundle\Entity\Group')->findAll();
+            $vars['groups'] = $this->managerRegistry->getRepository(Group::class)->findAll();
         }
         if ($grants['acl']) {
             $vars['acl_classes'] = $this->aclClasses;
         }
 
-        return $this->render('@FOMUser/Security/index.html.twig', $vars);
+        return new Response($this->twig->render('@FOMUser/Security/index.html.twig', $vars));
     }
 }
