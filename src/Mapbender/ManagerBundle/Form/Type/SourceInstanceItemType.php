fb72614404fdc8ac3da8546587cb851eda945381
werrolf <rolf.neuberger@wheregroup.com>
2022-10-25 08:40:34 +0200

    Remove technical id concatenation dump from instance form layer details container (all interactions linked from page)

--- src/Mapbender/ManagerBundle/Form/Type/SourceInstanceItemType.php
+++ src/Mapbender/ManagerBundle/Form/Type/SourceInstanceItemType.php
@@ -48,15 +48,6 @@ class SourceInstanceItemType extends AbstractType
                 'required' => false,
                 'label' => 'mb.wms.wmsloader.repo.instancelayerform.label.selectedtoc',
             ))
-            ->add('displayId', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                'mapped' => false,
-                'required' => false,
-                'attr' => array(
-                    'readonly' => 'readonly',
-                    'title' => 'mb.wms.wmsloader.repo.instancelayerform.label.id.description',
-                ),
-                'label' => 'mb.wms.wmsloader.repo.instancelayerform.label.id.title',
-            ))
             ->add('displayName', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                 'mapped' => false,
                 'required' => false,
