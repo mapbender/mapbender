3554f09b0c76a32100ecd4fdc44fe143dc6b8541
werrolf <rolf.neuberger@wheregroup.com>
2022-10-27 14:53:29 +0200

    Prune WmcBundle (keep Entity only)

--- src/Mapbender/WmcBundle/MapbenderWmcBundle.php
+++ src/Mapbender/WmcBundle/MapbenderWmcBundle.php
@@ -2,8 +2,12 @@
 
 namespace Mapbender\WmcBundle;
 
-use Mapbender\CoreBundle\Component\MapbenderBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MapbenderWmcBundle extends MapbenderBundle
+/**
+ * Mostly empty bundle kept only to prevent Doctrine from
+ * dropping the "mb_wmc_wmc" table on schema update.
+ */
+class MapbenderWmcBundle extends Bundle
 {
 }
