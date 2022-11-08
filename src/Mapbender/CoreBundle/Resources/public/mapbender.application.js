d9b18ca88fac372dee18eb883fbe23b1607b2a61
werrolf <rolf.neuberger@wheregroup.com>
2022-11-08 10:31:38 +0100

    Add client-side Mapbender.loadUserInfo method

--- src/Mapbender/CoreBundle/Resources/public/mapbender.application.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.application.js
@@ -429,7 +429,24 @@ $.extend(Mapbender, (function($) {
     return {
         source: {},
         initElement: initElement,
-        setup: setup
+        setup: setup,
+        /**
+         * @typedef {Object} mbUserInfo
+         * @property {String|null} name
+         * @property {boolean} isAnonymous
+         * @property {Array<String>} roles
+         */
+        /**
+         * @returns {Promise<mbUserInfo>}
+         * @since v3.2.2
+         */
+        loadUserInfo: (function() {
+            // Fetch once, reuse response
+            var promise = $.getJSON('../userinfo.json').promise();
+            return function() {
+                return promise;
+            }
+        }())
     }
 }(jQuery)));
 
