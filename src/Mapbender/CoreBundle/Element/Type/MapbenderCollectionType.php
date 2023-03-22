c291fd1d7e8e3c3611638a9e1b67fb32d0bdc3ab
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-03-22 10:46:43 +0100

    Add sortable functionality to collapsible collection

--- src/Mapbender/CoreBundle/Element/Type/MapbenderCollectionType.php
+++ src/Mapbender/CoreBundle/Element/Type/MapbenderCollectionType.php
@@ -3,20 +3,25 @@
 namespace Mapbender\CoreBundle\Element\Type;
 
 use Mapbender\ManagerBundle\Form\Type\SortableCollectionType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MapbenderCollectionType extends CollectionType
+class MapbenderCollectionType extends AbstractType
 {
     public const INITIAL_STATE_ALL_COLLAPSED = 'all_collapsed';
     public const INITIAL_STATE_ALL_OPENED = 'all_opened';
     public const INITIAL_STATE_FIRST_OPENED = 'first_opened';
 
+    public function getParent()
+    {
+        return SortableCollectionType::class;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
-        parent::configureOptions($resolver);
         $resolver->setDefaults([
             'allow_collapse' => true,
             'initial_collapse_state' => self::INITIAL_STATE_FIRST_OPENED,
@@ -25,7 +30,6 @@ class MapbenderCollectionType extends CollectionType
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        parent::buildView($view, $form, $options);
         $view->vars = array_replace($view->vars, [
             'allow_collapse' => $options['allow_collapse'],
             'initial_collapse_state' => $options['initial_collapse_state'],
