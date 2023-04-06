1fdcbd84cb1a6a8b18abed9df1017796347dcdf5
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-04-06 12:17:20 +0200

    Localise Scale Display admin and frontend interface

--- src/Mapbender/CoreBundle/Element/Type/ScaleDisplayAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/ScaleDisplayAdminType.php
@@ -1,22 +1,36 @@
 <?php
+
 namespace Mapbender\CoreBundle\Element\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ScaleDisplayAdminType extends AbstractType
 {
-    /**
-     * @inheritdoc
-     */
+    use MapbenderTypeTrait;
+
+    private TranslatorInterface $trans;
+
+    public function __construct(TranslatorInterface $trans)
+    {
+        $this->trans = $trans;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('scalePrefix', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('scalePrefix', 'Symfony\Component\Form\Extension\Core\Type\TextType', $this->createInlineHelpText([
                 'required' => false,
                 'trim' => false,
-            ))
-            ->add('unitPrefix', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array('required' => false))
+                'label' => 'mb.core.scaledisplay.scale_prefix',
+                'help' => 'mb.core.scaledisplay.scale_prefix.help',
+            ], $this->trans))
+            ->add('unitPrefix', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', $this->createInlineHelpText([
+                'required' => false,
+                'label' => 'mb.core.scaledisplay.unit_prefix',
+                'help' => 'mb.core.scaledisplay.unit_prefix.help',
+            ], $this->trans))
         ;
     }
 
