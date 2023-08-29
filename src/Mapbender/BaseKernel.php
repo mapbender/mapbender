a5a4c4dc7625beb37d15b443161d4829af8e8270
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-08-29 19:19:35 +0200

    Load config.yml file in BaseKernel

--- src/Mapbender/BaseKernel.php
+++ src/Mapbender/BaseKernel.php
@@ -65,6 +65,7 @@ class BaseKernel extends Kernel
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir().'/config';
 
+        $loader->load($confDir.'/config.yml', 'glob');
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
