594efa5d8badd305a1360ab4a1bdc483f782c365
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-08-30 09:18:48 +0200

    Load only parameters.yml in BaseKernel

--- src/Mapbender/BaseKernel.php
+++ src/Mapbender/BaseKernel.php
@@ -65,11 +65,11 @@ class BaseKernel extends Kernel
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir().'/config';
 
-        $loader->load($confDir.'/config.yml', 'glob');
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{parameters}'.self::CONFIG_EXTS, 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes): void
