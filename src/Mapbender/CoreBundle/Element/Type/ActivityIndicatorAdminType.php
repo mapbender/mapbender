803839c69eee109e007ac637847bf5b46027bd89
Thorsten Hack <thorsten.hack@wheregroup.com>
2024-01-09 14:04:48 +0100

    Clarify translations for activity indicator

--- src/Mapbender/CoreBundle/Element/Type/ActivityIndicatorAdminType.php
+++ src/Mapbender/CoreBundle/Element/Type/ActivityIndicatorAdminType.php
@@ -2,30 +2,42 @@
 namespace Mapbender\CoreBundle\Element\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 
 class ActivityIndicatorAdminType extends AbstractType
 {
+    use MapbenderTypeTrait;
+
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * @inheritdoc
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('tooltip', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('tooltip', TextType::class, array(
                 'required' => false,
                 'label' => 'mb.core.activityindicator.admin.tooltip',
             ))
-            ->add('activityClass', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('activityClass', TextType::class, array(
                 'required' => false,
                 'label' => 'mb.core.activityindicator.admin.activityclass',
             ))
-            ->add('ajaxActivityClass', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ->add('ajaxActivityClass', TextType::class, $this->createInlineHelpText([
                 'required' => false,
+                'help' => 'mb.core.activityindicator.admin.ajaxactivityclass_help',
                 'label' => 'mb.core.activityindicator.admin.ajaxactivityclass',
-            ))
-            ->add('tileActivityClass', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+            ], $this->translator))
+            ->add('tileActivityClass', TextType::class, array(
                 'required' => false,
                 'label' => 'mb.core.activityindicator.admin.tileactivityclass',
             ))
