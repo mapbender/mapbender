7e09cda2cc33dd9f3902511c1fb21c888336e25b
Felix Rauch <felix.rauch@wheregroup.com>
2024-03-01 13:30:14 +0100

    Update mbViewManager.js
    adjust disabling of save-new button

--- src/Mapbender/CoreBundle/Resources/public/element/mbViewManager.js
+++ src/Mapbender/CoreBundle/Resources/public/element/mbViewManager.js
@@ -58,8 +58,8 @@
             this._toggleEnabled(true);
         },
         _toggleEnabled: function(enabled) {
-            $('.-fn-save-new', this.element.prop('disabled', !enabled));
-            $('input[name="title"]', this.element).prop('disabled', !enabled);
+            $('.-fn-save-new', this.element).prop('disabled', !enabled);
+            $('.-js-viewmanager-new-name', this.element).prop('disabled', !enabled);
         },
         open: function(callback) {
             this.closeCallback = callback || null;
