f17c4619eddf66170ef7092fd64cc7b9d8d2a19d
Thorsten Hack <thorsten.hack@wheregroup.com>
2024-05-02 13:33:57 +0200

    Add more tests

--- src/FOM/UserBundle/Tests/PermissionTest.php
+++ src/FOM/UserBundle/Tests/PermissionTest.php
@@ -4,9 +4,14 @@ namespace FOM\UserBundle\Tests;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use FOM\UserBundle\Entity\Group;
 use FOM\UserBundle\Entity\User;
 use FOM\UserBundle\Security\Permission\PermissionManager;
+use FOM\UserBundle\Security\Permission\ResourceDomainApplication;
 use FOM\UserBundle\Security\Permission\ResourceDomainInstallation;
+use FOM\UserBundle\Security\Permission\SubjectDomainPublic;
+use FOM\UserBundle\Security\Permission\SubjectDomainRegistered;
+use Mapbender\CoreBundle\Entity\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -18,6 +23,7 @@ class PermissionTest extends KernelTestCase
 
     private PermissionManager $permissionManager;
     private EntityRepository $userRepo;
+    private EntityManagerInterface $em;
 
     /** @noinspection PhpFieldAssignmentTypeMismatchInspection */
     protected function setUp(): void
@@ -25,8 +31,8 @@ class PermissionTest extends KernelTestCase
         parent::setUp();
         $container = static::getContainer();
         $this->permissionManager = $container->get(PermissionManager::class);
-        $em = $container->get(EntityManagerInterface::class);
-        $this->userRepo = $em->getRepository(User::class);
+        $this->em = $container->get(EntityManagerInterface::class);
+        $this->userRepo = $this->em->getRepository(User::class);
     }
 
     private function getTokenForUser(string $username): TokenInterface
@@ -61,6 +67,110 @@ class PermissionTest extends KernelTestCase
 
         $this->assertEquals(VoterInterface::ACCESS_DENIED,
             $this->permissionManager->vote($tokenUser1, null, [$action])
+        );
+    }
+
+    function testPermissionOnApplication(): void
+    {
+        $tokenUser = $this->getTokenForUser("user1");
+        $application = $this->em->getRepository(Application::class)->findOneBy(['slug' => 'mapbender_user_db']);
+        $otherApplication = $this->em->getRepository(Application::class)->findOneBy(['slug' => 'mapbender_user_basic_db']);
+        $action = ResourceDomainApplication::ACTION_EDIT;
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, $application, [$action])
+        );
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, $otherApplication, [$action])
+        );
+
+
+        $this->permissionManager->grant($tokenUser->getUser(), $application, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED,
+            $this->permissionManager->vote($tokenUser, $application, [$action])
+        );
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, $otherApplication, [$action])
+        );
+
+
+        $this->permissionManager->revoke($tokenUser->getUser(), $application, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, $application, [$action])
+        );
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, $otherApplication, [$action])
+        );
+    }
+
+    function testGrantOnGroup(): void
+    {
+        $tokenUser = $this->getTokenForUser("user1");
+        $group = $this->em->getRepository(Group::class)->findOneBy(['title' => 'group1']);
+        $action = ResourceDomainInstallation::ACTION_CREATE_APPLICATIONS;
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->grant($group, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->revoke($group, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+    }
+
+    function testGrantPublicAccess(): void
+    {
+        $tokenUser = $this->getTokenForUser("user1");
+        $action = ResourceDomainInstallation::ACTION_CREATE_APPLICATIONS;
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->grant(SubjectDomainPublic::SLUG, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->revoke(SubjectDomainPublic::SLUG, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+    }
+
+    function testGrantRegisteredUsers(): void
+    {
+        $tokenUser = $this->getTokenForUser("user1");
+        $action = ResourceDomainInstallation::ACTION_CREATE_APPLICATIONS;
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->grant(SubjectDomainRegistered::SLUG, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
+        );
+
+        $this->permissionManager->revoke(SubjectDomainRegistered::SLUG, null, $action);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED,
+            $this->permissionManager->vote($tokenUser, null, [$action])
         );
     }
 
