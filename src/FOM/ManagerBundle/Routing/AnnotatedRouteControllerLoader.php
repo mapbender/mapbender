305d74bf3993ef9804fafe0a083e448447fc621e
Thorsten Hack <thorsten.hack@wheregroup.com>
2024-05-13 20:06:42 +0200

    Replace deprecated AnnotatedRouteControllerLoader by AttributeRouteControllerLoader

--- src/FOM/ManagerBundle/Routing/AnnotatedRouteControllerLoader.php
+++ src/FOM/ManagerBundle/Routing/AnnotatedRouteControllerLoader.php
@@ -4,12 +4,12 @@ namespace FOM\ManagerBundle\Routing;
 
 use Symfony\Component\Routing\Route;
 use FOM\ManagerBundle\Configuration\Route as ManagerRoute;
-use Symfony\Bundle\FrameworkBundle\Routing\AnnotatedRouteControllerLoader as FrameworkAnnotatedRouteControllerLoader;
+use Symfony\Bundle\FrameworkBundle\Routing\AttributeRouteControllerLoader as FrameworkAttributeRouteControllerLoader;
 
 /**
  * Prefixes manager routes with a configurable (fom_manager.route_prefix) prefix
  */
-class AnnotatedRouteControllerLoader extends FrameworkAnnotatedRouteControllerLoader
+class AnnotatedRouteControllerLoader extends FrameworkAttributeRouteControllerLoader
 {
 
     public function __construct(protected string $prefix)
