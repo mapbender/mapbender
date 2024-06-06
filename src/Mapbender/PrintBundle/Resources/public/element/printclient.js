84f7ab775ccd71163097fbd89bd7ccfcac1f925d
frauch <felix.rauch@wheregroup.com>
2024-06-06 10:02:16 +0200

    prevent entering overview widget without overview

--- src/Mapbender/PrintBundle/Resources/public/element/printclient.js
+++ src/Mapbender/PrintBundle/Resources/public/element/printclient.js
@@ -617,7 +617,7 @@
             return true;
         },
         _collectOverview: function() {
-            if (this.overviewWidget_ && typeof (this.overviewWidget_.getPrintData) === 'function') {
+            if (this.overviewWidget_ && this.overview && typeof (this.overviewWidget_.getPrintData) === 'function'){
                 try {
                     return this.overviewWidget_.getPrintData();
                 } catch (e) {
