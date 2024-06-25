8e87e865ae83963d0ca7375f55ab443bc4ef367d
frauch <felix.rauch@wheregroup.com>
2024-06-25 15:18:32 +0200

    enable parsing of hsl and hsla

--- src/Mapbender/CoreBundle/Resources/public/mapbender-model/StyleUtil.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender-model/StyleUtil.js
@@ -172,10 +172,45 @@ window.Mapbender.StyleUtil = (function() {
                         components.push(parseFloat(matches[4]));
                     }
                     return components;
+                } else {
+                    matches = (rule || '').match(/^hsl(a?)\((\d+),\s*(\d+)%,\s*(\d+)%(,\s*((?:\d*\.\d+)|(?:\d+(?:\.\d*)?)))?\)$/);
+                    if (matches) {
+                        var alpha = matches[6] ? parseFloat(matches[6]) : 1;
+                        return this.hslaToRgba(matches[2], matches[3], matches[4], alpha);
+                    }
                 }
             }
             throw new Error("Could not parse css color input " + rule);
         },
+
+        hslaToRgba: function(h, s, l, a) {
+            h = parseInt(h);
+            s = parseInt(s) / 100;
+            l = parseInt(l) / 100;
+            a = parseFloat(a);
+            let c = (1 - Math.abs(2 * l - 1)) * s;
+            let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+            let m = l - c / 2;
+            let r, g, b;
+            if (h >= 0 && h < 60) {
+                r = c; g = x; b = 0;
+            } else if (h >= 60 && h < 120) {
+                r = x; g = c; b = 0;
+            } else if (h >= 120 && h < 180) {
+                r = 0; g = c; b = x;
+            } else if (h >= 180 && h < 240) {
+                r = 0; g = x; b = c;
+            } else if (h >= 240 && h < 300) {
+                r = x; g = 0; b = c;
+            } else if (h >= 300 && h < 360) {
+                r = c; g = 0; b = x;
+            }
+            r = Math.round((r + m) * 255);
+            g = Math.round((g + m) * 255);
+            b = Math.round((b + m) * 255);
+            return [r, g, b, a];
+        },
+
         _resolveSvgDefault: function(styleObject, propName) {
             var styleValue = styleObject[propName];
             if (typeof styleValue !== 'undefined') {
