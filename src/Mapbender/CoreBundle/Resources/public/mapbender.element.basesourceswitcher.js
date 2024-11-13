37a27b913f5637de86bab46e755da153588ef746
dholler <daniela.holler@wheregroup.com>
2024-11-13 11:57:42 +0100

    Revert "clicking on the top menu closes the group menu"
    This reverts commit d558c48a8580925c49736cb066e49c79da0eb5ef.

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.basesourceswitcher.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.basesourceswitcher.js
@@ -11,7 +11,7 @@
                 self.mbMap = mbMap;
                 self._setup();
             }, function() {
-                //Mapbender.checkTarget('mbBaseSourceSwitcher')
+                Mapbender.checkTarget('mbBaseSourceSwitcher')
             });
         },
 
@@ -26,17 +26,12 @@
             this.updateHighlights();
             this.element.on('mouseover click', '.basesourcegroup', (evt) => {
                 const $group = $(evt.target).closest('.basesourcegroup');
-                $group.removeClass('deactivated');
                 if (evt.type === 'click') {
-                    if($group.hasClass('active')){
-                        $group.addClass('deactivated').removeClass('active');
-                    } else {
-                        $group.addClass('active');
-                        evt.stopPropagation();
-                        $(document).one('click', () => {
-                            $group.removeClass('active');
-                        });
-                    }
+                    $group.addClass('active');
+                    evt.stopPropagation();
+                    $(document).one('click', () => {
+                        $group.removeClass('active')
+                    });
                 }
                 $group.find('.basesourcesubswitcher').toggleClass('right', evt.clientX < 150);
             });
