e6e1f1cec02796245b285e00b7309ea98a120a87
Thorsten Hack <thorsten.hack@wheregroup.com>
2024-11-18 16:24:18 +0100

    Setup failure handler for Rest API API

--- src/Mapbender/CoreBundle/Controller/ApiController.php
+++ src/Mapbender/CoreBundle/Controller/ApiController.php
@@ -4,7 +4,7 @@ namespace Mapbender\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ApiController extends AbstractController
 {
