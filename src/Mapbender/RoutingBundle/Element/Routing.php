83a3023b9a7ae5f8399162323c655ccaaf7f7025
PhilVanB <philipp.vanbiesbrock@wheregroup.com>
2025-01-23 09:38:43 +0100

    [Routing] Add bootstrap class for attribution links

--- src/Mapbender/RoutingBundle/Element/Routing.php
+++ src/Mapbender/RoutingBundle/Element/Routing.php
@@ -93,10 +93,10 @@ class Routing extends AbstractElementService
             'routingConfig' => [
                 'osrm' => [
                     'url' => 'https://',
-                    'attribution' => 'Daten © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende
-  (<a href="https://opendatacommons.org/licenses/odbl/index.html">ODbL</a>), <a
-  href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, <a
-  href="https://openstreetmap.org/fixthemap">mitmachen/Fehler melden</a>',
+                    'attribution' => 'Daten © <a href="https://www.openstreetmap.org/copyright" class="link-primary">OpenStreetMap</a>-Mitwirkende
+  (<a href="https://opendatacommons.org/licenses/odbl/index.html" class="link-primary">ODbL</a>), <a
+  href="https://creativecommons.org/licenses/by-sa/2.0/" class="link-primary">CC-BY-SA</a>, <a
+  href="https://openstreetmap.org/fixthemap" class="link-primary">mitmachen/Fehler melden</a>',
                 ],
             ],
             'searchConfig' => [
