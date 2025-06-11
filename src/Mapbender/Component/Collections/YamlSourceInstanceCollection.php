82bb37613659f1f40a1db8cc6172ac39e3ef18d6
Thorsten Hack <thorsten.hack@wheregroup.com>
2025-06-11 14:36:27 +0200

    Fix YAML Mapper

--- src/Mapbender/Component/Collections/YamlSourceInstanceCollection.php
+++ src/Mapbender/Component/Collections/YamlSourceInstanceCollection.php
@@ -7,27 +7,17 @@ namespace Mapbender\Component\Collections;
 use Doctrine\Common\Collections\AbstractLazyCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Mapbender\CoreBundle\Component\Source\SourceInstanceFactory;
+use Mapbender\CoreBundle\Component\Source\TypeDirectoryService;
 use Mapbender\CoreBundle\Entity\Layerset;
 
 class YamlSourceInstanceCollection extends AbstractLazyCollection
 {
-    /** @var SourceInstanceFactory */
-    protected $factory;
-    /** @var Layerset */
-    protected $layerset;
-    /** @var array */
-    protected $data;
 
-    /**
-     * @param SourceInstanceFactory $factory
-     * @param Layerset $layerset
-     * @param array $data
-     */
-    public function __construct(SourceInstanceFactory $factory, Layerset $layerset, $data)
+    public function __construct(
+        protected TypeDirectoryService $directoryService,
+        protected Layerset             $layerset,
+        protected array                $data)
     {
-        $this->factory = $factory;
-        $this->layerset = $layerset;
-        $this->data = $data;
     }
 
     protected function doInitialize()
@@ -35,7 +25,8 @@ class YamlSourceInstanceCollection extends AbstractLazyCollection
         $this->collection = new ArrayCollection();
         $weight = 0;
         foreach ($this->data as $instanceId => $instanceDefinition) {
-            $instance = $this->factory->fromConfig($instanceDefinition, $instanceId);
+            $factory = $this->directoryService->getInstanceFactoryByType($instanceDefinition['type']);
+            $instance = $factory->fromConfig($instanceDefinition, $instanceId);
             $instance->setLayerset($this->layerset);
             $instance->setWeight($weight++);
             $this->collection->add($instance);
