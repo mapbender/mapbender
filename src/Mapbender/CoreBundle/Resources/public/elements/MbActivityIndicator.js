f0504cc2e8db321363ab03a9aaaa3966de1f445a
PhilVanB <philipp.vanbiesbrock@wheregroup.com>
2025-09-04 13:40:26 +0200

    [Native JS Elements] Add ActivityIndicator

--- src/Mapbender/CoreBundle/Resources/public/elements/MbActivityIndicator.js
+++ src/Mapbender/CoreBundle/Resources/public/elements/MbActivityIndicator.js
@@ -1,76 +1,72 @@
-(function ($) {
-    'use strict';
+(function() {
 
-    $.widget("mapbender.mbActivityIndicator", {
-        options: {
-            activityClass: 'mb-activity',
-            ajaxActivityClass: 'mb-activity-ajax',
-            titleActivityClass: 'mb-activity-tile'
-        },
+    class MbActivityIndicator extends MapbenderElement {
+        constructor(configuration, $element) {
+            super(configuration, $element);
 
-        ajaxActivity: false,
-        tileActivity: false,
-        loadingLayers: [],
-
-        _create: function () {
-            var $mbMap = $('.mb-element-map');
+            const $mbMap = $('.mb-element-map');
             if ($mbMap.length) {
                 this._setupMap({element: $mbMap.eq(0)});
             } else {
-                Mapbender.checkTarget("mbActivityIndicator");
+                Mapbender.checkTarget('mbActivityIndicator');
             }
-            this.element.on('ajaxStart', $.proxy(this._onAjaxStart, this));
-            this.element.on('ajaxStop', $.proxy(this._onAjaxStop, this));
-        },
-        _setupMap: function(mbMap) {
-            var self = this;
+            this.$element.on('ajaxStart', this._onAjaxStart.bind(this));
+            this.$element.on('ajaxStop', this._onAjaxStop.bind(this));
+        }
+
+        _setupMap(mbMap) {
+            const self = this;
             mbMap.element.on('mbmapsourceloadstart', function(event, data) {
-                var source = data.source;
+                const source = data.source;
                 self.loadingLayers.push(source);
                 self._onLayerLoadChange();
             });
             mbMap.element.on('mbmapsourceloadend mbmapsourceremoved mbmapsourceloaderror', function(event, data) {
-                var source = data.source;
+                const source = data.source;
                 self.loadingLayers = self.loadingLayers.filter(function(x) {
                     return x !== source;
                 });
                 self._onLayerLoadChange();
             });
-        },
+        }
+
         /**
          * Listener for global ajaxStart events
          */
-        _onAjaxStart: function () {
+        _onAjaxStart() {
             this.ajaxActivity = true;
             this._updateBodyClass();
-        },
+        }
 
         /**
          * Listener for global ajaxStop events
          */
-        _onAjaxStop: function () {
+        _onAjaxStop() {
             this.ajaxActivity = false;
             this._updateBodyClass();
-        },
+        }
 
-        _onLayerLoadChange: function () {
-            var stillLoading = this.loadingLayers.length > 0;
+        _onLayerLoadChange() {
+            const stillLoading = this.loadingLayers.length > 0;
 
             if (stillLoading !== this.tileActivity) {
                 this.tileActivity = stillLoading;
                 this._updateBodyClass();
             }
-        },
+        }
 
         /**
          * Update body classes to match current activity
          */
-        _updateBodyClass: function () {
-            var $body = $('body');
+        _updateBodyClass() {
+            const $body = $('body');
             $body.toggleClass(this.options.ajaxActivityClass, this.ajaxActivity);
             $body.toggleClass(this.options.titleActivityClass, this.tileActivity);
             $body.toggleClass(this.options.activityClass, this.tileActivity || this.ajaxActivity);
         }
-    });
+    }
 
-})(jQuery);
+    window.Mapbender.Element = window.Mapbender.Element || {};
+    window.Mapbender.Element.MbActivityIndicator = MbActivityIndicator;
+
+})();
