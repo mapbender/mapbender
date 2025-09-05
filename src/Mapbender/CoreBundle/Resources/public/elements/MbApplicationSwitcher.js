6390e05365af2b676e05e3659d6bcc21ecbc074f
PhilVanB <philipp.vanbiesbrock@wheregroup.com>
2025-09-05 14:02:39 +0200

    [Native JS Elements] Add MbApplicationSwitcher

--- src/Mapbender/CoreBundle/Resources/public/elements/MbApplicationSwitcher.js
+++ src/Mapbender/CoreBundle/Resources/public/elements/MbApplicationSwitcher.js
@@ -1,41 +1,37 @@
-;!(function($) {
-    "use strict";
-    $.widget("mapbender.mbApplicationSwitcher", {
-        options: {
-            open_in_new_tab: false
-        },
-        mbMap: null,
-        baseUrl: null,
-        elementUrl: null,
+(function() {
 
-        _create: function() {
-            var self = this;
-            this.elementUrl = [Mapbender.configuration.application.urls.element, this.element.attr('id')].join('/');
+    class MbApplicationSwitcher extends MapbenderElement {
+        constructor(configuration, $element) {
+            super(configuration, $element);
+
+            this.elementUrl = [Mapbender.configuration.application.urls.element, this.$element.attr('id')].join('/');
             this.baseUrl = window.location.href.replace(/(\/application\/).*$/, '$1');
-            Mapbender.elementRegistry.waitReady('.mb-element-map').then(function(mbMap) {
-                self._setup(mbMap);
+            Mapbender.elementRegistry.waitReady('.mb-element-map').then((mbMap) => {
+                this._setup(mbMap);
             });
-            $.ajax([this.elementUrl, 'granted'].join('/')).then(function(response) {
+            $.ajax([this.elementUrl, 'granted'].join('/')).then((response) => {
                 var slugs = response.concat([Mapbender.configuration.application.slug]);
-                self._filterGranted(slugs);
+                this._filterGranted(slugs);
             });
             this._updateDropoutDirection();
-        },
-        _setup: function(mbMap) {
+        }
+
+        _setup(mbMap) {
             this.mbMap = mbMap;
             this._initEvents();
-        },
-        _initEvents: function() {
-            var self = this;
-            this.element.on('change', 'select', function() {
-                self._switchApplication($(this).val());
+        }
+
+        _initEvents() {
+            this.$element.on('change', 'select', (e) => {
+                this._switchApplication($(e.currentTarget).val());
             });
-            window.addEventListener('resize', function() {
-                self._updateDropoutDirection();
+            window.addEventListener('resize', () => {
+                this._updateDropoutDirection();
             });
-        },
-        _filterGranted: function(slugs) {
-            var $options = $('select option', this.element);
+        }
+
+        _filterGranted(slugs) {
+            var $options = $('select option', this.$element);
             var optionsCount = $options.length;
             for (var i = 0; i < $options.length; ++i) {
                 var value = $options[i].value;
@@ -43,16 +39,17 @@
                     $options.eq(i).remove();
                     --optionsCount;
                     // Custom dropdown widget support needs separate removal of display element corresponding to option
-                    $('.dropdownList [data-value="' + value + '"]', this.element).remove();
+                    $('.dropdownList [data-value="' + value + '"]', this.$element).remove();
                 }
             }
-            $('select', this.element).trigger('dropdown.changevisual');
+            $('select', this.$element).trigger('dropdown.changevisual');
             // If we have nothing left to switch to (deleted yaml applications...), remove from DOM
             if (optionsCount <= 1) {
-                this.element.remove();
+                this.$element.remove();
             }
-        },
-        _switchApplication: function(slug) {
+        }
+
+        _switchApplication(slug) {
             var targetApplicationUrl = [this.baseUrl, slug].join('');
             var viewParams = this.mbMap.getModel().getCurrentViewParams();
             var targetHash = this.mbMap.getModel().encodeViewParams(viewParams);
@@ -62,11 +59,12 @@
             } else {
                 window.location.href = targetUrl;
             }
-        },
-        _updateDropoutDirection: function() {
-            if (this.element.closest('.toolBar').length) {
+        }
+
+        _updateDropoutDirection() {
+            if (this.$element.closest('.toolBar').length) {
                 var windowWidth = $('html').get(0).clientWidth;
-                var node = this.element.get(0);
+                var node = this.$element.get(0);
                 var ownWidth = node.clientWidth;
                 var distanceLeft = 0;
                 do {
@@ -76,19 +74,21 @@
                 var distanceRight = windowWidth - distanceLeft - ownWidth;
                 if (windowWidth && distanceRight >= windowWidth / 2) {
                     // Extend dropout list into free space to the right
-                    $('.dropdownList', this.element).css({
+                    $('.dropdownList', this.$element).css({
                         left: 0,
                         right: ''
                     });
                 } else {
                     // Extend dropout list into free space to the left
-                    $('.dropdownList', this.element).css({
+                    $('.dropdownList', this.$element).css({
                         left: '',
                         right: 0
                     });
                 }
             }
-        },
-        __dummy__: null
-    });
-})(jQuery);
+        }
+    }
+
+    window.Mapbender.Element = window.Mapbender.Element || {};
+    window.Mapbender.Element.MbApplicationSwitcher = MbApplicationSwitcher;
+})();
