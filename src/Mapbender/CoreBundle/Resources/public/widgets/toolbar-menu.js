d7dbbbb54eee59189e21ac45343fe59ea11bfbe4
dholler <daniela.holler@wheregroup.com>
2025-09-26 10:39:18 +0200

    Positioning dropdown-menu, lower opacity, remove border

--- src/Mapbender/CoreBundle/Resources/public/widgets/toolbar-menu.js
+++ src/Mapbender/CoreBundle/Resources/public/widgets/toolbar-menu.js
@@ -4,9 +4,22 @@
         // Leave Bootstrap script integration alone
         if (!$btn.attr('data-toggle')) {
             var $wrapper = $btn.closest('.menu-wrapper');
+            var $toolbar = $wrapper.closest('.toolBar');
+
+            // Calculate the dropdown-menu position
+            if ($wrapper.hasClass('open')) {
+                $wrapper.removeClass('open');
+            } else {
+                var toolbarRect = $toolbar[0].getBoundingClientRect();
+                var dropdownTop = toolbarRect.bottom -3;
+
+                document.documentElement.style.setProperty('--dropdown-top', dropdownTop + 'px');
+
+                $wrapper.addClass('open');
+            }
+
             // Wrapper is .dropdown OR .dropup
             // @see https://getbootstrap.com/docs/3.4/components/#dropdowns
-            $wrapper.toggleClass('open');
             $btn.toggleClass('active', $wrapper.is('.open'));
             $('i', $btn)
                 .toggleClass('fa-bars', !$wrapper.is('.open'))
