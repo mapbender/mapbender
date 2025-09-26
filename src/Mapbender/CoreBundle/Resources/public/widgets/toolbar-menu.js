ae4551c9c884b37d6e3433123f7c8f6bf5d29d85
dholler <daniela.holler@wheregroup.com>
2025-09-26 11:54:43 +0200

    Exchange icon

--- src/Mapbender/CoreBundle/Resources/public/widgets/toolbar-menu.js
+++ src/Mapbender/CoreBundle/Resources/public/widgets/toolbar-menu.js
@@ -23,7 +23,7 @@
             $btn.toggleClass('active', $wrapper.is('.open'));
             $('i', $btn)
                 .toggleClass('fa-bars', !$wrapper.is('.open'))
-                .toggleClass('fa-x', $wrapper.is('.open'));
+                .toggleClass('fa-xmark', $wrapper.is('.open'));
         }
     });
 }(jQuery));
