a70d55773233bb16187a2a37d9b462412ee5a7ad
Thorsten Hack <thorsten.hack@wheregroup.com>
2025-11-19 15:46:22 +0100

    Extend SQLSearchEngine with support for dates, numbers and greater/lower than operators

--- src/Mapbender/CoreBundle/Component/SQLSearchEngine.php
+++ src/Mapbender/CoreBundle/Component/SQLSearchEngine.php
@@ -3,6 +3,7 @@
 namespace Mapbender\CoreBundle\Component;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Mapbender\CoreBundle\Utils\ArrayUtil;
@@ -26,12 +27,11 @@ class SQLSearchEngine
      * @param String $srs Current map SRS
      * @param array $extent Current map extent
      * @return array              Autocomplete suggestions
-     * @todo Make like search configurable (exact, left, right, both)
      * @todo Make case invariant configurable
      * @todo Limit results
      *
      */
-    public function autocomplete($config, $key, $value, $properties, $srs, $extent)
+    public function autocomplete(array $config, string $key, string $value, array $properties, string $srs, array $extent): array
     {
         $connection = $this->getConnection($config);
         $qb = $connection->createQueryBuilder();
@@ -43,7 +43,7 @@ class SQLSearchEngine
         $qb->from($config['class_options']['relation'], 't');
 
         // Build WHERE condition
-        $qb->where($this->getTextMatchExpression($connection, $key, $value, 'ilike', $qb));
+        $qb->where($this->getMatchExpression($connection, $key, $value, 'ilike', $qb));
         if ($srs && $extent && !empty($config['class_options']['geometry_attribute'])) {
             $geomColumn = 't.' . $connection->quoteIdentifier(trim($config['class_options']['geometry_attribute'], '"'));
             $qb->andWhere($this->getBoundsExpression($qb, $geomColumn, $extent, $srs, $config));
@@ -53,7 +53,7 @@ class SQLSearchEngine
             $otherProps = explode(',', $fieldConfig['options']['attr']['data-autocomplete-using']);
             foreach ($otherProps as $otherProp) {
                 if (strlen($properties[$otherProp] ?? null)) {
-                    $qb->andWhere($this->getTextMatchExpression($connection, $otherProp, $properties[$otherProp], 'ilike-right', $qb));
+                    $qb->andWhere($this->getMatchExpression($connection, $otherProp, $properties[$otherProp], 'ilike-right', $qb));
                 } else {
                     $this->logger->warning('Key "' . $otherProp . '" for autocomplete-using does not exist in data.');
                 }
@@ -86,11 +86,8 @@ class SQLSearchEngine
      * @param array $extent Search extent
      * @return array         Search results
      * @todo Paging
-     *
-     * @todo Make like search configurable (exact, left, right, both)
-     * @todo Make case invariant configurable
      */
-    public function search($config, $data, $srs, $extent)
+    public function search(array $config, array $data, string $srs, array $extent): array
     {
         $options = $config['class_options'];
         $connection = $this->getConnection($config);
@@ -111,12 +108,12 @@ class SQLSearchEngine
         $qb->from($config['class_options']['relation'], 't');
 
         foreach ($data['form'] as $key => $value) {
-            if (!\strlen($value)) {
+            if (!$value) {
                 continue;
             }
             $fieldConfig = $this->getFormFieldConfig($config, $key);
             $matchMode = ArrayUtil::getDefault($fieldConfig, 'compare', 'ilike');
-            $qb->andWhere($this->getTextMatchExpression($connection, $key, $value, $matchMode, $qb));
+            $qb->andWhere($this->getMatchExpression($connection, $key, $value, $matchMode, $qb));
         }
         if ($srs && $extent) {
             $qb->andWhere($this->getBoundsExpression($qb, $geomColumn, $extent, $srs, $config));
@@ -126,11 +123,7 @@ class SQLSearchEngine
         return $this->rowsToGeoJson($stmt);
     }
 
-    /**
-     * @param array|\Traversable $rows
-     * @return array
-     */
-    protected static function rowsToGeoJson($rows)
+    protected static function rowsToGeoJson(iterable $rows): array
     {
         $features = array();
         foreach ($rows as $row) {
@@ -158,73 +151,73 @@ class SQLSearchEngine
         return $this->registry->getConnection($connectionName);
     }
 
-    protected function getTextMatchExpression(Connection $connection, string $key, $value, string $mode, QueryBuilder $qb): mixed
+    protected function getMatchExpression(Connection $connection, string $key, $value, string $mode, QueryBuilder $qb): string
     {
-        switch ($mode) {
-            case 'exact':
-            case 'like':
-            case 'like-left':
-            case 'like-right':
-                $caseInsensitive = false;
-                break;
-            default:
-            case 'iexact':
-            case 'ilike':
-            case 'ilike-left':
-            case 'ilike-right':
-                $caseInsensitive = true;
-                break;
-        }
-        switch ($mode) {
-            default:
-            case 'ilike':
-            case 'like':
-                $patternPrefix = '%';
-                $patternSuffix = '%';
-                break;
-            case 'ilike-left':
-            case 'like-left':
-                $patternPrefix = '%';
-                $patternSuffix = '';
-                break;
-            case 'ilike-right':
-            case 'like-right':
-                $patternPrefix = '';
-                $patternSuffix = '%';
-                break;
-            case 'exact':
-            case 'iexact':
-                $patternPrefix = '';
-                $patternSuffix = '';
-                break;
-        }
-        $matchValue = strtr($value, array(
-            '%' => '\%',
-            '_' => '\_',
-        ));
-        $matchValue = "{$patternPrefix}{$matchValue}{$patternSuffix}";
-        $placeHolder = $qb->createNamedParameter($matchValue);
         $referenceExpression = "t." . $connection->quoteIdentifier($key);
-        $matchExpression = $placeHolder;
-        if ($caseInsensitive) {
-            $referenceExpression = "LOWER({$referenceExpression})";
-            $matchExpression = "LOWER({$matchExpression})";
+        $matchValue = $this->prepareValue($value, $mode);
+
+        $parameterType = match (true) {
+            is_string($value) => ParameterType::STRING,
+            is_numeric($value) => ParameterType::INTEGER,
+            default => ParameterType::STRING
+        };
+        $matchExpression = $qb->createNamedParameter($matchValue, $parameterType);
+
+        $ignoreCase = match ($mode) {
+            'iexact', 'ilike', 'ilike-left', 'ilike-right' => true,
+            default => false,
+        };
+
+        if ($ignoreCase) {
+            $referenceExpression = "LOWER($referenceExpression)";
+            $matchExpression = "LOWER($matchExpression)";
         }
-        if (!$patternPrefix && !$patternSuffix) {
-            return $qb->expr()->eq($referenceExpression, $matchExpression);
-        } else {
-            return $qb->expr()->like($referenceExpression, $matchExpression);
+
+        return $this->getMatchExpressionForMode($mode, $qb, $referenceExpression, $matchExpression);
+    }
+
+    protected function getMatchExpressionForMode(string $mode, QueryBuilder $qb, string $referenceExpression, string $matchExpression): string
+    {
+        return match ($mode) {
+            'greater' => $qb->expr()->gt($referenceExpression, $matchExpression),
+            'greater-equal' => $qb->expr()->gte($referenceExpression, $matchExpression),
+            'lower',  => $qb->expr()->lt($referenceExpression, $matchExpression),
+            'lower-equal' => $qb->expr()->lte($referenceExpression, $matchExpression),
+            'ilike', 'like', 'ilike-left', 'like-left', 'ilike-right', 'like-right' => $qb->expr()->like($referenceExpression, $matchExpression),
+            'not' => $qb->expr()->neq($referenceExpression, $matchExpression),
+            default => $qb->expr()->eq($referenceExpression, $matchExpression),
+        };
+    }
+
+    protected function prepareValue(mixed $value, string $mode): mixed
+    {
+        if ($value instanceof \DateTime) {
+            return $value->format('Y-m-d');
         }
+
+        if (is_string($value)) {
+            $patternPrefix = match ($mode) {
+                'ilike', 'like', 'ilike-left', 'like-left' => '%',
+                default => '',
+            };
+            $patternSuffix = match ($mode) {
+                'ilike', 'like', 'ilike-right', 'like-right' => '%',
+                default => '',
+            };
+
+            $value = strtr($value, array(
+                '%' => '\%',
+                '_' => '\_',
+            ));
+            return "$patternPrefix$value$patternSuffix";
+        }
+        return $value;
     }
 
     /**
-     * @param QueryBuilder $qb
-     * @param string $geomReference
      * @param float[] $extent 4 values left / bottom / right / top
-     * @param string $srsName
-     * @return string
      */
-    protected function getBoundsExpression(QueryBuilder $qb, $geomReference, $extent, $srsName, array $config)
+    protected function getBoundsExpression(QueryBuilder $qb, string $geomReference, array $extent, string $srsName, array $config): string
     {
         // per default, the map extent is compared to a feature by converting both to EPSG:4326 (WGS84).
         // comparing in a non-global SRS (like UTM32) can result in errors when comparing it to an extent defined
@@ -246,11 +239,8 @@ class SQLSearchEngine
      * Strips namespace prefix from given $srsName and returns the numeric srs id.
      * Only supports 'EPSG:' namespace prefix. If $srsName already is a plain number,
      * return it cast to integer but otherwise unchanged.
-     *
-     * @param $srsName
-     * @return int
      */
-    protected function srsIdFromName($srsName)
+    protected function srsIdFromName($srsName): int
     {
         $parts = explode(':', $srsName);
         if (count($parts) === 1 && (strval(intval($parts[0])) === strval($parts[0])) && $parts[0]) {
@@ -262,12 +252,7 @@ class SQLSearchEngine
         return intval($parts[1]);
     }
 
-    /**
-     * @param array $config
-     * @param string $key
-     * @return array
-     */
-    protected static function getFormFieldConfig($config, $key)
+    protected static function getFormFieldConfig(array $config, string $key): array
     {
         if (!array_key_exists($key, $config['form'])) {
             return $config['form']['"' . $key . '"'];
